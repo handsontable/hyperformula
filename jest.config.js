7c50d5b57f30bc025c08460cc8fff2392cba4fd4
Rafał Łasocha <me@swistak35.com>
2018-09-22 10:45:52 +0200

    Move tests to saner directory
    And run tests only from test/, not from lib/test/

--- jest.config.js
+++ jest.config.js
@@ -137,7 +137,7 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/__tests__/*.+(ts|tsx|js)"
+    "<rootDir>/test/*.+(ts|tsx|js)"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
