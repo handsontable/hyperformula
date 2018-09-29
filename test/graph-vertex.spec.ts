d0ea5a89c0cbc25c051adb3683ed5fbe235e1321
Jakub Kowalski <kowalski_jakub@hotmail.com>
2018-09-29 10:46:11 +0200

    Fixed graph-vertex specs

--- test/graph-vertex.spec.ts
+++ test/graph-vertex.spec.ts
@@ -1,12 +1,12 @@
 import {Graph} from "../src/Graph";
-import {Vertex} from "../src/Vertex";
+import {ValueCellVertex, Vertex} from "../src/Vertex";
 
 describe('Graph with Vertex', () => {
   it('#addNode works correctly with Vertex instances', () => {
     const graph = new Graph<Vertex>()
 
-    const v1 = new Vertex();
-    const v2 = new Vertex();
+    const v1 = new ValueCellVertex("1'");
+    const v2 = new ValueCellVertex("2");
     graph.addNode(v1)
     graph.addNode(v1)
     graph.addNode(v2)
