631e0f920b1bb0a056426c7f02fb7c9bb839e133
Jakub Kowalski <kowalski_jakub@hotmail.com>
2018-09-29 11:30:39 +0200

    Use AstNodeType enum in parser.specs.ts

--- test/parser.spec.ts
+++ test/parser.spec.ts
@@ -1,4 +1,5 @@
 import {Parser} from '../src/parser/parser'
+import {AstNodeType} from "../src/AstNodeType"
 
 
 describe('Basic parser test', () => {
@@ -6,28 +7,28 @@ describe('Basic parser test', () => {
         var parser = new Parser()
 
         // operators
-        expect(parser.parse("1").type).toBe("NUMBER")
-        expect(parser.parse("'foo'").type).toBe("STRING")
-        expect(parser.parse("1+2").type).toBe("PLUS_OP")
-        expect(parser.parse("1-2").type).toBe("MINUS_OP")
-        expect(parser.parse("1*2").type).toBe("TIMES_OP")
-        expect(parser.parse("1/2").type).toBe("DIV_OP")
-        expect(parser.parse("1^2").type).toBe("POW_OP")
-        expect(parser.parse("-2").type).toBe("NEGATIVE_OP")
-        expect(parser.parse("+2").type).toBe("POSITIVE_OP")
-        expect(parser.parse("1&2").type).toBe("AND_OP")
+        expect(parser.parse("1").type).toBe(AstNodeType.NUMBER)
+        expect(parser.parse("'foo'").type).toBe(AstNodeType.STRING)
+        expect(parser.parse("1+2").type).toBe(AstNodeType.PLUS_OP)
+        expect(parser.parse("1-2").type).toBe(AstNodeType.MINUS_OP)
+        expect(parser.parse("1*2").type).toBe(AstNodeType.TIMES_OP)
+        expect(parser.parse("1/2").type).toBe(AstNodeType.DIV_OP)
+        expect(parser.parse("1^2").type).toBe(AstNodeType.POW_OP)
+        expect(parser.parse("-2").type).toBe(AstNodeType.NEGATIVE_OP)
+        expect(parser.parse("+2").type).toBe(AstNodeType.POSITIVE_OP)
+        expect(parser.parse("1&2").type).toBe(AstNodeType.AND_OP)
 
         // cell addressing
-        expect(parser.parse("A5").type).toBe("RELATIVE_CELL")
-        expect(parser.parse("$A$5").type).toBe("ABSOLUTE_CELL")
-        expect(parser.parse("A$5").type).toBe("MIXED_CELL")
-        expect(parser.parse("$A5").type).toBe("MIXED_CELL")
+        expect(parser.parse("A5").type).toBe(AstNodeType.RELATIVE_CELL)
+        expect(parser.parse("$A$5").type).toBe(AstNodeType.ABSOLUTE_CELL)
+        expect(parser.parse("A$5").type).toBe(AstNodeType.MIXED_CELL)
+        expect(parser.parse("$A5").type).toBe(AstNodeType.MIXED_CELL)
 
         // range of cells
-        expect(parser.parse("A5:B5").type).toBe("CELL_RANGE")
-        expect(parser.parse("$A$5:B5").type).toBe("CELL_RANGE")
-        expect(parser.parse("A$5:B5").type).toBe("CELL_RANGE")
-        expect(parser.parse("$A5:B5").type).toBe("CELL_RANGE")
+        expect(parser.parse("A5:B5").type).toBe(AstNodeType.CELL_RANGE)
+        expect(parser.parse("$A$5:B5").type).toBe(AstNodeType.CELL_RANGE)
+        expect(parser.parse("A$5:B5").type).toBe(AstNodeType.CELL_RANGE)
+        expect(parser.parse("$A5:B5").type).toBe(AstNodeType.CELL_RANGE)
     })
 
     it("function calls without arguments", () => {
@@ -35,7 +36,7 @@ describe('Basic parser test', () => {
 
         const ast = parser.parse("SUM()")
 
-        expect(ast.type).toBe("FUNCTION_CALL")
+        expect(ast.type).toBe(AstNodeType.FUNCTION_CALL)
         expect(ast.args).toEqual(["SUM"])
     })
 
@@ -44,11 +45,11 @@ describe('Basic parser test', () => {
 
         const ast = parser.parse("SUM(1, 2)")
 
-        expect(ast.type).toBe("FUNCTION_CALL")
+        expect(ast.type).toBe(AstNodeType.FUNCTION_CALL)
         expect(ast.args[0]).toBe("SUM")
         expect(ast.args[1]).toEqual([
-            { type: "NUMBER", args: ["1"] },
-            { type: "NUMBER", args: ["2"] },
+            { type: AstNodeType.NUMBER, args: ["1"] },
+            { type: AstNodeType.NUMBER, args: ["2"] },
         ])
     })
 });
