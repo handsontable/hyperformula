eab3d88496323c398bbb4b74551fd17e86f11a68
Rafał Łasocha <me@swistak35.com>
2018-10-03 12:18:08 +0200

    Move ast building functions into FullParser
    Because FullParser is anaemic

--- src/parser/FullParser.ts
+++ src/parser/FullParser.ts
@@ -1,6 +1,6 @@
 import {Parser, RawAst} from './Parser'
-import {Ast} from './Ast'
-import {buildAst} from './AstBuilder'
+import {Ast, MinusOpAst, PlusOpAst, RelativeCellAst, NumberAst, Ast} from "./Ast";
+import {RawAstNodeType} from "./RawAstNodeType"
 
 export class FullParser {
   private parser: Parser
@@ -15,3 +15,23 @@ export class FullParser {
   }
 }
 
+function buildAst(rawAst: RawAst) : Ast {
+  switch (rawAst.type) {
+    case RawAstNodeType.RELATIVE_CELL:
+      return new RelativeCellAst(rawAst.args[0] as string)
+    case RawAstNodeType.PLUS_OP:
+      return new PlusOpAst(buildAst(rawAst.args[0] as RawAst), buildAst(rawAst.args[1] as RawAst))
+    case RawAstNodeType.MINUS_OP:
+      return new MinusOpAst(buildAst(rawAst.args[0] as RawAst), processArgs(rawAst.args as Array<RawAst>))
+    case RawAstNodeType.NUMBER:
+      return new NumberAst(rawAst.args[0] as string)
+    default:
+      throw Error("Unsupported AST node type")
+  }
+}
+
+function processArgs(args : Array<RawAst>) : Array<Ast> {
+  return args.map((value) => {
+    return buildAst(value as RawAst)
+  })
+};
