64c39284d04c35cbfb8422f1d0ebd8277e6ef5c0
Rafał Łasocha <me@swistak35.com>
2018-10-03 11:50:26 +0200

    Fix the test after rename

--- test/parser.spec.ts
+++ test/parser.spec.ts
@@ -1,59 +1,59 @@
 import {FullParser} from '../src/parser/FullParser'
-import {AstNodeType} from "../src/AstNodeType"
+import {RawAstNodeType} from "../src/parser/RawAstNodeType"
 
 describe('Basic parser test', () => {
     const parser = new FullParser()
 
     it('#parse produces simple node with correct type', () => {
         // operators
-        expect(parser.parse("1").type).toBe(AstNodeType.NUMBER)
-        expect(parser.parse("'foo'").type).toBe(AstNodeType.STRING)
-        expect(parser.parse("1-2").type).toBe(AstNodeType.MINUS_OP)
-        expect(parser.parse("1*2").type).toBe(AstNodeType.TIMES_OP)
-        expect(parser.parse("1/2").type).toBe(AstNodeType.DIV_OP)
-        expect(parser.parse("1^2").type).toBe(AstNodeType.POW_OP)
-        expect(parser.parse("-2").type).toBe(AstNodeType.NEGATIVE_OP)
-        expect(parser.parse("+2").type).toBe(AstNodeType.POSITIVE_OP)
-        expect(parser.parse("1&2").type).toBe(AstNodeType.AND_OP)
+        expect(parser.parse("1").type).toBe(RawAstNodeType.NUMBER)
+        expect(parser.parse("'foo'").type).toBe(RawAstNodeType.STRING)
+        expect(parser.parse("1-2").type).toBe(RawAstNodeType.MINUS_OP)
+        expect(parser.parse("1*2").type).toBe(RawAstNodeType.TIMES_OP)
+        expect(parser.parse("1/2").type).toBe(RawAstNodeType.DIV_OP)
+        expect(parser.parse("1^2").type).toBe(RawAstNodeType.POW_OP)
+        expect(parser.parse("-2").type).toBe(RawAstNodeType.NEGATIVE_OP)
+        expect(parser.parse("+2").type).toBe(RawAstNodeType.POSITIVE_OP)
+        expect(parser.parse("1&2").type).toBe(RawAstNodeType.AND_OP)
 
         // cell addressing
-        expect(parser.parse("A5").type).toBe(AstNodeType.RELATIVE_CELL)
-        expect(parser.parse("$A$5").type).toBe(AstNodeType.ABSOLUTE_CELL)
-        expect(parser.parse("A$5").type).toBe(AstNodeType.MIXED_CELL)
-        expect(parser.parse("$A5").type).toBe(AstNodeType.MIXED_CELL)
+        expect(parser.parse("A5").type).toBe(RawAstNodeType.RELATIVE_CELL)
+        expect(parser.parse("$A$5").type).toBe(RawAstNodeType.ABSOLUTE_CELL)
+        expect(parser.parse("A$5").type).toBe(RawAstNodeType.MIXED_CELL)
+        expect(parser.parse("$A5").type).toBe(RawAstNodeType.MIXED_CELL)
 
         // range of cells
-        expect(parser.parse("A5:B5").type).toBe(AstNodeType.CELL_RANGE)
-        expect(parser.parse("$A$5:B5").type).toBe(AstNodeType.CELL_RANGE)
-        expect(parser.parse("A$5:B5").type).toBe(AstNodeType.CELL_RANGE)
-        expect(parser.parse("$A5:B5").type).toBe(AstNodeType.CELL_RANGE)
+        expect(parser.parse("A5:B5").type).toBe(RawAstNodeType.CELL_RANGE)
+        expect(parser.parse("$A$5:B5").type).toBe(RawAstNodeType.CELL_RANGE)
+        expect(parser.parse("A$5:B5").type).toBe(RawAstNodeType.CELL_RANGE)
+        expect(parser.parse("$A5:B5").type).toBe(RawAstNodeType.CELL_RANGE)
     })
 
     it("plus operator on literals", () => {
         const ast = parser.parse("1+2")
 
-        expect(ast.type).toBe(AstNodeType.PLUS_OP)
+        expect(ast.type).toBe(RawAstNodeType.PLUS_OP)
         expect(ast.args).toEqual([
-            { type: AstNodeType.NUMBER, args: ["1"] },
-            { type: AstNodeType.NUMBER, args: ["2"] },
+            { type: RawAstNodeType.NUMBER, args: ["1"] },
+            { type: RawAstNodeType.NUMBER, args: ["2"] },
         ])
     })
 
     it("function calls without arguments", () => {
         const ast = parser.parse("SUM()")
 
-        expect(ast.type).toBe(AstNodeType.FUNCTION_CALL)
+        expect(ast.type).toBe(RawAstNodeType.FUNCTION_CALL)
         expect(ast.args).toEqual(["SUM"])
     })
 
     it("function calls with arguments", () => {
         const ast = parser.parse("SUM(1, 2)")
 
-        expect(ast.type).toBe(AstNodeType.FUNCTION_CALL)
+        expect(ast.type).toBe(RawAstNodeType.FUNCTION_CALL)
         expect(ast.args[0]).toBe("SUM")
         expect(ast.args[1]).toEqual([
-            { type: AstNodeType.NUMBER, args: ["1"] },
-            { type: AstNodeType.NUMBER, args: ["2"] },
+            { type: RawAstNodeType.NUMBER, args: ["1"] },
+            { type: RawAstNodeType.NUMBER, args: ["2"] },
         ])
     })
 });
