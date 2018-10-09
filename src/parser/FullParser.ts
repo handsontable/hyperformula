da6c111428353d19f8cd798e4a9a74142f124e6e
Rafał Łasocha <me@swistak35.com>
2018-10-09 23:50:21 +0200

    Remove old parser

--- src/parser/FullParser.ts
+++ src/parser/FullParser.ts
@@ -1,41 +1,9 @@
-import {Parser, RawAst} from './Parser'
-import {
-  Ast,
-  buildNumberAst,
-  buildPlusOpAst,
-  buildMinusOpAst,
-  buildTimesOpAst,
-  buildRelativeCellAst,
-  AstNodeType
-} from "./Ast";
+import { Ast } from "./Ast";
 import {parseFormula} from "../chevro";
 
 export class FullParser {
-  private parser: Parser
-
-  constructor() {
-    this.parser = new Parser()
-  }
-
   parse(text: string): Ast {
     return parseFormula(text)
-  }
-}
-
-function buildAst(rawAst: RawAst): Ast {
-  switch (rawAst.type) {
-    case AstNodeType.RELATIVE_CELL:
-      return buildRelativeCellAst(rawAst.args[0] as string)
-    case AstNodeType.PLUS_OP:
-      return buildPlusOpAst(buildAst(rawAst.args[0] as RawAst), buildAst(rawAst.args[1] as RawAst))
-    case AstNodeType.MINUS_OP:
-      return buildMinusOpAst(buildAst(rawAst.args[0] as RawAst), buildAst(rawAst.args[1] as RawAst))
-    case AstNodeType.TIMES_OP:
-      return buildTimesOpAst(buildAst(rawAst.args[0] as RawAst), buildAst(rawAst.args[1] as RawAst))
-    case AstNodeType.NUMBER:
-      return buildNumberAst(rawAst.args[0] as number)
-    default:
-      throw Error("Unsupported AST node type")
   }
 }
 
