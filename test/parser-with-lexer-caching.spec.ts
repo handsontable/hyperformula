402de3a04d4105d379b3b764c09feb0b0cecc6e8
Rafał Łasocha <me@swistak35.com>
2018-10-13 14:22:58 +0200

    Share parser specs for both optimization modes

--- test/parser-with-lexer-caching.spec.ts
+++ test/parser-with-lexer-caching.spec.ts
@@ -2,9 +2,9 @@ import {ParserWithCaching} from '../src/parser/ParserWithCaching'
 import { tokenizeFormula } from '../src/parser/FormulaParser'
 import {AstNodeType, NumberAst, PlusOpAst, MinusOpAst, TimesOpAst, CellReferenceAst} from '../src/parser/Ast'
 
-describe('ParserWithCaching', () => {
+const sharedExamples = (optimizationMode: string) => {
   it("integer literal", () => {
-    const parser = new ParserWithCaching('lexer')
+    const parser = new ParserWithCaching(optimizationMode)
 
     const bast = parser.parse("=42")
 
@@ -15,7 +15,7 @@ describe('ParserWithCaching', () => {
   })
 
   it("plus operator on different nodes", () => {
-    const parser = new ParserWithCaching('lexer')
+    const parser = new ParserWithCaching(optimizationMode)
 
     const bast = parser.parse("=1+A5")
 
@@ -27,7 +27,7 @@ describe('ParserWithCaching', () => {
   })
 
   it("plus operator on different nodes with more addresses", () => {
-    const parser = new ParserWithCaching('lexer')
+    const parser = new ParserWithCaching(optimizationMode)
 
     const bast = parser.parse("=A6+A5")
 
@@ -39,7 +39,7 @@ describe('ParserWithCaching', () => {
   })
 
   it("it use cache for similar formulas", () => {
-    const parser = new ParserWithCaching('lexer')
+    const parser = new ParserWithCaching(optimizationMode)
 
     const bast1 = parser.parse("=A1")
     const bast2 = parser.parse("=A2")
@@ -49,11 +49,19 @@ describe('ParserWithCaching', () => {
   })
 
   it("doesn't count cache for different formulas", () => {
-    const parser = new ParserWithCaching('lexer')
+    const parser = new ParserWithCaching(optimizationMode)
 
     const bast1 = parser.parse("=A1")
     const bast2 = parser.parse("=A2+A3")
 
     expect(parser.statsCacheUsed).toBe(0)
   })
+};
+
+describe('ParserWithCaching - parser optimizations', () => {
+  sharedExamples('parser')
+});
+
+describe('ParserWithCaching - lexer optimizations', () => {
+  sharedExamples('lexer')
 });
