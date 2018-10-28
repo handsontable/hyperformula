50fe088c0bfe76cc3f6a4e900a52a4fff74bbb99
Rafał Łasocha <me@swistak35.com>
2018-10-28 20:47:13 +0100

    Make sheet A use absolute addresses

--- benchmark/03-sheet-a.ts
+++ benchmark/03-sheet-a.ts
@@ -3,9 +3,9 @@ import {benchmark} from "./benchmark";
 const rows = 100000
 
 const X = [
-  [0.5, 0.1, 0.4],
-  [0.2, 0.7, 0.1],
-  [0.3, 0.3, 0.4]
+  ["0.5", "0.1", "0.4"],
+  ["0.2", "0.7", "0.1"],
+  ["0.3", "0.3", "0.4"]
 ]
 
 let sheet = []
@@ -14,12 +14,17 @@ sheet.push(['0', '1', '0', '0'])
 let prev = 1
 
 while (prev < rows) {
-  sheet.push([
+  const rowToPush = [
     `${prev}`,
-    `=B${prev}*${X[0][0]} + C${prev}*${X[1][0]} + D${prev}*${X[2][0]}`,
-    `=B${prev}*${X[0][1]} + C${prev}*${X[1][1]} + D${prev}*${X[2][1]}`,
-    `=B${prev}*${X[0][2]} + C${prev}*${X[1][2]} + D${prev}*${X[2][2]}`
-  ])
+    `=B${prev}*$E$2 + C${prev}*$E$3 + D${prev}*$E$4`,
+    `=B${prev}*$F$2 + C${prev}*$F$3 + D${prev}*$F$4`,
+    `=B${prev}*$G$2 + C${prev}*$G$3 + D${prev}*$G$4`
+  ]
+  if (prev <= X.length) {
+    rowToPush.concat(X[prev - 1])
+  }
+
+  sheet.push(rowToPush)
   ++prev
 }
 
