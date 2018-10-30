77d690d5eb298ab6623e2ea1ce576f0b78e7295e
Jakub Kowalski <kowalski_jakub@hotmail.com>
2018-10-30 17:27:46 +0100

    reversed address mapping

--- src/AddressMapping.ts
+++ src/AddressMapping.ts
@@ -3,6 +3,7 @@ import {CellAddress} from "./Cell";
 
 export class AddressMapping {
   private mapping: Map<number, Map<number, CellVertex>> = new Map()
+  private reversedMapping: Map<CellVertex, CellAddress> = new Map()
 
   constructor() {
   }
@@ -15,6 +16,14 @@ export class AddressMapping {
     return colMapping.get(address.row) || null
   }
 
+  getAddress(vertex: CellVertex): CellAddress | null {
+    const address = this.reversedMapping.get(vertex)
+    if (!address) {
+      return null
+    }
+    return address
+  }
+
   setCell(address: CellAddress, newVertex: CellVertex) {
     let colMapping = this.mapping.get(address.col)
     if (!colMapping) {
@@ -22,6 +31,7 @@ export class AddressMapping {
       this.mapping.set(address.col, colMapping)
     }
     colMapping.set(address.row, newVertex)
+    this.reversedMapping.set(newVertex, address)
   }
 
   has(address: CellAddress): boolean {
