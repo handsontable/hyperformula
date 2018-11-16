e3ec1b2a56030cf238bd0c20aa667cefbcfc5ee4
Rafał Łasocha <me@swistak35.com>
2018-11-16 20:38:29 +0100

    Fix linter

--- test/criterion.spec.ts
+++ test/criterion.spec.ts
@@ -1,11 +1,11 @@
-import {parseCriterion, buildCriterion, CriterionType} from '../src/interpreter/Criterion'
+import {buildCriterion, CriterionType, parseCriterion} from '../src/interpreter/Criterion'
 
 describe('Criterion', () => {
   it('greater than', () => {
-    expect(parseCriterion(">0")).toEqual(buildCriterion(CriterionType.GREATER_THAN, 0))
+    expect(parseCriterion('>0')).toEqual(buildCriterion(CriterionType.GREATER_THAN, 0))
   })
 
   it('greater or equal than', () => {
-    expect(parseCriterion(">=0")).toEqual(buildCriterion(CriterionType.GREATER_THAN_OR_EQUAL, 0))
+    expect(parseCriterion('>=0')).toEqual(buildCriterion(CriterionType.GREATER_THAN_OR_EQUAL, 0))
   })
 })
