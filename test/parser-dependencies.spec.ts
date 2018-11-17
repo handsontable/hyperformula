78ed85b85abf9609b431627fac56a54b69ee675f
Rafał Łasocha <me@swistak35.com>
2018-11-17 15:29:52 +0100

    Add spec for testing dependencies we get from parser

---
+++ test/parser-dependencies.spec.ts
@@ -0,0 +1,14 @@
+import {absoluteCellAddress, simpleCellAddress} from '../src/Cell'
+import {ParserWithCaching} from '../src/parser/ParserWithCaching'
+
+describe('Parsing collecting dependencies', () => {
+  it('works for CELL_REFERENCE', () => {
+    const parser = new ParserWithCaching('parser')
+
+    const dependencies = parser.parse('=B2', absoluteCellAddress(0, 0)).dependencies
+
+    expect(dependencies).toEqual([
+      simpleCellAddress(1, 1),
+    ])
+  })
+})
