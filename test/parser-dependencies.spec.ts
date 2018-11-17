e917c5ca069c5cdf097563e6894ee6d570499fe6
Rafał Łasocha <me@swistak35.com>
2018-11-17 15:31:57 +0100

    Add spec for cell range

--- test/parser-dependencies.spec.ts
+++ test/parser-dependencies.spec.ts
@@ -11,4 +11,14 @@ describe('Parsing collecting dependencies', () => {
       simpleCellAddress(1, 1),
     ])
   })
+
+  it('works for CELL_RANGE', () => {
+    const parser = new ParserWithCaching('parser')
+
+    const dependencies = parser.parse('=B2:C4', absoluteCellAddress(0, 0)).dependencies
+
+    expect(dependencies).toEqual([
+      [simpleCellAddress(1, 1), simpleCellAddress(2, 3)]
+    ])
+  })
 })
