97ea95b664313a86fd135a681d6c83ce05353745
Jakub Kowalski <kowalski_jakub@hotmail.com>
2018-11-23 15:03:53 +0100

    Sheet T validation

--- benchmark/05-sheet-t.ts
+++ benchmark/05-sheet-t.ts
@@ -1,22 +1,28 @@
-import {benchmark} from './benchmark'
+import {benchmark, ExpectedValue} from './benchmark'
 
 const rows = 10000
 
 const sheet = []
 
-let prev = 0
+let prev = 1
 
 const prettyRandomString = (chars: number) => [...Array(chars)].map((i) => (~~(Math.random() * 36)).toString(36)).join('')
 
-while (prev < rows) {
+while (prev <= rows) {
   const rowToPush = [
     prettyRandomString(30),
     prettyRandomString(30),
-    `=CONCATENATE(A${prev}, B${prev})`,
+    `=CONCATENATE(A${prev}; B${prev})`,
   ]
 
   sheet.push(rowToPush)
   ++prev
 }
 
-benchmark(sheet, [], { millisecondsPerThousandRows: 25, numberOfRuns: 3 })
+const expectedValues: ExpectedValue[] = [
+  { address: 'C1', value: `${sheet[0][0]}${sheet[0][1]}` },
+  { address: 'C1000', value: `${sheet[999][0]}${sheet[999][1]}` },
+  { address: 'C10000', value: `${sheet[9999][0]}${sheet[9999][1]}` },
+]
+
+benchmark(sheet, expectedValues, { millisecondsPerThousandRows: 25, numberOfRuns: 3 })
