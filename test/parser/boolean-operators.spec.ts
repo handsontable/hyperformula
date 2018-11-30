07d54a4521f6ea883cd4bc1fe58cfcf4361c66a2
Jakub Kowalski <kowalski_jakub@hotmail.com>
2018-11-30 11:52:46 +0100

    boolean operator precedence test

--- test/parser/boolean-operators.spec.ts
+++ test/parser/boolean-operators.spec.ts
@@ -1,12 +1,19 @@
 import {absoluteCellAddress, relativeCellAddress} from '../../src/Cell'
-import {AstNodeType, CellReferenceAst} from '../../src/parser/Ast'
+import {
+  AstNodeType,
+  BinaryOpAst,
+  CellReferenceAst,
+  EqualsOpAst,
+  GreaterThanOpAst,
+  LessThanOpAst
+} from '../../src/parser/Ast'
 import {ParserWithCaching} from '../../src/parser/ParserWithCaching'
 
 describe('Parser - Boolean operators', () => {
   it('Equals operator', () => {
     const parser = new ParserWithCaching('parser')
 
-    const ast = parser.parse('=1=2', absoluteCellAddress(0, 0)).ast as CellReferenceAst
+    const ast = parser.parse('=1=2', absoluteCellAddress(0, 0)).ast as EqualsOpAst
 
     expect(ast.type).toBe(AstNodeType.EQUALS_OP)
   })
@@ -14,7 +21,7 @@ describe('Parser - Boolean operators', () => {
   it('Greater than operator', () => {
     const parser = new ParserWithCaching('parser')
 
-    const ast = parser.parse('=1>2', absoluteCellAddress(0, 0)).ast as CellReferenceAst
+    const ast = parser.parse('=1>2', absoluteCellAddress(0, 0)).ast as GreaterThanOpAst
 
     expect(ast.type).toBe(AstNodeType.GREATER_THAN_OP)
   })
@@ -22,8 +29,18 @@ describe('Parser - Boolean operators', () => {
   it('Less than operator', () => {
     const parser = new ParserWithCaching('parser')
 
-    const ast = parser.parse('=1<2', absoluteCellAddress(0, 0)).ast as CellReferenceAst
+    const ast = parser.parse('=1<2', absoluteCellAddress(0, 0)).ast as LessThanOpAst
 
     expect(ast.type).toBe(AstNodeType.LESS_THAN_OP)
+  })
+
+  it('Boolean operator with more complex childs', () => {
+    const parser = new ParserWithCaching('parser')
+
+    const ast = parser.parse('=1+2=1+2*6', absoluteCellAddress(0, 0)).ast as EqualsOpAst
+
+    expect(ast.type).toBe(AstNodeType.EQUALS_OP)
+    expect(ast.left.type).toBe(AstNodeType.PLUS_OP)
+    expect(ast.right.type).toBe(AstNodeType.PLUS_OP)
   })
 })
