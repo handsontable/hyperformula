b311f78be77f3a86d0918a603503c546c260777f
Rafał Łasocha <me@swistak35.com>
2018-11-30 11:59:53 +0100

    Extract SPLIT function specs to different file

--- test/interpreter.spec.ts
+++ test/interpreter.spec.ts
@@ -552,35 +552,4 @@ describe('Interpreter', () => {
     expect(engine.getCellValue('A1')).toEqual(cellError(ErrorType.REF))
     expect(engine.getCellValue('B1')).toEqual(cellError(ErrorType.REF))
   })
-
-  it('function SPLIT happy path', () => {
-    engine.loadSheet([['some words', '=SPLIT(A1; 0)']])
-
-    expect(engine.getCellValue('B1')).toEqual('some')
-  })
-
-  it('function SPLIT bigger index', () => {
-    engine.loadSheet([['some words', '=SPLIT(A1; 1)']])
-
-    expect(engine.getCellValue('B1')).toEqual('words')
-  })
-
-  it('function SPLIT when 1st arg not a string', () => {
-    engine.loadSheet([['42', '=SPLIT(A1; 1)']])
-
-    expect(engine.getCellValue('B1')).toEqual(cellError(ErrorType.VALUE))
-  })
-
-  it('function SPLIT when 2nd arg not a number', () => {
-    engine.loadSheet([['some words', '=SPLIT(A1; "foo")']])
-
-    expect(engine.getCellValue('B1')).toEqual(cellError(ErrorType.VALUE))
-  })
-
-  it('function SPLIT when index arg is not value within bounds', () => {
-    engine.loadSheet([['some words', '=SPLIT(A1; 17)', '=SPLIT(A1; -1)']])
-
-    expect(engine.getCellValue('B1')).toEqual(cellError(ErrorType.VALUE))
-    expect(engine.getCellValue('C1')).toEqual(cellError(ErrorType.VALUE))
-  })
 })
