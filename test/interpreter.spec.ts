0bb35b423602ae981e76ae06d88ae7cfe8cd599a
Bartek Dudek <bardek.dudek@gmail.com>
2018-12-18 20:46:13 +0100

    specification for ISBLANK

--- test/interpreter.spec.ts
+++ test/interpreter.spec.ts
@@ -430,6 +430,32 @@ describe('Interpreter', () => {
     expect(engine.getCellValue('B1')).toEqual(cellError(ErrorType.NA))
   })
 
+  it.skip('function ISBLANK should return true for references to empty cells', () => {
+    const engine = HandsOnEngine.buildFromArray([
+        ['','=ISBLANK(A1)','=ISBLANK($A1)','=ISBLANK(OFFSET(C1,0,-2))'],
+    ])
+    expect(engine.getCellValue('B1')).toEqual(true)
+    expect(engine.getCellValue('C1')).toEqual(true)
+  })
+
+  it.skip('function ISERROR should return false if it is not reference to empty cell', () => {
+    const engine = HandsOnEngine.buildFromArray([
+        ['','=A1','=ISBLANK(B1)','=ISBLANK("")','=ISBLANK(4)','=ISBLANK(CONCATENATE(A1,A1))'],
+    ])
+    expect(engine.getCellValue('C1')).toEqual(false)
+    expect(engine.getCellValue('D1')).toEqual(false)
+    expect(engine.getCellValue('E1')).toEqual(false)
+    expect(engine.getCellValue('F1')).toEqual(false)
+  })
+
+  it.skip('function ISBLANK takes exactly one argument', () => {
+    const engine = HandsOnEngine.buildFromArray([
+        ['=ISBLANK(A3, A2)', '=ISBLANK()'],
+    ])
+    expect(engine.getCellValue('A1')).toEqual(cellError(ErrorType.NA))
+    expect(engine.getCellValue('B1')).toEqual(cellError(ErrorType.NA))
+  })
+
   it('function AND usage', () => {
     const engine = HandsOnEngine.buildFromArray([
         ['=AND(TRUE(), TRUE())', '=AND(TRUE(), FALSE())', '=AND(TRUE(), "asdf")'],
