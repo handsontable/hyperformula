cf32d35a1ea13a543cb3235b13e72e74f8b36394
Rafał Łasocha <me@swistak35.com>
2018-12-20 00:55:34 +0100

    Initial version of the diff tool

---
+++ bin/handsonengine-diff.ts
@@ -0,0 +1,57 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import parse from 'csv-parse/lib/sync'
+import {findBoundaries} from '../src'
+
+// Config
+const CSV_DELIMITER = ","
+const FLOAT_ROUND = 10
+
+if (process.argv.length < 4) {
+  console.warn('Usage:\nyarn ts-node bin/handsonengine-diff expected.csv ours.csv 1,2,3,4\nLast argument is optional and represents indexes of columns to ignore. Zero-based, comma-separated, no spaces.')
+  process.exit(1)
+}
+const expectedCsvPath = path.resolve(process.cwd(), process.argv[2])
+if (!fs.existsSync(expectedCsvPath)) {
+  console.warn(`File ${expectedCsvPath} does not exist.`)
+  process.exit(1)
+}
+
+const actualCsvPath = path.resolve(process.cwd(), process.argv[3])
+if (!fs.existsSync(actualCsvPath)) {
+  console.warn(`File ${actualCsvPath} does not exist.`)
+  process.exit(1)
+}
+
+const expectedCsvString = fs.readFileSync(expectedCsvPath, { encoding: 'utf8' })
+const actualCsvString = fs.readFileSync(actualCsvPath, { encoding: 'utf8' })
+
+const expectedArray = parse(expectedCsvString, { delimiter: CSV_DELIMITER })
+const actualArray = parse(actualCsvString, { delimiter: CSV_DELIMITER })
+
+// errors on not matching size
+const expectedArrayBoundaries = findBoundaries(expectedArray)
+const actualArrayBoundaries = findBoundaries(actualArray)
+if (expectedArrayBoundaries.height !== actualArrayBoundaries.height || expectedArrayBoundaries.width !== actualArrayBoundaries.width) {
+  console.warn(`Different sheet boundaries.`)
+  console.warn(`Expected sheet: width=${expectedArrayBoundaries.width}, height=${expectedArrayBoundaries.height}`)
+  console.warn(`Actual sheet: width=${actualArrayBoundaries.width}, height=${actualArrayBoundaries.height}`)
+  process.exit(1)
+}
+const {width, height} = expectedArrayBoundaries
+
+const differences: Array<[number, number, string, string]> = []
+for(let currentRowIdx = 0; currentRowIdx < height; currentRowIdx++) {
+  for(let currentColumnIdx = 0; currentColumnIdx < width; currentColumnIdx++) {
+    const expectedValue = expectedArray[currentRowIdx][currentColumnIdx]
+    const actualValue = actualArray[currentRowIdx][currentColumnIdx]
+    if (expectedValue !== actualValue) {
+      differences.push([currentRowIdx, currentColumnIdx, expectedValue, actualValue])
+    }
+  }
+}
+
+differences.forEach((e: [number, number, string, string]) => {
+  const [currentRowIdx, currentColumnIdx, expectedValue, actualValue] = e;
+  console.warn(`In cell R${currentRowIdx}C${currentColumnIdx} expected '${expectedValue}' but got '${actualValue}'`)
+})
