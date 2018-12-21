0ce0760068e5eaf1535ab2b7b7486ca08b89bd64
Jakub Kowalski <kowalski_jakub@hotmail.com>
2018-12-21 09:58:04 +0100

    move converter params check to top

--- bin/handsonengine-convert.ts
+++ bin/handsonengine-convert.ts
@@ -2,11 +2,12 @@ import * as fs from 'fs'
 import * as path from 'path'
 import {HandsOnEngine} from '../src'
 
-const formulasCsvPath = path.resolve(process.cwd(), process.argv[2])
 if (process.argv.length < 4) {
   console.warn('Usage:\nyarn ts-node bin/handsonengine-convert formulas.csv output.csv')
   process.exit(1)
 }
+
+const formulasCsvPath = path.resolve(process.cwd(), process.argv[2])
 const outputCsvPath = path.resolve(process.cwd(), process.argv[3])
 
 if (!fs.existsSync(formulasCsvPath)) {
