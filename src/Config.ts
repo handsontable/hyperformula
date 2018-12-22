10cab076a88120dfc9966fd7babf542298e1121a
Rafał Łasocha <me@swistak35.com>
2018-12-22 13:18:47 +0100

    Make Config instantiable

--- src/Config.ts
+++ src/Config.ts
@@ -7,11 +7,29 @@ export class Config {
   public static readonly LANGUAGE = 'EN'
   public static readonly DATE_FORMAT = 'MM/DD/YYYY'
 
+  public readonly addressMappingFillThreshold: number
+  public readonly csvDelimiter: string
+  public readonly dateFormat: string
+  public readonly functionArgSeparator: string
+  public readonly language: string
+
   constructor({
-    public readonly csvDelimiter,
-    public readonly functionArgSeparator,
-    public readonly addressMappingFillThreshold,
-    public readonly language,
-    public readonly dateFormat,
-  })
+    addressMappingFillThreshold,
+    csvDelimiter,
+    dateFormat,
+    functionArgSeparator,
+    language,
+  }: {
+    addressMappingFillThreshold?: number,
+    csvDelimiter?: string,
+    dateFormat?: string,
+    functionArgSeparator?: string,
+    language?: string,
+  } = {}) {
+    this.addressMappingFillThreshold = addressMappingFillThreshold || Config.ADDRESS_MAPPING_FILL_THRESHOLD
+    this.csvDelimiter = csvDelimiter || Config.CSV_DELIMITER
+    this.dateFormat = dateFormat || Config.DATE_FORMAT
+    this.functionArgSeparator = functionArgSeparator || Config.FUNCTION_ARG_SEPARATOR
+    this.language = language || Config.LANGUAGE
+  }
 }
---
+++ test/config.spec.ts
@@ -0,0 +1,15 @@
+import {Config} from '../src/Config'
+
+describe('Config', () => {
+  it('works', () => {
+    const config = new Config({language: 'PL'})
+
+    expect(config.language).toBe('PL')
+  })
+
+  it('has some defaults', () => {
+    const config = new Config()
+
+    expect(config.language).toBe(Config.LANGUAGE)
+  })
+})
