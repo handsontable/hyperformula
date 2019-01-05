fc067be1a3b27073338f834a2dfe60b03b9d4e9f
Jakub Kowalski <kowalski_jakub@hotmail.com>
2019-01-05 11:31:51 +0100

    Boolean functions doccomments

--- src/interpreter/plugin/BooleanPlugin.ts
+++ src/interpreter/plugin/BooleanPlugin.ts
@@ -27,6 +27,14 @@ export class BooleanPlugin extends FunctionPlugin {
     },
   }
 
+  /**
+   * Corresponds to TRUE()
+   *
+   * Returns the logical true
+   *
+   * @param ast
+   * @param formulaAddress
+   */
   public true(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
     if (ast.args.length > 0) {
       return cellError(ErrorType.NA)
@@ -35,6 +43,14 @@ export class BooleanPlugin extends FunctionPlugin {
     }
   }
 
+  /**
+   * Corresponds to FALSE()
+   *
+   * Returns the logical false
+   *
+   * @param ast
+   * @param formulaAddress
+   */
   public false(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
     if (ast.args.length > 0) {
       return cellError(ErrorType.NA)
@@ -43,6 +59,14 @@ export class BooleanPlugin extends FunctionPlugin {
     }
   }
 
+  /**
+   * Corresponds to IF(expression, value_if_true, value_if_false)
+   *
+   * Returns value specified as second argument if expression is true and third argument if expression is false
+   *
+   * @param ast
+   * @param formulaAddress
+   */
   public if(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
     const condition = booleanRepresentation(this.evaluateAst(ast.args[0], formulaAddress))
     if (condition === true) {
@@ -58,6 +82,14 @@ export class BooleanPlugin extends FunctionPlugin {
     }
   }
 
+  /**
+   * Corresponds to AND(expression1, [expression2, ...])
+   *
+   * Returns true if all of the provided arguments are logically true, and false if any of it is logically false
+   *
+   * @param ast
+   * @param formulaAddress
+   */
   public and(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
     if (ast.args.length < 1) {
       return cellError(ErrorType.NA)
@@ -73,6 +105,14 @@ export class BooleanPlugin extends FunctionPlugin {
     return result
   }
 
+  /**
+   * Corresponds to OR(expression1, [expression2, ...])
+   *
+   * Returns true if any of the provided arguments are logically true, and false otherwise
+   *
+   * @param ast
+   * @param formulaAddress
+   */
   public or(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
     if (ast.args.length < 1) {
       return cellError(ErrorType.NA)
