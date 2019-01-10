654aaaebb3a315e974ee6e3664ef4c3718c55aee
Rafał Łasocha <me@swistak35.com>
2019-01-10 22:34:26 +0100

    Extract COUNTIF tests to separate file

--- test/interpreter.spec.ts
+++ test/interpreter.spec.ts
@@ -374,41 +374,6 @@ describe('Interpreter', () => {
     expect(engine.getCellValue('C1')).toEqual(cellError(ErrorType.DIV_BY_ZERO))
   })
 
-  it('function COUNTIF usage', () => {
-    const engine = HandsOnEngine.buildFromArray([
-      ['0'],
-      ['1'],
-      ['2'],
-      ['=COUNTIF(A1:A3, ">=1")'],
-    ])
-
-    expect(engine.getCellValue('A4')).toEqual(2)
-  })
-
-  it('function COUNTIF error when 1st arg is not a range', () => {
-    const engine = HandsOnEngine.buildFromArray([
-      ['=COUNTIF(42, ">0")'],
-    ])
-
-    expect(engine.getCellValue('A1')).toEqual(cellError(ErrorType.VALUE))
-  })
-
-  it('function COUNTIF error when 2nd arg is not a string', () => {
-    const engine = HandsOnEngine.buildFromArray([
-      ['=COUNTIF(C1:C2, 78)'],
-    ])
-
-    expect(engine.getCellValue('A1')).toEqual(cellError(ErrorType.VALUE))
-  })
-
-  it('function COUNTIF error when criterion unparsable', () => {
-    const engine = HandsOnEngine.buildFromArray([
-      ['=COUNTIF(B1:B2, "%")'],
-    ])
-
-    expect(engine.getCellValue('A1')).toEqual(cellError(ErrorType.VALUE))
-  })
-
   it('function ISERROR should return true for common errors', () => {
     const engine = HandsOnEngine.buildFromArray([
         ['=ISERROR(1/0)', '=ISERROR(FOO())', '=ISERROR(TRUE(1))'],
