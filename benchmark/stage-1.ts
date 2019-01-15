74588a464e5d1fde2c95ae8d9b8bf2ce7c16fd98
Jakub Kowalski <kowalski_jakub@hotmail.com>
2019-01-15 09:14:01 +0100

    Changed number of runs for stage 1 benchmark

--- benchmark/stage-1.ts
+++ benchmark/stage-1.ts
@@ -4,8 +4,8 @@ import {sheet as B} from './sheets/04-sheet-b'
 import {sheet as T} from './sheets/05-sheet-t'
 
 console.info('Sheet A')
-benchmark(A(), [], { millisecondsPerThousandRows: 60, numberOfRuns: 3 })
+benchmark(A(), [], { millisecondsPerThousandRows: 60, numberOfRuns: 100 })
 console.info('\nSheet B')
-benchmark(B(), [], { millisecondsPerThousandRows: 70, numberOfRuns: 3 })
+benchmark(B(), [], { millisecondsPerThousandRows: 70, numberOfRuns: 100 })
 console.info('\nSheet T')
-benchmark(T(), [], { millisecondsPerThousandRows: 25, numberOfRuns: 3 })
+benchmark(T(), [], { millisecondsPerThousandRows: 25, numberOfRuns: 100 })
