4aabedb0cbe92a117fc181abe5f0aae38f8c4627
Rafał Łasocha <me@swistak35.com>
2019-01-17 16:09:25 +0100

    Add script to check whether compiled bundle works correctly

---
+++ script/check-minified.js
@@ -0,0 +1,12 @@
+var HandsOnEngine = require("../dist/minified-full.js").HandsOnEngine
+var assert = require('assert')
+
+var engine = HandsOnEngine.buildFromArray([
+  ['42', '=A1 + 2']
+])
+var valueA1 = engine.getCellValue('A1')
+var valueB1 = engine.getCellValue('B1')
+
+assert(valueA1 == 42)
+assert(valueB1 == 44)
+console.log("OK")
