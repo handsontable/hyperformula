95ece86334472453b4a1a6b78bd751e7275b0a9c
Rafał Łasocha <me@swistak35.com>
2019-01-17 16:37:29 +0100

    Make variety of different bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,59 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    filename: 'minified-full.js',
-    library: "HandsOnEngine",
-    libraryTarget: "umd",
-    path: path.resolve(__dirname, 'lib'),
+const buildConfiguration = ({ name, mode, excludeDependencies }) => {
+  const configuration = {
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+      filename: `${name}.js`,
+      library: "HandsOnEngine",
+      libraryTarget: "umd",
+      path: path.resolve(__dirname, 'lib'),
 
-    // https://github.com/webpack/webpack/issues/6784
-    globalObject: 'typeof self !== \'undefined\' ? self : this',
-  },
-  mode: 'development'
+      // https://github.com/webpack/webpack/issues/6784
+      globalObject: 'typeof self !== \'undefined\' ? self : this',
+    },
+    externals: (excludeDependencies ? ['moment', 'chevrotain', /csv-parse/, /csv-stringify/] : []),
+    mode,
+  }
+  return configuration
 };
+
+const unoptimized_full = buildConfiguration({
+  name: "unoptimized-full",
+  mode: "development",
+  excludeDependencies: false,
+});
+const unoptimized_without_dependencies = buildConfiguration({
+  name: "unoptimized-without-dependencies",
+  mode: "development",
+  excludeDependencies: true,
+})
+const optimized_full = buildConfiguration({
+  name: "optimized-full",
+  mode: "production",
+  excludeDependencies: false,
+})
+const optimized_without_dependencies = buildConfiguration({
+  name: "optimized-without-dependencies",
+  mode: "production",
+  excludeDependencies: true,
+})
+
+module.exports = [
+  unoptimized_full,
+  unoptimized_without_dependencies,
+  optimized_full,
+  optimized_without_dependencies,
+];
