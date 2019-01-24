66e77740ef2817d0306389110a2a681a48ff3ede
Rafał Łasocha <me@swistak35.com>
2019-01-24 13:42:56 +0100

    Remove old benchmarks with too little columns

--- benchmark/stage-1.ts
+++ benchmark/stage-1.ts
@@ -1,11 +1,5 @@
 import {benchmark} from './benchmark'
-import {sheet as A} from './sheets/03-sheet-a'
-import {sheet as B} from './sheets/04-sheet-b'
 import {sheet as T} from './sheets/05-sheet-t'
 
-console.info(' === Sheet A === ')
-benchmark(A(), [], { millisecondsPerThousandRows: 60, numberOfRuns: 100 })
-console.info('\n === Sheet B === ')
-benchmark(B(), [], { millisecondsPerThousandRows: 70, numberOfRuns: 100 })
 console.info('\n === Sheet T === ')
 benchmark(T(), [], { millisecondsPerThousandRows: 25, numberOfRuns: 100 })
