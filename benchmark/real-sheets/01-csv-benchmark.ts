106473bcfc87aa1991313c37bdc6a52b07229348
Jakub Kowalski <kowalski_jakub@hotmail.com>
2019-02-01 09:46:04 +0100

    Tidy up csv benchmark

--- benchmark/real-sheets/01-csv-benchmark.ts
+++
@@ -1,6 +0,0 @@
-import * as fs from 'fs'
-import {benchmarkCSV} from '../benchmark'
-
-fs.readFile('/tmp/money-manager.csv', 'utf8', (err, data: string) => {
-  benchmarkCSV(data, { millisecondsPerThousandRows: 100, numberOfRuns: 3, csvDump: true })
-})
