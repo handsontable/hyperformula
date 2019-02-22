36bbe8b8c715afac8280a8a7b05e142800ba530e
Rafał Łasocha <me@swistak35.com>
2019-02-22 12:41:54 +0100

    Different many medians benchmark

--- benchmark/sheets/11-many-medians.ts
+++ benchmark/sheets/11-many-medians.ts
@@ -2,19 +2,33 @@ import {benchmark, ExpectedValue} from '../benchmark'
 
 export function sheet() {
   const rows = 2000
-  const sheet = []
+  const divider = 3000
 
+  let dependent = 0
+  const sheet = []
   let current = 1
   while (current <= rows) {
-    const rowToPush = [
-      `${current}`,
-      `=MEDIAN(A1:A${current})`,
-      `${current}`,
-      `=MEDIAN(C1:C${current})`,
-      `${current}`,
-      `=MEDIAN(E1:E${current})`,
-    ]
-
+    let rowToPush
+    if (current % divider === 0) {
+      rowToPush = [
+        `${current}`,
+        `=MEDIAN(A1, A1:A${current}, C${current})`,
+        `${current}`,
+        `=MEDIAN(C1, C1:C${current}, E${current})`,
+        `${current}`,
+        `=MEDIAN(E1, E1:E${current}, A${current})`,
+      ]
+      dependent++
+    } else {
+      rowToPush = [
+        `${current}`,
+        `=MEDIAN(A1, A1:A${current}, A1)`,
+        `${current}`,
+        `=MEDIAN(C1, C1:C${current}, C1)`,
+        `${current}`,
+        `=MEDIAN(E1, E1:E${current}, E1)`,
+      ]
+    }
     sheet.push(rowToPush)
     ++current
   }
@@ -25,10 +39,10 @@ export function sheet() {
 export function expectedValues(sheet: string[][]): ExpectedValue[] {
   return [
     { address: 'A1000', value: 1000 },
-    { address: 'B1000', value: 500.5 },
+    { address: 'B1000', value: 499.5 },
     { address: 'C1000', value: 1000 },
-    { address: 'D1000', value: 500.5 },
+    { address: 'D1000', value: 499.5 },
     { address: 'E1000', value: 1000 },
-    { address: 'F1000', value: 500.5 },
+    { address: 'F1000', value: 499.5 },
   ]
 }
