cfa017dcb5574db880b034839fd70452c7fcf39f
Rafał Łasocha <me@swistak35.com>
2019-04-11 12:20:15 +0200

    Revert cell range to be based on simple cell address
    ...for now

--- src/Cell.ts
+++ src/Cell.ts
@@ -154,10 +154,10 @@ export interface CellRange {
 export const buildCellRange = (start: CellAddress, end: CellAddress): CellRange => ({ start, end })
 
 export interface SimpleCellRange {
-  start: SheetCellAddress,
-  end: SheetCellAddress,
+  start: SimpleCellAddress,
+  end: SimpleCellAddress,
 }
-export const simpleCellRange = (start: SheetCellAddress, end: SheetCellAddress): SimpleCellRange => ({ start, end })
+export const simpleCellRange = (start: SimpleCellAddress, end: SimpleCellAddress): SimpleCellRange => ({ start, end })
 
 export function cellRangeToSimpleCellRange(cellRange: CellRange, baseAddress: SimpleCellAddress) {
   return simpleCellRange(getAbsoluteAddress(cellRange.start, baseAddress), getAbsoluteAddress(cellRange.end, baseAddress))
