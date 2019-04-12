3c3d9c18e83e7746e8a69582826a4be10a4d09e0
Jakub Kowalski <kowalski_jakub@hotmail.com>
2019-04-12 14:04:40 +0200

    simplified sumprod heuristic

--- src/GraphBuilderMatrixHeuristic.ts
+++ src/GraphBuilderMatrixHeuristic.ts
@@ -20,11 +20,12 @@ import {CellVertex, EmptyCellVertex, FormulaCellVertex, Matrix, RangeVertex, Val
 
 export class GraphBuilderMatrixHeuristic {
   constructor(
-    private readonly graph: Graph<Vertex>,
-    private readonly addressMapping: IAddressMapping,
-    private readonly dependencies: Map<Vertex, CellDependency[]>,
-    private readonly parser: ParserWithCaching,
-  ) { }
+      private readonly graph: Graph<Vertex>,
+      private readonly addressMapping: IAddressMapping,
+      private readonly dependencies: Map<Vertex, CellDependency[]>,
+      private readonly parser: ParserWithCaching,
+  ) {
+  }
 
   public run() {
     const cacheMapping = this.parser.getMapping()
@@ -38,13 +39,13 @@ export class GraphBuilderMatrixHeuristic {
         const matrixDependencies = this.dependencies.get(leftCorner)!
 
         addresses.forEach((address) => {
-            const vertex = this.addressMapping.getCell(address)
-            const deps = this.dependencies.get(vertex)!
-            matrixDependencies.push(...deps)
-            this.addressMapping.setCell(address, matrixVertex)
-            this.dependencies.delete(vertex)
-            this.graph.removeNode(vertex)
-          })
+          const vertex = this.addressMapping.getCell(address)
+          const deps = this.dependencies.get(vertex)!
+          matrixDependencies.push(...deps)
+          this.addressMapping.setCell(address, matrixVertex)
+          this.dependencies.delete(vertex)
+          this.graph.removeNode(vertex)
+        })
 
         this.graph.addNode(matrixVertex)
       }
@@ -53,53 +54,45 @@ export class GraphBuilderMatrixHeuristic {
 
   private ifMatrixCompatibile(leftCorner: FormulaCellVertex, size: MatrixSize): boolean {
     const formula = leftCorner.getFormula()
-    if (formula.type === AstNodeType.FUNCTION_CALL && formula.procedureName === 'SUMPROD') {
-      const args = formula.args
 
-      if (args.length !== 2) {
+    if (formula.type === AstNodeType.FUNCTION_CALL && formula.procedureName === 'SUMPROD') {
+      if (formula.args.length !== 2) {
         return false
       }
 
-      const rawLeftArg = args[0]
-      const rawRightArg = args[1]
-      let leftArg, rightArg
-      if (rawLeftArg.type === AstNodeType.CELL_RANGE && rawRightArg.type === AstNodeType.FUNCTION_CALL && rawRightArg.procedureName === 'TRANSPOSE') {
-        const rightInsideArg = rawRightArg.args[0] as CellRangeAst
-        if (rawLeftArg.start.type === CellReferenceType.CELL_REFERENCE_ABSOLUTE_COL && rawLeftArg.end.type === CellReferenceType.CELL_REFERENCE_ABSOLUTE_COL && rightInsideArg.start.type === CellReferenceType.CELL_REFERENCE_ABSOLUTE_ROW && rightInsideArg.end.type === CellReferenceType.CELL_REFERENCE_ABSOLUTE_ROW) {
-          leftArg = rawLeftArg
-          rightArg = rawRightArg
-        } else {
-          return false
-        }
-      } else if (rawRightArg.type === AstNodeType.CELL_RANGE && rawLeftArg.type === AstNodeType.FUNCTION_CALL && rawLeftArg.procedureName === 'TRANSPOSE') {
-        leftArg = rawRightArg
-        rightArg = rawLeftArg
+      const [leftArg, rightArg] = formula.args
+      let leftRange, rightRange
+
+      if (leftArg.type === AstNodeType.CELL_RANGE && rightArg.type === AstNodeType.FUNCTION_CALL && rightArg.procedureName === 'TRANSPOSE') {
+        leftRange = leftArg
+        rightRange = rightArg.args[0] as CellRangeAst
+      } else if (rightArg.type === AstNodeType.CELL_RANGE && leftArg.type === AstNodeType.FUNCTION_CALL && leftArg.procedureName === 'TRANSPOSE') {
+        leftRange = leftArg.args[0] as CellRangeAst
+        rightRange = rightArg
       } else {
         return false
       }
 
-      const currentMatrix = simpleCellRange(leftCorner.getAddress(), simpleCellAddress(leftCorner.getAddress().sheet, leftCorner.getAddress().col + size.width - 1, leftCorner.getAddress().row + size.height - 1))
-      const transposeArg = rightArg.args[0] as CellRangeAst
+      if (leftRange.start.type !== CellReferenceType.CELL_REFERENCE_ABSOLUTE_COL
+          || leftRange.end.type !== CellReferenceType.CELL_REFERENCE_ABSOLUTE_COL
+          || rightRange.start.type !== CellReferenceType.CELL_REFERENCE_ABSOLUTE_ROW
+          || rightRange.end.type !== CellReferenceType.CELL_REFERENCE_ABSOLUTE_ROW) {
+        return false
+      }
 
-      const leftArgRange = cellRangeToSimpleCellRange(leftArg, leftCorner.getAddress())
-      const rightArgRange = cellRangeToSimpleCellRange(transposeArg, leftCorner.getAddress())
+      const leftArgRange = cellRangeToSimpleCellRange(leftRange, leftCorner.getAddress())
+      const rightArgRange = cellRangeToSimpleCellRange(rightRange, leftCorner.getAddress())
 
       const leftRangeSize = this.rangeSize(leftArgRange)
       const rightRangeSize = this.rangeSize(rightArgRange)
 
-      let leftMatrix, rightMatrix
-
       if (leftRangeSize.height === 1 && rightRangeSize.width === 1 && leftRangeSize.width === rightRangeSize.height) {
-        leftMatrix = simpleCellRange(leftArgRange.start, simpleCellAddress(leftArgRange.start.sheet, leftArgRange.end.col, leftArgRange.end.row + size.height - 1))
-        rightMatrix = simpleCellRange(rightArgRange.start, simpleCellAddress(rightArgRange.start.sheet, rightArgRange.end.col + size.width - 1, rightArgRange.end.row))
-      } else if (leftRangeSize.width === 1 && rightRangeSize.height === 1 && leftRangeSize.height === rightRangeSize.width) {
-        leftMatrix = simpleCellRange(leftArgRange.start, simpleCellAddress(leftArgRange.start.sheet, leftArgRange.end.col + size.width - 1, leftArgRange.end.row))
-        rightMatrix = simpleCellRange(rightArgRange.start, simpleCellAddress(rightArgRange.start.sheet, rightArgRange.end.col, rightArgRange.end.row + size.height - 1))
-      } else {
-        return false
-      }
+        const leftMatrix = simpleCellRange(leftArgRange.start, simpleCellAddress(leftArgRange.start.sheet, leftArgRange.end.col, leftArgRange.end.row + size.height - 1))
+        const rightMatrix = simpleCellRange(rightArgRange.start, simpleCellAddress(rightArgRange.start.sheet, rightArgRange.end.col + size.width - 1, rightArgRange.end.row))
+        const currentMatrix = simpleCellRange(leftCorner.getAddress(), simpleCellAddress(leftCorner.getAddress().sheet, leftCorner.getAddress().col + size.width - 1, leftCorner.getAddress().row + size.height - 1))
 
-      return !this.overlap(leftMatrix, currentMatrix) && !this.overlap(rightMatrix, currentMatrix)
+        return !this.overlap(leftMatrix, currentMatrix) && !this.overlap(rightMatrix, currentMatrix)
+      }
     }
 
     return false
