27a3730aee3397d27a5760ff34225449fe01d66b
Rafał Łasocha <me@swistak35.com>
2019-04-18 15:07:46 +0200

    Extract a strategy out of AddressMapping

--- src/AddressMapping.ts
+++ src/AddressMapping.ts
@@ -1,4 +1,4 @@
-import {CellValue, SimpleCellAddress} from './Cell'
+import {SheetCellAddress, CellValue, SimpleCellAddress} from './Cell'
 import {IAddressMapping} from './IAddressMapping'
 import {CellVertex, EmptyCellVertex, MatrixVertex, Vertex} from './Vertex'
 
@@ -7,14 +7,60 @@ import {CellVertex, EmptyCellVertex, MatrixVertex, Vertex} from './Vertex'
  *
  * Uses Map to store addresses, having minimal memory usage for sparse sheets but not necessarily constant set/lookup.
  */
-export class AddressMapping implements IAddressMapping {
+class SparseStrategy {
   /**
    * Map of Maps in which actual data is stored.
    *
    * Key of map in first level is column number.
    * Key of map in second level is row number.
    */
-  private mapping: Map<number, Map<number, Map<number, CellVertex>>> = new Map()
+  private mapping: Map<number, Map<number, CellVertex>> = new Map()
+
+  /** @inheritDoc */
+  public getCell(address: SheetCellAddress): CellVertex {
+    const colMapping = this.mapping.get(address.col)
+    if (!colMapping) {
+      return EmptyCellVertex.getSingletonInstance()
+    }
+    return colMapping.get(address.row) || EmptyCellVertex.getSingletonInstance()
+  }
+
+  /** @inheritDoc */
+  public setCell(address: SheetCellAddress, newVertex: CellVertex) {
+    let colMapping = this.mapping.get(address.col)
+    if (!colMapping) {
+      colMapping = new Map()
+      this.mapping.set(address.col, colMapping)
+    }
+    colMapping.set(address.row, newVertex)
+  }
+
+  /** @inheritDoc */
+  public has(address: SheetCellAddress): boolean {
+    const colMapping = this.mapping.get(address.col)
+    if (!colMapping) {
+      return false
+    }
+    return !!colMapping.get(address.row)
+  }
+
+  /** @inheritDoc */
+  public getHeight(): number {
+    let currentMax = 0
+    this.mapping.forEach((colMapping) => {
+      currentMax = Math.max(currentMax, Math.max(...Array.from(colMapping.keys())) + 1)
+    })
+    return currentMax
+  }
+
+  /** @inheritDoc */
+  public getWidth(): number {
+    return Math.max(0, Math.max(...Array.from(this.mapping.keys())) + 1)
+  }
+}
+
+export class AddressMapping implements IAddressMapping {
+  private mapping: Map<number, SparseStrategy> = new Map()
 
   /** @inheritDoc */
   public getCell(address: SimpleCellAddress): CellVertex {
@@ -22,11 +68,7 @@ export class AddressMapping implements IAddressMapping {
     if (!sheetMapping) {
       return EmptyCellVertex.getSingletonInstance()
     }
-    const colMapping = sheetMapping.get(address.col)
-    if (!colMapping) {
-      return EmptyCellVertex.getSingletonInstance()
-    }
-    return colMapping.get(address.row) || EmptyCellVertex.getSingletonInstance()
+    return sheetMapping.getCell(address)
   }
 
   public getCellValue(address: SimpleCellAddress): CellValue {
@@ -43,15 +85,10 @@ export class AddressMapping implements IAddressMapping {
   public setCell(address: SimpleCellAddress, newVertex: CellVertex) {
     let sheetMapping = this.mapping.get(address.sheet)
     if (!sheetMapping) {
-      sheetMapping = new Map()
+      sheetMapping = new SparseStrategy()
       this.mapping.set(address.sheet, sheetMapping)
     }
-    let colMapping = sheetMapping.get(address.col)
-    if (!colMapping) {
-      colMapping = new Map()
-      sheetMapping.set(address.col, colMapping)
-    }
-    colMapping.set(address.row, newVertex)
+    sheetMapping.setCell(address, newVertex)
   }
 
   /** @inheritDoc */
@@ -60,25 +97,17 @@ export class AddressMapping implements IAddressMapping {
     if (!sheetMapping) {
       return false
     }
-    const colMapping = sheetMapping.get(address.col)
-    if (!colMapping) {
-      return false
-    }
-    return !!colMapping.get(address.row)
+    return sheetMapping.has(address)
   }
 
   /** @inheritDoc */
   public getHeight(sheetId: number): number {
-    let currentMax = 0
-    this.mapping.get(sheetId)!.forEach((colMapping) => {
-      currentMax = Math.max(currentMax, Math.max(...Array.from(colMapping.keys())) + 1)
-    })
-    return currentMax
+    return this.mapping.get(sheetId)!.getHeight()
   }
 
   /** @inheritDoc */
   public getWidth(sheetId: number): number {
-    return Math.max(0, Math.max(...Array.from(this.mapping.get(sheetId)!.keys())) + 1)
+    return this.mapping.get(sheetId)!.getWidth()
   }
 
   public isEmpty(address: SimpleCellAddress): boolean {
