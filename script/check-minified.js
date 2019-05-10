26412faa311e28598f9f6df9da98c705b8977de2
Rafał Łasocha <me@swistak35.com>
2019-05-10 20:06:50 +0200

    Wrap script checking minified version into async

--- script/check-minified.js
+++ script/check-minified.js
@@ -1,14 +1,16 @@
-var HandsOnEngine = require("../dist/optimized-full/bundle.js")
-var Engine = HandsOnEngine.HandsOnEngine
-var Config = HandsOnEngine.Config
-var assert = require('assert')
+(async function() {
+  var HandsOnEngine = require("../dist/optimized-full/bundle.js")
+  var Engine = HandsOnEngine.HandsOnEngine
+  var Config = HandsOnEngine.Config
+  var assert = require('assert')
 
-var engine = Engine.buildFromArray([
-  ['42', '=A1 + 2']
-], new Config({ gpuMode: "cpu" }))
-var valueA1 = engine.getCellValue('A1')
-var valueB1 = engine.getCellValue('B1')
+  var engine = await Engine.buildFromArray([
+    ['42', '=A1 + 2']
+  ], new Config({ gpuMode: "cpu" }))
+  var valueA1 = engine.getCellValue('A1')
+  var valueB1 = engine.getCellValue('B1')
 
-assert(valueA1 == 42)
-assert(valueB1 == 44)
-console.log("OK")
+  assert(valueA1 == 42)
+  assert(valueB1 == 44)
+  console.log("OK")
+})()
