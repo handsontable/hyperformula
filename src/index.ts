ca5b0913968d8dbdc989917a25672e7f575469b3
Rafał Łasocha <me@swistak35.com>
2019-05-11 13:45:09 +0200

    Inline SingleThreadEvaluator
    We'll diverge soon

--- src/index.ts
+++ src/index.ts
@@ -74,7 +74,52 @@ class SingleThreadEvaluator implements Evaluator {
   }
 }
 
-class ParallelEvaluator extends SingleThreadEvaluator {
+class ParallelEvaluator implements Evaluator {
+  /** Topologically sorted list of vertices. */
+  private sortedVertices: Vertex[] = []
+
+  /** List of vertices which are on some cycle */
+  private verticesOnCycle: Vertex[] = []
+
+  private interpreter: Interpreter
+
+  constructor(
+    private readonly addressMapping: AddressMapping,
+    private readonly rangeMapping: RangeMapping,
+    private readonly graph: Graph<Vertex>,
+    private readonly config: Config,
+    private readonly stats: Statistics,
+  ) {
+    this.interpreter = new Interpreter(this.addressMapping, this.rangeMapping, this.graph, this.config)
+  }
+
+  public run() {
+    this.stats.measure(StatType.TOP_SORT, () => {
+      ({ sorted: this.sortedVertices, cycled: this.verticesOnCycle } = this.graph.topologicalSort())
+    })
+
+    this.recomputeFormulas()
+  }
+
+  /**
+   * Recalculates formulas in the topological sort order
+   */
+  private recomputeFormulas() {
+    this.verticesOnCycle.forEach((vertex: Vertex) => {
+      (vertex as FormulaCellVertex).setCellValue(new CellError(ErrorType.CYCLE))
+    })
+    this.sortedVertices.forEach((vertex: Vertex) => {
+      if (vertex instanceof FormulaCellVertex || (vertex instanceof MatrixVertex && vertex.isFormula())) {
+        const address = vertex.getAddress()
+        const formula = vertex.getFormula() as Ast
+        const cellValue = this.interpreter.evaluateAst(formula, address)
+        vertex.setCellValue(cellValue)
+      } else if (vertex instanceof RangeVertex) {
+        vertex.clear()
+      }
+    })
+  }
+
 }
 
 class EvaluatorPolicy {
