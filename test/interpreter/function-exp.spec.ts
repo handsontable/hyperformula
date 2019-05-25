0e4bc9a9237b6fbe2d8c0cbd9f8858c4d373cf2c
Rafał Łasocha <me@swistak35.com>
2019-05-25 18:00:26 +0200

    Add tests for exp function

---
+++ test/interpreter/function-exp.spec.ts
@@ -0,0 +1,32 @@
+import {HandsOnEngine} from '../../src'
+import {CellError, ErrorType} from '../../src/Cell'
+import '../testConfig'
+
+describe('Interpreter', () => {
+  it('function EXP happy path', async () => {
+    const engine = await HandsOnEngine.buildFromArray([
+      ['=EXP(0)', '=EXP(2)']
+    ])
+
+    expect(engine.getCellValue('A1')).toEqual(1)
+    expect(engine.getCellValue('B1')).toBeCloseTo(7.38905609893065)
+  })
+
+  it('function EXP given wrong argument type', async () => {
+    const engine = await HandsOnEngine.buildFromArray([
+      ['=EXP("foo")']
+    ])
+
+    expect(engine.getCellValue('A1')).toEqual(new CellError(ErrorType.VALUE))
+  })
+
+  it('function EXP given wrong number of arguments', async () => {
+    const engine = await HandsOnEngine.buildFromArray([
+      ['=EXP()'],
+      ['=EXP(1, 2)'],
+    ])
+
+    expect(engine.getCellValue('A1')).toEqual(new CellError(ErrorType.NA))
+    expect(engine.getCellValue('A2')).toEqual(new CellError(ErrorType.NA))
+  })
+})
