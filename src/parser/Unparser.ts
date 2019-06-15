f38cc0957e22a99bf53ea754f48f1a5605415a87
Jakub Kowalski <kowalski_jakub@hotmail.com>
2019-06-15 08:05:07 +0200

    Make unparser a class

--- src/parser/Unparser.ts
+++ src/parser/Unparser.ts
@@ -1,6 +1,7 @@
 import {Ast, AstNodeType} from "./index";
 import {SimpleCellAddress} from "../Cell";
 import {CellAddress, CellReferenceType} from "./CellAddress";
+import {Config} from "../Config";
 
 export type SheetMappingFn = (sheetId: number) => string
 
@@ -19,6 +20,54 @@ const binaryOpTokenMap = {
   [AstNodeType.LESS_THAN_OR_EQUAL_OP]: "<=",
 }
 
+export class Unparser {
+  constructor(
+      private readonly config: Config,
+      private readonly sheetMappingFn: SheetMappingFn
+  ) {
+  }
+
+  public unparse(ast: Ast, address: SimpleCellAddress): string {
+    switch (ast.type) {
+      case AstNodeType.NUMBER: {
+        return ast.value.toString()
+      }
+      case AstNodeType.STRING: {
+        return ast.value
+      }
+      case AstNodeType.FUNCTION_CALL: {
+        const that = this
+        const result = ast.args.reduce(function (acc, val) {
+          return acc + that.unparse(val, address) + that.config.functionArgSeparator
+        }, ast.procedureName + "(")
+        return result.slice(0, result.length - 1) + ")"
+      }
+      case AstNodeType.CELL_REFERENCE: {
+        const sheet = this.sheetMappingFn(ast.reference.sheet)
+        return "$" + sheet + "." + addressToString(ast.reference, address)
+      }
+      case AstNodeType.CELL_RANGE: {
+        const sheet = this.sheetMappingFn(ast.start.sheet)
+        return "$" + sheet + "." + addressToString(ast.start, address) + ":" + addressToString(ast.end, address)
+      }
+      case AstNodeType.MINUS_UNARY_OP: {
+        return "-" + this.unparse(ast.value, address)
+      }
+      case AstNodeType.ERROR: {
+        return "!ERR"
+      }
+      default: {
+        if (binaryOpTokenMap.hasOwnProperty(ast.type)) {
+          return this.unparse(ast.left, address) + binaryOpTokenMap[ast.type] + this.unparse(ast.right, address)
+        } else {
+          throw Error("Cannot unparse formula")
+        }
+      }
+    }
+  }
+}
+
+
 export function columnIndexToLabel(column: number) {
   let result = '';
 
@@ -35,37 +84,5 @@ export function addressToString(address: CellAddress, baseAddress: SimpleCellAdd
   const column = columnIndexToLabel(simpleAddress.col)
   const rowDolar = address.type === CellReferenceType.CELL_REFERENCE_ABSOLUTE || address.type === CellReferenceType.CELL_REFERENCE_ABSOLUTE_ROW ? "$" : ''
   const colDolar = address.type === CellReferenceType.CELL_REFERENCE_ABSOLUTE || address.type === CellReferenceType.CELL_REFERENCE_ABSOLUTE_COL ? "$" : ''
-  return `${colDolar}${column}${rowDolar}${simpleAddress.row+1}`
-}
-
-export function unparse(ast: Ast, address: SimpleCellAddress, sheetMappingFn: SheetMappingFn): string {
-  switch (ast.type) {
-    case AstNodeType.NUMBER:
-      return ast.value.toString()
-    case AstNodeType.STRING:
-      return ast.value
-    case AstNodeType.FUNCTION_CALL:
-      const result = ast.args.reduce(function (acc, val) {
-        return acc + unparse(val, address, sheetMappingFn) + ","
-      }, ast.procedureName + "(")
-      return result.slice(0, result.length - 1) + ")"
-    case AstNodeType.CELL_REFERENCE: {
-      const sheet = sheetMappingFn(ast.reference.sheet)
-      return "$" + sheet + "." + addressToString(ast.reference, address)
-    }
-    case AstNodeType.CELL_RANGE: {
-      const sheet = sheetMappingFn(ast.start.sheet)
-      return "$" + sheet + "." + addressToString(ast.start, address) + ":" + addressToString(ast.end, address)
-    }
-    case AstNodeType.MINUS_UNARY_OP:
-      return "-" + unparse(ast.value, address, sheetMappingFn)
-    case AstNodeType.ERROR:
-      return "!ERR"
-    default:
-      if (binaryOpTokenMap.hasOwnProperty(ast.type)) {
-        return unparse(ast.left, address, sheetMappingFn) + binaryOpTokenMap[ast.type] + unparse(ast.right, address, sheetMappingFn)
-      } else {
-        throw Error("Cannot unparse formula")
-      }
-  }
+  return `${colDolar}${column}${rowDolar}${simpleAddress.row + 1}`
 }
--- test/unparse.spec.ts
+++ test/unparse.spec.ts
@@ -2,83 +2,66 @@ import {CellReferenceAst, ParserWithCaching} from "../src/parser";
 import {Config} from "../src";
 import {SheetMapping} from "../src/SheetMapping";
 import {CellAddress} from "../src/parser/CellAddress";
-import {unparse} from "../src/parser/Unparser";
 import {simpleCellAddress} from "../src/Cell";
+import {Unparser} from "../src/parser/Unparser";
 
-describe('Unparse', () => {
+describe('Unparser', () => {
+  const sheetMapping = new SheetMapping()
+  sheetMapping.addSheet("Sheet1")
+  const parser = new ParserWithCaching(new Config(), sheetMapping.fetch)
+  const unparser = new Unparser(new Config(), sheetMapping.name)
+
   it('#unparse', async () => {
-    const sheetMapping = new SheetMapping()
-    sheetMapping.addSheet("Sheet1")
     const formula = '=1+SUM(1,2,3)*3'
-    const parser = new ParserWithCaching(new Config(), sheetMapping.fetch)
     const ast = parser.parse(formula, CellAddress.absolute(0, 0, 0)).ast
-    const unparsed = unparse(ast, simpleCellAddress(0, 0, 0), sheetMapping.name)
-
+    const unparsed = unparser.unparse(ast, simpleCellAddress(0, 0, 0))
     expect(formula).toEqual("=" + unparsed)
   })
 
   it('#unparse unary minus', async () => {
-    const sheetMapping = new SheetMapping()
-    sheetMapping.addSheet("Sheet1")
     const formula = '=-1+3'
-    const parser = new ParserWithCaching(new Config(), sheetMapping.fetch)
     const ast = parser.parse(formula, CellAddress.absolute(0, 0, 0)).ast
-    const unparsed = unparse(ast, simpleCellAddress(0, 0, 0), sheetMapping.name)
+    const unparsed = unparser.unparse(ast, simpleCellAddress(0, 0, 0))
 
     expect(formula).toEqual("=" + unparsed)
   })
 
   it('#unparse simple addreess', async () => {
-    const sheetMapping = new SheetMapping()
-    sheetMapping.addSheet("Sheet1")
     const formula = '=$Sheet1.A1'
-    const parser = new ParserWithCaching(new Config(), sheetMapping.fetch)
     const ast = parser.parse(formula, CellAddress.absolute(0, 0, 0)).ast
-    const unparsed = unparse(ast, simpleCellAddress(0, 0, 0), sheetMapping.name)
+    const unparsed = unparser.unparse(ast, simpleCellAddress(0, 0, 0))
 
     expect(formula).toEqual("=" + unparsed)
   })
 
   it('#unparse absolute col', async () => {
-    const sheetMapping = new SheetMapping()
-    sheetMapping.addSheet("Sheet1")
     const formula = '=$Sheet1.$A1'
-    const parser = new ParserWithCaching(new Config(), sheetMapping.fetch)
     const ast = parser.parse(formula, CellAddress.absolute(0, 0, 0)).ast
-    const unparsed = unparse(ast, simpleCellAddress(0, 0, 0), sheetMapping.name)
+    const unparsed = unparser.unparse(ast, simpleCellAddress(0, 0, 0))
 
     expect(formula).toEqual("=" + unparsed)
   })
 
   it('#unparse absolute row addreess', async () => {
-    const sheetMapping = new SheetMapping()
-    sheetMapping.addSheet("Sheet1")
     const formula = '=$Sheet1.A$1'
-    const parser = new ParserWithCaching(new Config(), sheetMapping.fetch)
     const ast = parser.parse(formula, CellAddress.absolute(0, 0, 0)).ast
-    const unparsed = unparse(ast, simpleCellAddress(0, 0, 0), sheetMapping.name)
+    const unparsed = unparser.unparse(ast, simpleCellAddress(0, 0, 0))
 
     expect(formula).toEqual("=" + unparsed)
   })
 
   it('#unparse absolute address', async () => {
-    const sheetMapping = new SheetMapping()
-    sheetMapping.addSheet("Sheet1")
     const formula = '=$Sheet1.$A$1'
-    const parser = new ParserWithCaching(new Config(), sheetMapping.fetch)
     const ast = parser.parse(formula, CellAddress.absolute(0, 0, 0)).ast
-    const unparsed = unparse(ast, simpleCellAddress(0, 0, 0), sheetMapping.name)
+    const unparsed = unparser.unparse(ast, simpleCellAddress(0, 0, 0))
 
     expect(formula).toEqual("=" + unparsed)
   })
 
   it('#unparse cell range', async () => {
-    const sheetMapping = new SheetMapping()
-    sheetMapping.addSheet("Sheet1")
     const formula = '=$Sheet1.$A$1:B$2'
-    const parser = new ParserWithCaching(new Config(), sheetMapping.fetch)
     const ast = parser.parse(formula, CellAddress.absolute(0, 0, 0)).ast
-    const unparsed = unparse(ast, simpleCellAddress(0, 0, 0), sheetMapping.name)
+    const unparsed = unparser.unparse(ast, simpleCellAddress(0, 0, 0))
 
     expect(formula).toEqual("=" + unparsed)
   })
