c3d3fb444236f5333d403570bc77394c8196afa1
Jakub Kowalski <kowalski_jakub@hotmail.com>
2019-06-25 15:44:34 +0200

    more tests

--- test/disable-matrix-optimizations.spec.ts
+++ test/disable-matrix-optimizations.spec.ts
@@ -1,13 +1,13 @@
 import {Config, HandsOnEngine} from "../src";
 import {simpleCellAddress} from "../src/Cell";
-import {MatrixVertex, ValueCellVertex} from "../src/Vertex";
+import {MatrixVertex, RangeVertex, ValueCellVertex} from "../src/Vertex";
 
 describe("Disable matrix optimizatoins", () => {
-  it("should split matrix into singluar value cell vertices", () => {
-    const config = new Config({ matrixDetection: true, matrixDetectionThreshold: 1})
+  it("should split matrix into value cell vertices", () => {
+    const config = new Config({matrixDetection: true, matrixDetectionThreshold: 1})
     const sheet = [
-        ['1','2'],
-        ['3','4'],
+      ['1', '2'],
+      ['3', '4'],
     ]
 
     const engine = HandsOnEngine.buildFromArray(sheet, config)
@@ -22,5 +22,26 @@ describe("Disable matrix optimizatoins", () => {
     expect(engine.addressMapping!.fetchCell(simpleCellAddress(0, 1, 1))).toBeInstanceOf(ValueCellVertex)
     expect(engine.getCellValue("A1")).toBe(1)
     expect(engine.getCellValue("B2")).toBe(4)
+  })
+
+  it("should update edges between matrix and range", () => {
+    const config = new Config({matrixDetection: true, matrixDetectionThreshold: 1})
+    const sheet = [
+      ['1', '2'],
+      ['3', '4'],
+      ['=SUM(A1:B1)']
+    ]
+
+    const engine = HandsOnEngine.buildFromArray(sheet, config)
+    let range = engine.rangeMapping.getRange(simpleCellAddress(0, 0, 0), simpleCellAddress(0, 1, 0)) as RangeVertex
+    expect(engine.graph.getDependecies(range).length).toBe(1)
+
+    engine.disableMatrixOptimizations()
+    const a1 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 0, 0)) as ValueCellVertex
+    const b1 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 1, 0)) as ValueCellVertex
+    range = engine.rangeMapping.getRange(simpleCellAddress(0, 0, 0), simpleCellAddress(0, 1, 0)) as RangeVertex
+    expect(engine.graph.getDependecies(range).length).toBe(2)
+    expect(engine.graph.existsEdge(a1, range)).toBe(true)
+    expect(engine.graph.existsEdge(b1, range)).toBe(true)
   })
 })
