02b2b2525f9187f6ec92da33b255323b9ec690d0
Jakub Kowalski <kowalski_jakub@hotmail.com>
2019-06-28 12:36:32 +0200

    tests

--- test/disable-matrix-optimizations.spec.ts
+++ test/disable-matrix-optimizations.spec.ts
@@ -1,6 +1,6 @@
 import {Config, HandsOnEngine} from "../src";
 import {simpleCellAddress} from "../src/Cell";
-import {MatrixVertex, RangeVertex, ValueCellVertex} from "../src/Vertex";
+import {EmptyCellVertex, MatrixVertex, RangeVertex, ValueCellVertex} from "../src/Vertex";
 import './testConfig.ts'
 
 describe("Disable matrix optimizatoins", () => {
@@ -60,21 +60,51 @@ describe("Disable matrix optimizatoins", () => {
     ]
 
     const engine = HandsOnEngine.buildFromArray(sheet, config)
-    let a1 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 0, 0)) as ValueCellVertex
-    let b2 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 1, 1)) as ValueCellVertex
+    const a1 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 0, 0)) as MatrixVertex
+    const b2 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 1, 1)) as MatrixVertex
+    expect(a1).toBeInstanceOf(MatrixVertex)
+    expect(b2).toBeInstanceOf(MatrixVertex)
     expect(a1).toBe(b2)
-    expect(Array.from(engine.addressMapping!.numericMatrices()).length).toBe(1)
 
     engine.disableNumericMatrices()
 
-    a1 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 0, 0)) as ValueCellVertex
-    b2 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 1, 1)) as ValueCellVertex
-    expect(a1).not.toBe(b2)
-    const a3 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 0, 2)) as ValueCellVertex
+    const a1_after_update = engine.addressMapping!.fetchCell(simpleCellAddress(0, 0, 0)) as ValueCellVertex
+    const b2_after_update = engine.addressMapping!.fetchCell(simpleCellAddress(0, 1, 1)) as ValueCellVertex
+    expect(a1_after_update).toBeInstanceOf(ValueCellVertex)
+    expect(b2_after_update).toBeInstanceOf(ValueCellVertex)
+    expect(a1_after_update).not.toBe(b2_after_update)
 
-    expect(a1).toBeInstanceOf(ValueCellVertex)
-    expect(b2).toBeInstanceOf(ValueCellVertex)
-    expect(engine.graph.existsEdge(a1, a3)).toBe(true)
-    expect(engine.graph.existsEdge(b2, a3)).toBe(true)
+    const a3 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 0, 2)) as ValueCellVertex
+    expect(engine.graph.existsEdge(a1_after_update, a3)).toBe(true)
+    expect(engine.graph.existsEdge(b2_after_update, a3)).toBe(true)
+  })
+
+  it("should not change edges not related to matrix", () => {
+    const config = new Config({matrixDetection: true, matrixDetectionThreshold: 1})
+    const sheet = [
+      ['1', '2'],
+      ['3', '4'],
+      ['=A1+C1'],
+    ]
+
+    const engine = HandsOnEngine.buildFromArray(sheet, config)
+
+    const a3 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 0, 2)) as ValueCellVertex
+    const a1 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 0, 0)) as ValueCellVertex
+    const c1 = engine.addressMapping!.fetchCell(simpleCellAddress(0, 2, 0)) as EmptyCellVertex
+    expect(a1).toBeInstanceOf(MatrixVertex)
+    expect(c1).toBeInstanceOf(EmptyCellVertex)
+    expect(engine.graph.existsEdge(c1, a3)).toBe(true)
+
+    engine.disableNumericMatrices()
+
+    let a1_after_update = engine.addressMapping!.fetchCell(simpleCellAddress(0, 0, 0)) as ValueCellVertex
+    let c1_after_update = engine.addressMapping!.fetchCell(simpleCellAddress(0, 2, 0)) as EmptyCellVertex
+    expect(c1).toBe(c1_after_update)
+
+    expect(a1_after_update).toBeInstanceOf(ValueCellVertex)
+    expect(c1_after_update).toBeInstanceOf(EmptyCellVertex)
+    expect(engine.graph.existsEdge(a1_after_update, a3)).toBe(true)
+    expect(engine.graph.existsEdge(c1_after_update, a3)).toBe(true)
   })
 })
