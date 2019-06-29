a66511b001dac2e34d3009516a1bc43b1283053f
Jakub Kowalski <kowalski_jakub@hotmail.com>
2019-06-29 11:57:29 +0200

    basic spec

---
+++ test/removing-rows.spec.ts
@@ -0,0 +1,30 @@
+import {HandsOnEngine} from "../src";
+import {SimpleCellAddress, simpleCellAddress} from "../src/Cell";
+import {CellAddress} from "../src/parser/CellAddress";
+import './testConfig.ts'
+import {FormulaCellVertex} from "../src/Vertex";
+import {CellReferenceAst} from "../src/parser";
+
+const extractReference = (engine: HandsOnEngine, address: SimpleCellAddress): CellAddress => {
+  return ((engine.addressMapping!.fetchCell(address) as FormulaCellVertex).getFormula() as CellReferenceAst).reference
+}
+
+describe('Removing rows', () => {
+  it('should not affect absolute dependencies to other sheet', () => {
+    const engine = HandsOnEngine.buildFromSheets({
+      Sheet1: [
+          ['1'], // rows to delete
+          ['2'], //
+          ['=$Sheet2.A$1']
+      ],
+      Sheet2: [
+          ['3'],
+          ['4']
+      ]
+    })
+
+    expect(extractReference(engine, simpleCellAddress(0, 0, 2))).toEqual(CellAddress.absoluteRow(1, 0, 0))
+    engine.removeRows(0, 0, 1)
+    expect(extractReference(engine, simpleCellAddress(0, 0, 0))).toEqual(CellAddress.absoluteRow(1, 0, 0))
+  })
+})
