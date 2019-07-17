79b4b921f020bf43c2ec8f707de3d52350bf5a86
Rafał Łasocha <me@swistak35.com>
2019-07-17 16:12:11 +0200

    Extracted MAX tests

--- test/interpreter/numeric-aggregation-plugin.spec.ts
+++ test/interpreter/numeric-aggregation-plugin.spec.ts
@@ -59,48 +59,6 @@ describe('SUM', () => {
   })
 })
 
-describe('MAX', () => {
-  it('MAX with empty args', async () => {
-    const engine = await HandsOnEngine.buildFromArray([['=MAX()']])
-    expect(engine.getCellValue('A1')).toEqual(new CellError(ErrorType.NA))
-  })
-
-  it('MAX with args', async () => {
-    const engine = await HandsOnEngine.buildFromArray([['=MAX(1, B1)', '3.14']])
-    expect(engine.getCellValue('A1')).toBeCloseTo(3.14)
-  })
-
-  it('MAX with range', async () => {
-    const engine = await HandsOnEngine.buildFromArray([['1'], ['3'], ['2'], ['=MAX(A1:A3)']])
-    expect(engine.getCellValue('A4')).toEqual(3)
-  })
-
-  it('MAX with mixed arguments', async () => {
-    const engine = await HandsOnEngine.buildFromArray([['1'], ['3'], ['2'], ['=MAX(4,A1:A3)']])
-    expect(engine.getCellValue('A4')).toEqual(4)
-  })
-
-  it('MAX with string', async () => {
-    const engine = await HandsOnEngine.buildFromArray([['1'], ['3'], ['2'], ['=MAX(A1:A3,"foo")']])
-    expect(engine.getCellValue('A4')).toEqual(3)
-  })
-
-  it('MAX with string in range', async () => {
-    const engine = await HandsOnEngine.buildFromArray([['1'], ['3'], ['foo'], ['=MAX(A1:A3)']])
-    expect(engine.getCellValue('A4')).toEqual(3)
-  })
-
-  it('MAX of strings', async () => {
-    const engine = await HandsOnEngine.buildFromArray([['foo'], ['bar'], ['=MAX(A1:A2)']])
-    expect(engine.getCellValue('A3')).toEqual(0)
-  })
-
-  it('MAX of strings and -1', async () => {
-    const engine = await HandsOnEngine.buildFromArray([['foo'], ['bar'], ['-1'], ['=MAX(A1:A3)']])
-    expect(engine.getCellValue('A4')).toEqual(-1)
-  })
-})
-
 describe('MIN', () => {
   it('MIN with empty args', async () => {
     const engine = await HandsOnEngine.buildFromArray([['=MIN()']])
