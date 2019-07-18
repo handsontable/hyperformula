d39ae6c7dee2534ec63a2cbfccadb753f40fc258
Rafał Łasocha <me@swistak35.com>
2019-07-18 11:23:58 +0200

    Add test for CONCATENATE with empty string

--- test/interpreter/function-concatenate.spec.ts
+++ test/interpreter/function-concatenate.spec.ts
@@ -3,21 +3,27 @@ import {CellError, ErrorType} from '../../src/Cell'
 import '../testConfig'
 
 describe('function CONCATENATE', () => {
-  it('function CONCATENATE by default returns empty string', async () => {
+  it('by default returns empty string', async () => {
     const engine = await HandsOnEngine.buildFromArray([['=CONCATENATE()']])
 
     expect(engine.getCellValue('A1')).toEqual('')
   })
 
-  it('function CONCATENATE works', async () => {
+  it('works', async () => {
     const engine = await HandsOnEngine.buildFromArray([['John', 'Smith', '=CONCATENATE(A1, B1)']])
 
     expect(engine.getCellValue('C1')).toEqual('JohnSmith')
   })
 
-  it('function CONCATENATE returns error if one of the arguments is error', async () => {
+  it('returns error if one of the arguments is error', async () => {
     const engine = await HandsOnEngine.buildFromArray([['John', '=1/0', '=CONCATENATE(A1, B1)']])
 
     expect(engine.getCellValue('C1')).toEqual(new CellError(ErrorType.DIV_BY_ZERO))
+  })
+
+  it('empty value is empty string', () => {
+    const engine = HandsOnEngine.buildFromArray([['foo', '', 'bar', '=CONCATENATE(A1, B1, C1)']])
+
+    expect(engine.getCellValue('D1')).toEqual("foobar")
   })
 })
