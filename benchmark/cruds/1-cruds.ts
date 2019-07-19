62d9dd5e3e27c5bc450981bbd4e7f358902750f5
Jakub Kowalski <kowalski_jakub@hotmail.com>
2019-07-19 14:00:16 +0200

    simple crud benchmarks

---
+++ benchmark/cruds/1-cruds.ts
@@ -0,0 +1,30 @@
+import {HandsOnEngine} from "../../src";
+import {sheet as Sb} from "../sheets/01-simple-big";
+import {sheet as Bs} from "../sheets/06-big-sum";
+import {addColumns, addRows, removeColumns, removeRows } from "./operations";
+
+function batch(engine: HandsOnEngine) {
+  addRows(engine)
+  addColumns(engine)
+  removeRows(engine)
+  removeColumns(engine)
+}
+
+function start() {
+  let engine = HandsOnEngine.buildFromArray(Sb(10000))
+  let dimensions = engine.getSheetsDimensions().get("Sheet1")!
+  console.log("\n== Simple big ==", dimensions)
+  batch(engine)
+
+  engine = HandsOnEngine.buildFromArray(Sb(100000))
+  dimensions = engine.getSheetsDimensions().get("Sheet1")!
+  console.log("\n== Simple big ==", dimensions)
+  batch(engine)
+
+  engine = HandsOnEngine.buildFromArray(Bs(100000))
+  dimensions = engine.getSheetsDimensions().get("Sheet1")!
+  console.log("\n== Big sum ==", dimensions)
+  batch(engine)
+}
+
+start()
--- benchmark/cruds/operations.ts
+++ benchmark/cruds/operations.ts
@@ -1,12 +1,11 @@
 import {HandsOnEngine} from "../../src";
-import {sheet as Sb} from '../sheets/01-simple-big'
 
 
 export function addColumns(engine: HandsOnEngine) {
   const dimensions = engine.getSheetsDimensions().get("Sheet1")!
 
   measure("Add column at the beginning", () => engine.addColumns(0, 0, 1))
-  measure("Add column in the middle   ", () => engine.addColumns(0, dimensions.width / 2, 1))
+  measure("Add column in the middle   ", () => engine.addColumns(0, half(dimensions.width), 1))
   measure("Add column at the end      ", () => engine.addColumns(0, dimensions.width, 1))
 }
 
@@ -14,7 +13,7 @@ export function addRows(engine: HandsOnEngine) {
   const dimensions = engine.getSheetsDimensions().get("Sheet1")!
 
   measure("Add row at the beginning", () => engine.addRows(0, 0, 1))
-  measure("Add row in the middle   ", () => engine.addRows(0, dimensions.height / 2, 1))
+  measure("Add row in the middle   ", () => engine.addRows(0, half(dimensions.height), 1))
   measure("Add row at the end      ", () => engine.addRows(0, dimensions.height, 1))
 }
 
@@ -22,7 +21,7 @@ export function removeColumns(engine: HandsOnEngine) {
   const dimensions = engine.getSheetsDimensions().get("Sheet1")!
 
   measure("Remove column at the beginning", () => engine.removeColumns(0, 0, 1))
-  measure("Remove column in the middle   ", () => engine.removeColumns(0, dimensions.width / 2, dimensions.width / 2))
+  measure("Remove column in the middle   ", () => engine.removeColumns(0, half(dimensions.width), half(dimensions.width)))
   measure("Remove column at the end      ", () => engine.removeColumns(0, 0, 1))
 }
 
@@ -30,10 +29,13 @@ export function removeRows(engine: HandsOnEngine) {
   const dimensions = engine.getSheetsDimensions().get("Sheet1")!
 
   measure("Remove row at the beginning", () => engine.removeRows(0, 0, 1))
-  measure("Remove row in the middle   ", () => engine.removeRows(0, dimensions.height / 2, dimensions.height / 2))
+  measure("Remove row in the middle   ", () => engine.removeRows(0, half(dimensions.height), half(dimensions.height)))
   measure("Remove row at the end      ", () => engine.removeRows(0, dimensions.height, dimensions.height))
 }
 
+function half(num: number) {
+  return Math.floor(num / 2)
+}
 
 function measure<T>(name: String, func: () => T): { time: number, result: T } {
   const start = Date.now()
@@ -43,17 +45,3 @@ function measure<T>(name: String, func: () => T): { time: number, result: T } {
   console.log(`${name}: ${time} ms`)
   return { time: time, result: result}
 }
-
-function init() {
-  let engine = HandsOnEngine.buildFromArray(Sb(100000))
-  let dimensions = engine.getSheetsDimensions().get("Sheet1")!
-
-  console.log("== Simple big ==", dimensions)
-
-  addRows(engine)
-  addColumns(engine)
-  removeRows(engine)
-  removeColumns(engine)
-}
-
-init()
