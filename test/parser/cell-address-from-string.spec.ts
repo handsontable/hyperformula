90981785dfb7bf491d043f59a723db7ddf18c5c2
Rafał Łasocha <me@swistak35.com>
2019-08-02 21:18:25 +0200

    Move files into parser spec dir

--- test/parser/cell-address-from-string.spec.ts
+++ test/parser/cell-address-from-string.spec.ts
@@ -1,7 +1,7 @@
-import {simpleCellAddress} from '../src/Cell'
-import {SheetMapping} from '../src/DependencyGraph'
-import {cellAddressFromString} from '../src/parser'
-import {CellAddress} from '../src/parser/CellAddress'
+import {simpleCellAddress} from '../../src/Cell'
+import {SheetMapping} from '../../src/DependencyGraph'
+import {cellAddressFromString} from '../../src/parser'
+import {CellAddress} from '../../src/parser/CellAddress'
 
 describe('cellAddressFromString', () => {
   it('is zero based', () => {
