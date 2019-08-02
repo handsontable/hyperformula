890ceb691d6c9900813cfed389dc540a2559c5d5
Rafał Łasocha <me@swistak35.com>
2019-08-02 22:59:51 +0200

    Add two more tests for Config

--- test/config.spec.ts
+++ test/config.spec.ts
@@ -13,4 +13,16 @@ describe('Config', () => {
 
     expect(config.language).toBe(Config.defaultConfig.language)
   })
+
+  it('computes list of volatile functions according to translation', () => {
+    const config = new Config({ language: plPL })
+
+    expect(config.volatileFunctions()).toContain('LOSUJ')
+  })
+
+  it('can translate functions', () => {
+    const config = new Config({ language: plPL })
+
+    expect(config.getFunctionTranslationFor('SUM')).toEqual('SUMA')
+  })
 })
