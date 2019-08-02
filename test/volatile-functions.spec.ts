bd3163d83b38aeacdf28a8675756a893322f7547
Rafał Łasocha <me@swistak35.com>
2019-08-02 23:10:18 +0200

    Add two more cases for volatile formulas

--- test/volatile-functions.spec.ts
+++ test/volatile-functions.spec.ts
@@ -15,6 +15,17 @@ describe('Interpreter - function RAND', () => {
     expect(engine.getCellValue('A1')).not.toEqual(valueBeforeRecomputation)
   })
 
+  it('cell which is dependent on volatile formula is also recomputed', () => {
+    const engine = HandsOnEngine.buildFromArray([
+      ['=RAND()', '42', '=A1'],
+    ])
+    const valueBeforeRecomputation = engine.getCellValue('C1')
+
+    engine.setCellContent(adr('B1'), '35')
+
+    expect(engine.getCellValue('C1')).not.toEqual(valueBeforeRecomputation)
+  })
+
   it('formula can be recognized as volatile even if entered later', () => {
     const engine = HandsOnEngine.buildFromArray([
       ['=A2+A3', '42'],
@@ -34,5 +45,16 @@ describe('Interpreter - function RAND', () => {
     engine.setCellContent(adr('A1'), '35')
 
     expect(engine.dependencyGraph!.verticesToRecompute()).toEqual(new Set())
+  })
+
+  it('volatile formula after moving is still volatile', () => {
+    const engine = HandsOnEngine.buildFromArray([
+      ['=RAND()', '42'],
+    ])
+
+    engine.moveCells(adr('A1'), 1, 1, adr('A2'))
+
+    const a2 = engine.addressMapping!.getCell(adr('A2'))
+    expect(engine.dependencyGraph!.volatileVertices()).toEqual(new Set([a2]))
   })
 })
