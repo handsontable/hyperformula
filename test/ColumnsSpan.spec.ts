db54aaa68441ff6ac2a2acb123d69e69ef827eb7
Rafał Łasocha <me@swistak35.com>
2019-08-27 08:59:35 +0200

    Add tests for ColumnsSpan

---
+++ test/ColumnsSpan.spec.ts
@@ -0,0 +1,84 @@
+import {ColumnsSpan} from '../src/ColumnsSpan'
+
+describe("ColumnsSpan", () => {
+  it("raise error when starting column is less than 0", () => {
+    expect(() => {
+      const span = new ColumnsSpan(0, -1, 0)
+    }).toThrow("Starting column cant be less than 0")
+  })
+
+  it("raise error when column end before column start", () => {
+    expect(() => {
+      const span = new ColumnsSpan(0, 1, 0)
+    }).toThrow("Column span cant end before start")
+  })
+
+  it("#numberOfColumns for one column", () => {
+    const span = new ColumnsSpan(0, 42, 42)
+
+    expect(span.numberOfColumns).toBe(1)
+  })
+
+  it("#numberOfColumns for more than one column", () => {
+    const span = new ColumnsSpan(0, 42, 45)
+
+    expect(span.numberOfColumns).toBe(4)
+  })
+
+  it("#columns iterates over column numbers", () => {
+    const span = new ColumnsSpan(0, 42, 45)
+
+    expect(Array.from(span.columns())).toEqual([42, 43, 44, 45])
+  })
+
+  it("#intersect with span from other sheet", () => {
+    const span1 = new ColumnsSpan(0, 42, 45)
+    const span2 = new ColumnsSpan(1, 43, 45)
+
+    expect(() => {
+      span1.intersect(span2)
+    }).toThrow("Can't intersect spans from different sheets")
+  })
+
+  it("#intersect with span overlapping at right", () => {
+    const span1 = new ColumnsSpan(0, 42, 45)
+    const span2 = new ColumnsSpan(0, 43, 46)
+
+    expect(span1.intersect(span2)).toEqual(new ColumnsSpan(0, 43, 45))
+  })
+
+  it("#intersect with span whole after", () => {
+    const span1 = new ColumnsSpan(0, 42, 45)
+    const span2 = new ColumnsSpan(0, 46, 49)
+
+    expect(span1.intersect(span2)).toEqual(null)
+  })
+
+  it("#intersect with span overlapping at left", () => {
+    const span1 = new ColumnsSpan(0, 42, 45)
+    const span2 = new ColumnsSpan(0, 40, 44)
+
+    expect(span1.intersect(span2)).toEqual(new ColumnsSpan(0, 42, 44))
+  })
+
+  it("#intersect with span whole before", () => {
+    const span1 = new ColumnsSpan(0, 42, 45)
+    const span2 = new ColumnsSpan(0, 39, 41)
+
+    expect(span1.intersect(span2)).toEqual(null)
+  })
+
+  it("#intersect with span included", () => {
+    const span1 = new ColumnsSpan(0, 42, 45)
+    const span2 = new ColumnsSpan(0, 43, 44)
+
+    expect(span1.intersect(span2)).toEqual(span2)
+  })
+
+  it("#intersect with span outside", () => {
+    const span1 = new ColumnsSpan(0, 42, 45)
+    const span2 = new ColumnsSpan(0, 40, 47)
+
+    expect(span1.intersect(span2)).toEqual(span1)
+  })
+})
