4878e42256f6e6638addd3f05d0c329d84ed60ed
Jakub Kowalski <kowalski_jakub@hotmail.com>
2019-09-09 16:12:00 +0200

    Basic ColumnIndex crud tests

--- test/column-index.spec.ts
+++ test/column-index.spec.ts
@@ -2,9 +2,10 @@ import {ColumnIndex} from "../src/ColumnIndex";
 import {adr} from "./testUtils";
 import {AbsoluteCellRange} from "../src/AbsoluteCellRange";
 import {Statistics} from "../src/statistics/Statistics";
+import {simpleCellAddress} from "../src/Cell";
 
 describe("Column index", () => {
-  it('should add value to empty index', function () {
+  it('should add value to empty index', () => {
     const index = new ColumnIndex(new Statistics())
     index.add(1, adr("B5"))
 
@@ -14,7 +15,7 @@ describe("Column index", () => {
     expect(columnMap.get(1)![0]).toBe(4)
   });
 
-  it('should keep values in sorted order', function () {
+  it('should keep values in sorted order', () => {
     const index = new ColumnIndex(new Statistics())
     index.add(1, adr("A3"))
     index.add(1, adr("A5"))
@@ -26,7 +27,7 @@ describe("Column index", () => {
     expect(columnMap.get(1)![0]).toBe(0)
   });
 
-  it('should not store duplicates', function () {
+  it('should not store duplicates', () => {
     const index = new ColumnIndex(new Statistics())
     index.add(1, adr("A1"))
     index.add(1, adr("A5"))
@@ -39,4 +40,46 @@ describe("Column index", () => {
     expect(columnMap.size).toBe(1)
     expect(columnMap.get(1)!.length).toBe(2)
   });
+})
+
+describe('ColumnIndex cruds', () => {
+  it('should remove value from index', () => {
+    const index = new ColumnIndex(new Statistics())
+    index.add(1, adr("A1"))
+    index.add(1, adr("A2"))
+    index.add(1, adr("A3"))
+
+    index.remove(1, simpleCellAddress(0, 0, 1))
+
+    const valueIndex = index.getColumnMap(0, 0).get(1)!
+    expect(valueIndex.length).toBe(2)
+    expect(valueIndex).toContain(0)
+    expect(valueIndex).toContain(2)
+  })
+
+  it('should do nothing if passed value is null', () => {
+    const index = new ColumnIndex(new Statistics())
+    index.add(1, adr("A1"))
+    index.add(1, adr("A2"))
+    index.add(1, adr("A3"))
+
+    index.remove(null, simpleCellAddress(0, 0, 1))
+
+    const valueIndex = index.getColumnMap(0, 0).get(1)!
+    expect(valueIndex.length).toBe(3)
+    expect(valueIndex).toContain(0)
+    expect(valueIndex).toContain(1)
+    expect(valueIndex).toContain(2)
+  })
+
+  it('should change value in index', () => {
+    const index = new ColumnIndex(new Statistics())
+    index.add(1, adr("A1"))
+
+    index.change(1, 2, simpleCellAddress(0, 0, 0))
+
+    expect(index.getColumnMap(0, 0).keys()).not.toContain(1)
+    const valueIndex = index.getColumnMap(0, 0).get(2)!
+    expect(valueIndex).toContain(0)
+  })
 })
