e87dafbebfe33eba14cae4d07d7295fedb65618c
Jakub Kowalski <kowalski_jakub@hotmail.com>
2019-09-13 09:59:08 +0200

    ColumnIndex#removeRows specs

--- test/column-index.spec.ts
+++ test/column-index.spec.ts
@@ -162,23 +162,19 @@ describe('ColumnIndex#addRows', () => {
 })
 
 describe('ColumnIndex#removeRows', () => {
-  it('should shift rows when removing rows', () => {
+  it('should remove rows', () => {
+    const index = new ColumnIndex(new Statistics())
+    index.add(1, adr("A1"))
+
+    index.removeRows(RowsSpan.fromNumberOfRows(0, 0, 1))
+
+    expect(index.getValueIndex(0, 0, 1)).toEqual([])
+  })
+
+  it('should remove rows in the middle ', () => {
     const index = new ColumnIndex(new Statistics())
     index.add(1, adr("A1"))
     index.add(1, adr("A2"))
-    index.add(1, adr("A3"))
-    index.add(1, adr("A4"))
-    index.add(1, adr("A5"))
-
-    index.removeRows(RowsSpan.fromNumberOfRows(0, 1, 2))
-
-    expect(index.getValueIndex(0, 0, 1)).toEqual([0, 1, 2])
-  })
-
-  it('should only shift rows', () => {
-    const index = new ColumnIndex(new Statistics())
-    index.add(1, adr("A1"))
-    index.add(2, adr("A2"))
     index.add(1, adr("A3"))
     index.add(1, adr("A4"))
 
@@ -187,14 +183,53 @@ describe('ColumnIndex#removeRows', () => {
     expect(index.getValueIndex(0, 0, 1)).toEqual([0, 1])
   })
 
-  it('should only shift rows 2', () => {
+  it('should remove rows in every column', () => {
+    const index = new ColumnIndex(new Statistics())
+    index.add(1, adr("A2"))
+    index.add(1, adr("B2"))
+    index.add(1, adr("C2"))
+
+    index.removeRows(RowsSpan.fromNumberOfRows(0, 0, 1))
+
+    expect(index.getValueIndex(0, 0, 1)).toEqual([0])
+    expect(index.getValueIndex(0, 1, 1)).toEqual([0])
+    expect(index.getValueIndex(0, 2, 1)).toEqual([0])
+  })
+
+  it('should remove rows for different values', () => {
+    const index = new ColumnIndex(new Statistics())
+    index.add(1, adr("A2"))
+    index.add(2, adr("A3"))
+    index.add(3, adr("A4"))
+    index.add(4, adr("B3"))
+
+    index.removeRows(RowsSpan.fromNumberOfRows(0, 0, 2))
+
+    expect(index.getValueIndex(0, 0, 1)).toEqual([])
+    expect(index.getValueIndex(0, 0, 2)).toEqual([0])
+    expect(index.getValueIndex(0, 0, 3)).toEqual([1])
+    expect(index.getValueIndex(0, 1, 4)).toEqual([0])
+  })
+
+  it('should remove rows only in one sheet', () => {
+    const index = new ColumnIndex(new Statistics())
+    index.add(1, adr("A2"))
+    index.add(1, adr("A2", 1))
+
+    index.removeRows(RowsSpan.fromNumberOfRows(0, 0, 1))
+
+    expect(index.getValueIndex(0, 0, 1)).toEqual([0])
+    expect(index.getValueIndex(1, 0, 1)).toEqual([1])
+  })
+
+  it('should remove proper rows', () => {
     const index = new ColumnIndex(new Statistics())
     index.add(1, adr("A1"))
-    index.add(2, adr("A2"))
-    index.add(2, adr("A3"))
+    index.add(1, adr("A3"))
     index.add(1, adr("A4"))
+    index.add(1, adr("A6"))
 
-    index.removeRows(RowsSpan.fromNumberOfRows(0, 1, 2))
+    index.removeRows(RowsSpan.fromNumberOfRows(0, 1, 4))
 
     expect(index.getValueIndex(0, 0, 1)).toEqual([0, 1])
   })
