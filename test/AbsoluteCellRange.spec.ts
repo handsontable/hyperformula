62d0448ac9b66dd361b2919cd4d494a51d052312
Rafał Łasocha <me@swistak35.com>
2019-10-04 20:35:00 +0200

    Add spec for AbsoluteCellRange

---
+++ test/AbsoluteCellRange.spec.ts
@@ -0,0 +1,19 @@
+import {AbsoluteCellRange} from '../src/AbsoluteCellRange'
+import {adr} from './testUtils'
+import './testConfig'
+
+describe('AbsoluteCellRange', () => {
+  describe('#addressInRange', () => {
+    it('true in simplest case', () => {
+      const range = AbsoluteCellRange.fromCoordinates(0, 0, 0, 0, 0)
+
+      expect(range.addressInRange(adr('A1')))
+    })
+
+    it('false if different sheets', () => {
+      const range = AbsoluteCellRange.fromCoordinates(1, 0, 0, 0, 0)
+
+      expect(range.addressInRange(adr('A1')))
+    })
+  })
+})
