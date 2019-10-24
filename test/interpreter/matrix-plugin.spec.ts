347f72be12d6d3d2323b0488fa1a501363fdc865
Rafał Łasocha <me@swistak35.com>
2019-10-24 14:02:31 +0200

    Extract common config

--- test/interpreter/matrix-plugin.spec.ts
+++ test/interpreter/matrix-plugin.spec.ts
@@ -4,9 +4,10 @@ import {CellError, ErrorType} from '../../src/Cell'
 import {MatrixPlugin} from '../../src/interpreter/plugin/MatrixPlugin'
 import '../testConfig.ts'
 
+const configWithMatrixPlugin = new Config({functionPlugins: [MatrixPlugin]})
+
 describe('Matrix plugin', () => {
   it('matrix multiplication', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin]})
     const engine = HandsOnEngine.buildFromArray([
       ['1', '2'],
       ['3', '4'],
@@ -14,7 +15,7 @@ describe('Matrix plugin', () => {
       ['1', '2'],
       ['3', '4'],
       ['{=MMULT(A1:B3,A4:B5)}'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('A6')).toBeCloseTo(7)
     expect(engine.getCellValue('B6')).toBeCloseTo(10)
@@ -25,7 +26,6 @@ describe('Matrix plugin', () => {
   })
 
   it('matrix multiplication wrong size', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin], matrixDetection: true})
     const engine = HandsOnEngine.buildFromArray([
       ['1', '2'],
       ['3', '4'],
@@ -34,20 +34,19 @@ describe('Matrix plugin', () => {
       ['4', '5', '6'],
       ['7', '8', '9'],
       ['{=mmult(A1:B3,A4:C6)}'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('A7')).toEqual(new CellError(ErrorType.VALUE))
     expect(engine.getCellValue('B7')).toEqual(EmptyValue)
   })
 
   it('matrix multiplication with string in data', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin]})
     const engine = HandsOnEngine.buildFromArray([
       ['1', '2', '{=MMULT(A1:B2,A3:B4)}'],
       ['3', 'foo'],
       ['1', '2', '{=MMULT(A3:B4,A1:B2)}'],
       ['3', '4'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('C1')).toEqual(new CellError(ErrorType.VALUE))
     expect(engine.getCellValue('D1')).toEqual(new CellError(ErrorType.VALUE))
@@ -60,12 +59,11 @@ describe('Matrix plugin', () => {
   })
 
   it('nested matrix multiplication', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin]})
     const engine = HandsOnEngine.buildFromArray([
       ['1', '2'],
       ['3', '4'],
       ['{=MMULT(A1:B2, MMULT(A1:B2,A1:B2))}'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('A3')).toEqual(37)
     expect(engine.getCellValue('B3')).toEqual(54)
@@ -74,12 +72,11 @@ describe('Matrix plugin', () => {
   })
 
   it('mmult of other mmult', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin]})
     const engine = HandsOnEngine.buildFromArray([
       ['1', '2', '{=MMULT(A1:B2, A1:B2)}', '{=MMULT(A1:B2, A1:B2)}'],
       ['3', '4', '{=MMULT(A1:B2, A1:B2)}', '{=MMULT(A1:B2, A1:B2)}'],
       ['{=MMULT(A1:B2, C1:D2)}'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('A3')).toEqual(37)
     expect(engine.getCellValue('B3')).toEqual(54)
@@ -88,22 +85,20 @@ describe('Matrix plugin', () => {
   })
 
   it('mmult of a number', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin]})
     const engine = HandsOnEngine.buildFromArray([
       ['{=MMULT(3, 4)}'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('A1')).toEqual(12)
   })
 
   it('matrix transpose', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin]})
     const engine = HandsOnEngine.buildFromArray([
       ['1', '2'],
       ['3', '4'],
       ['5', '6'],
       ['{=transpose(A1:B3)}'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('A4')).toBeCloseTo(1)
     expect(engine.getCellValue('B4')).toBeCloseTo(3)
@@ -114,19 +109,17 @@ describe('Matrix plugin', () => {
   })
 
   xit('transpose works even if theres an error', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin]})
     const engine = HandsOnEngine.buildFromArray([
       ['1'],
       ['=3/0'],
       ['{=TRANSPOSE(A1:A2)}'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('A3')).toBeCloseTo(1)
     expect(engine.getCellValue('B3')).toEqual(new CellError(ErrorType.DIV_BY_ZERO))
   })
 
   it('matrix multiplication by sumproduct', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin], matrixDetection: true })
     const engine = HandsOnEngine.buildFromArray([
       ['1', '2'],
       ['3', '4'],
@@ -136,7 +129,7 @@ describe('Matrix plugin', () => {
       ['=SUMPRODUCT($A1:$B1,transpose(A$4:A$5))', '=SUMPRODUCT($A1:$B1,transpose(B$4:B$5))'],
       ['=SUMPRODUCT($A2:$B2,transpose(A$4:A$5))', '=SUMPRODUCT($A2:$B2,transpose(B$4:B$5))'],
       ['=SUMPRODUCT($A3:$B3,transpose(A$4:A$5))', '=SUMPRODUCT($A3:$B3,transpose(B$4:B$5))'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('A6')).toBeCloseTo(7)
     expect(engine.getCellValue('B6')).toBeCloseTo(10)
@@ -147,27 +140,25 @@ describe('Matrix plugin', () => {
   })
 
   it('matrix maxpool', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin]})
     const engine = HandsOnEngine.buildFromArray([
       ['1', '2', '3', '4', '5', '6'],
       ['11', '12', '13', '14', '15', '16'],
       ['21', '22', '23', '24', '25', '26'],
       ['{=maxpool(A1:F3,3)}'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('A4')).toBeCloseTo(23)
     expect(engine.getCellValue('B4')).toBeCloseTo(26)
   })
 
   it('matrix maxpool, custom stride', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin]})
     const engine = HandsOnEngine.buildFromArray([
       ['1', '2', '3', '4', '5', '6'],
       ['11', '12', '13', '14', '15', '16'],
       ['21', '22', '23', '24', '25', '26'],
       ['28', '29', '30', '31', '32', '33'],
       ['{=maxpool(A1:F4,3,1)}'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('A5')).toBeCloseTo(23)
     expect(engine.getCellValue('A6')).toBeCloseTo(30)
@@ -180,12 +171,11 @@ describe('Matrix plugin', () => {
   })
 
   it('matrix medianpool on even square', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin]})
     const engine = HandsOnEngine.buildFromArray([
       ['1', '2', '1', '2', '1', '5'],
       ['3', '4', '3', '7', '6', '7'],
       ['{=medianpool(A1:F2,2)}'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('A3')).toBeCloseTo(2.5)
     expect(engine.getCellValue('B3')).toBeCloseTo(2.5)
@@ -193,7 +183,6 @@ describe('Matrix plugin', () => {
   })
 
   it('matrix medianpool on odd square', () => {
-    const config = new Config({functionPlugins: [MatrixPlugin]})
     const engine = HandsOnEngine.buildFromArray([
       ['1', '1', '1'], // right shot from the beginning
       ['1', '2', '3'],
@@ -208,7 +197,7 @@ describe('Matrix plugin', () => {
       ['8', '8', '8'],
 
       ['{=medianpool(A1:C9,3)}'],
-    ], config)
+    ], configWithMatrixPlugin)
 
     expect(engine.getCellValue('A10')).toBeCloseTo(2)
     expect(engine.getCellValue('A11')).toBeCloseTo(4)
