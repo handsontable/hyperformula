c1b86cfd1a13b1c2475111762c0ce3dd038431b0
Rafał Łasocha <me@swistak35.com>
2019-11-04 21:22:57 +0100

    More tests for COUNTBLANK

--- test/interpreter/function-countblank.spec.ts
+++ test/interpreter/function-countblank.spec.ts
@@ -18,4 +18,22 @@ describe('COUNTBLANK', () => {
     const engine = HyperFormula.buildFromArray([['1', '', '', '=COUNTBLANK(A1:C1)']])
     expect(engine.getCellValue(adr('D1'))).toEqual(2)
   })
+
+  it('does not propagate errors from ranges', () => {
+    const engine = HyperFormula.buildFromArray([
+      [''],
+      ['=4/0'],
+      ['=COUNTBLANK(A1:A2)']
+    ])
+
+    expect(engine.getCellValue(adr('A3'))).toEqual(1)
+  })
+
+  it('does not propagate errors from arguments', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=COUNTBLANK(4/0)']
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqual(0)
+  })
 })
