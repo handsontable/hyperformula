22976661e867a02ac92ad3fc8b36fe04a430a370
Jakub Kowalski <kowalski_jakub@hotmail.com>
2019-11-07 15:27:56 +0100

    simpleCellAddressFromString specs

--- test/cell-address-to-string.spec.ts
+++ test/cell-address-to-string.spec.ts
@@ -1,4 +1,5 @@
-import {sheetCellAddress, sheetCellAddressToString} from '../src/Cell'
+import {sheetCellAddress, sheetCellAddressToString, simpleCellAddress} from '../src/Cell'
+import {simpleCellAddressFromString} from "../src/parser/cellAddressFromString";
 
 describe('sheetCellAddressToString', () => {
   it('is zero based', () => {
@@ -16,5 +17,33 @@ describe('sheetCellAddressToString', () => {
 
   it('works for many letters', () => {
     expect(sheetCellAddressToString(sheetCellAddress(730, 0))).toBe('ABC1')
+  })
+})
+
+describe('simpleCellAddressFromString', () => {
+  const sheetMappingFunction = (name: string): number | undefined => {
+    const index =  ['Sheet1', 'Sheet2', 'Sheet3'].indexOf(name)
+    return index > 0 ? index : undefined
+  }
+
+  it('should return simple cell address', () => {
+    expect(simpleCellAddressFromString(sheetMappingFunction, 'A1')).toEqual(simpleCellAddress(0, 0, 0))
+    expect(simpleCellAddressFromString(sheetMappingFunction, 'AY7')).toEqual(simpleCellAddress(0, 50, 6))
+  })
+
+  it('should return undefined', () => {
+    expect(simpleCellAddressFromString(sheetMappingFunction, '$Sheet4.A1')).toBeUndefined()
+  })
+
+  it('should return address with overridden sheet', () => {
+    expect(simpleCellAddressFromString(sheetMappingFunction, 'A1', 1)).toEqual(simpleCellAddress(1, 0, 0))
+  })
+
+  it('should return address with sheet number from sheet mapping', () => {
+    expect(simpleCellAddressFromString(sheetMappingFunction, '$Sheet2.A1')).toEqual(simpleCellAddress(1, 0, 0))
+  })
+
+  it('should return address with sheet number from sheet mapping regardless of override parameter', () => {
+    expect(simpleCellAddressFromString(sheetMappingFunction, '$Sheet3.A1', 1)).toEqual(simpleCellAddress(2, 0, 0))
   })
 })
