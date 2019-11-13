9837d8559eaf0980538d62bb73ea2fd08a40ddfd
Rafał Łasocha <me@swistak35.com>
2019-11-13 23:54:17 +0100

    Add YEAR tests for coercions and errors

--- test/interpreter/function-year.spec.ts
+++ test/interpreter/function-year.spec.ts
@@ -4,7 +4,19 @@ import '../testConfig'
 import {adr, dateNumberToString} from '../testUtils'
 
 describe('Function YEAR', () => {
-  it('function YEAR with numerical arguments', () => {
+  it('validate arguments', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=YEAR(1, 2)'],
+      ['=YEAR()'],
+      ['=YEAR("foo")'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqual(new CellError(ErrorType.NA))
+    expect(engine.getCellValue(adr('A2'))).toEqual(new CellError(ErrorType.NA))
+    expect(engine.getCellValue(adr('A3'))).toEqual(new CellError(ErrorType.VALUE))
+  })
+
+  it('with numerical arguments', () => {
     const engine = HyperFormula.buildFromArray([['=YEAR(0)', '=YEAR(2)', '=YEAR(43465)']])
 
     expect(engine.getCellValue(adr('A1'))).toEqual(1899)
@@ -12,7 +24,7 @@ describe('Function YEAR', () => {
     expect(engine.getCellValue(adr('C1'))).toEqual(2018)
   })
 
-  it('function YEAR with string arguments', () => {
+  it('with string arguments', () => {
     const engine = HyperFormula.buildFromArray([['=YEAR("12/31/1899")', '=YEAR("01/01/1900")', '=YEAR("12/31/2018")']])
 
     expect(engine.getCellValue(adr('A1'))).toEqual(1899)
@@ -20,12 +32,33 @@ describe('Function YEAR', () => {
     expect(engine.getCellValue(adr('C1'))).toEqual(2018)
   })
 
-  it('function YEAR with wrong arguments', () => {
-    const engine = HyperFormula.buildFromArray([['=YEAR("foo")', '=YEAR("30/12/2018")', '=YEAR(1, 2)', '=YEAR()']])
+  it('use datenumber coercion for 1st argument', () => {
+    const engine =  HyperFormula.buildFromArray([
+      ['=YEAR(TRUE())'],
+      ['=YEAR(1)'],
+    ])
 
-    expect(engine.getCellValue(adr('A1'))).toEqual(new CellError(ErrorType.VALUE))
+    expect(engine.getCellValue(adr('A1'))).toEqual(1899)
+    expect(engine.getCellValue(adr('A2'))).toEqual(1899)
+  })
+
+  it('propagate errors', () => {
+    const engine =  HyperFormula.buildFromArray([
+      ['=YEAR(4/0)'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqual(new CellError(ErrorType.DIV_BY_ZERO))
+  })
+
+  // Inconsistency with Product 1
+  it('range value in 1st argument results in VALUE error', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=DATE(2019, 3, 31)', '=YEAR(A1:A3)'],
+      ['=DATE(2018, 3, 31)', '=YEAR(A1:A3)'],
+      ['=DATE(2017, 3, 31)'],
+    ])
+
     expect(engine.getCellValue(adr('B1'))).toEqual(new CellError(ErrorType.VALUE))
-    expect(engine.getCellValue(adr('C1'))).toEqual(new CellError(ErrorType.NA))
-    expect(engine.getCellValue(adr('D1'))).toEqual(new CellError(ErrorType.NA))
+    expect(engine.getCellValue(adr('B2'))).toEqual(new CellError(ErrorType.VALUE))
   })
 })
