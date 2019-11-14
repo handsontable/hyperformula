053cecb375dc0d4a21bc9915f7ad17f2d067b6cf
Rafał Łasocha <me@swistak35.com>
2019-11-14 20:22:04 +0100

    Renamed tests

--- test/interpreter/function-split.spec.ts
+++ test/interpreter/function-split.spec.ts
@@ -3,39 +3,39 @@ import {CellError, ErrorType} from '../../src/Cell'
 import {adr} from '../testUtils'
 import '../testConfig'
 
-describe('Interpreter', () => {
-  it('function SPLIT happy path', () => {
+describe('Function SPLIT', () => {
+  it('happy path', () => {
     const engine = HyperFormula.buildFromArray([['some words', '=SPLIT(A1, 0)']])
 
     expect(engine.getCellValue(adr('B1'))).toEqual('some')
   })
 
-  it('function SPLIT bigger index', () => {
+  it('bigger index', () => {
     const engine = HyperFormula.buildFromArray([['some words', '=SPLIT(A1, 1)']])
 
     expect(engine.getCellValue(adr('B1'))).toEqual('words')
   })
 
-  it('function SPLIT when continuous delimeters', () => {
+  it('when continuous delimeters', () => {
     const engine = HyperFormula.buildFromArray([['some  words', '=SPLIT(A1, 1)', '=SPLIT(A1, 2)']])
 
     expect(engine.getCellValue(adr('B1'))).toEqual('')
     expect(engine.getCellValue(adr('C1'))).toEqual('words')
   })
 
-  it('function SPLIT when 1st arg not a string', () => {
+  it('coerce first argument to string', () => {
     const engine = HyperFormula.buildFromArray([['42', '=SPLIT(A1, 1)']])
 
     expect(engine.getCellValue(adr('B1'))).toEqual(new CellError(ErrorType.VALUE))
   })
 
-  it('function SPLIT when 2nd arg not a number', () => {
+  it('when 2nd arg not a number', () => {
     const engine = HyperFormula.buildFromArray([['some words', '=SPLIT(A1, "foo")']])
 
     expect(engine.getCellValue(adr('B1'))).toEqual(new CellError(ErrorType.VALUE))
   })
 
-  it('function SPLIT when index arg is not value within bounds', () => {
+  it('when index arg is not value within bounds', () => {
     const engine = HyperFormula.buildFromArray([['some words', '=SPLIT(A1, 17)', '=SPLIT(A1, -1)']])
 
     expect(engine.getCellValue(adr('B1'))).toEqual(new CellError(ErrorType.VALUE))
