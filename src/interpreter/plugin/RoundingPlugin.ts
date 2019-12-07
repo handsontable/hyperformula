b3752ae004ad4e7402ea5403b98660b3458d1918
Rafał Łasocha <me@swistak35.com>
2019-12-07 18:59:54 +0100

    Refactoring: Extract method (common part of ROUNDDOWN/ROUNDUP/ROUND/TRUNC)

--- src/interpreter/plugin/RoundingPlugin.ts
+++ src/interpreter/plugin/RoundingPlugin.ts
@@ -4,6 +4,8 @@ import {FunctionPlugin} from './FunctionPlugin'
 import {SimpleRangeValue} from '../InterpreterValue'
 import {coerceScalarToNumber} from '../coerce'
 
+type RoundingFunction = (numberToRound: number, places: number) => number
+
 export class RoundingPlugin extends FunctionPlugin {
   public static implementedFunctions = {
     roundup: {
@@ -24,111 +26,36 @@ export class RoundingPlugin extends FunctionPlugin {
   }
 
   public roundup(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
-    if (ast.args.length < 1 || ast.args.length > 2) {
-      return new CellError(ErrorType.NA)
-    } else {
-      const numberToRound = this.evaluateAst(ast.args[0], formulaAddress)
-      if (numberToRound instanceof SimpleRangeValue) {
-        return new CellError(ErrorType.VALUE)
-      }
-
-      let coercedPlaces
-      if (ast.args[1]) {
-        const places = this.evaluateAst(ast.args[1], formulaAddress)
-        if (places instanceof SimpleRangeValue) {
-          return new CellError(ErrorType.VALUE)
-        }
-        coercedPlaces = coerceScalarToNumber(places)
+    return this.commonArgumentsHandling(ast, formulaAddress, (numberToRound: number, places: number): number => {
+      const placesMultiplier = Math.pow(10, places)
+      if (numberToRound < 0) {
+        return -Math.ceil(-numberToRound * placesMultiplier) / placesMultiplier
       } else {
-        coercedPlaces = 0
+        return Math.ceil(numberToRound * placesMultiplier) / placesMultiplier
       }
-
-      const coercedNumberToRound = coerceScalarToNumber(numberToRound)
-      if (coercedNumberToRound instanceof CellError) {
-        return coercedNumberToRound
-      } else if (coercedPlaces instanceof CellError) {
-        return coercedPlaces
-      } else {
-        const placesMultiplier = Math.pow(10, coercedPlaces)
-        if (coercedNumberToRound < 0) {
-          return -Math.ceil(-coercedNumberToRound * placesMultiplier) / placesMultiplier
-        } else {
-          return Math.ceil(coercedNumberToRound * placesMultiplier) / placesMultiplier
-        }
-      }
-    }
+    })
   }
 
   public rounddown(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
-    if (ast.args.length < 1 || ast.args.length > 2) {
-      return new CellError(ErrorType.NA)
-    } else {
-      const numberToRound = this.evaluateAst(ast.args[0], formulaAddress)
-      if (numberToRound instanceof SimpleRangeValue) {
-        return new CellError(ErrorType.VALUE)
-      }
-
-      let coercedPlaces
-      if (ast.args[1]) {
-        const places = this.evaluateAst(ast.args[1], formulaAddress)
-        if (places instanceof SimpleRangeValue) {
-          return new CellError(ErrorType.VALUE)
-        }
-        coercedPlaces = coerceScalarToNumber(places)
+    return this.commonArgumentsHandling(ast, formulaAddress, (numberToRound: number, places: number): number => {
+      const placesMultiplier = Math.pow(10, places)
+      if (numberToRound < 0) {
+        return -Math.floor(-numberToRound * placesMultiplier) / placesMultiplier
       } else {
-        coercedPlaces = 0
+        return Math.floor(numberToRound * placesMultiplier) / placesMultiplier
       }
-
-      const coercedNumberToRound = coerceScalarToNumber(numberToRound)
-      if (coercedNumberToRound instanceof CellError) {
-        return coercedNumberToRound
-      } else if (coercedPlaces instanceof CellError) {
-        return coercedPlaces
-      } else {
-        const placesMultiplier = Math.pow(10, coercedPlaces)
-        if (coercedNumberToRound < 0) {
-          return -Math.floor(-coercedNumberToRound * placesMultiplier) / placesMultiplier
-        } else {
-          return Math.floor(coercedNumberToRound * placesMultiplier) / placesMultiplier
-        }
-      }
-    }
+    })
   }
 
   public round(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
-    if (ast.args.length < 1 || ast.args.length > 2) {
-      return new CellError(ErrorType.NA)
-    } else {
-      const numberToRound = this.evaluateAst(ast.args[0], formulaAddress)
-      if (numberToRound instanceof SimpleRangeValue) {
-        return new CellError(ErrorType.VALUE)
-      }
-
-      let coercedPlaces
-      if (ast.args[1]) {
-        const places = this.evaluateAst(ast.args[1], formulaAddress)
-        if (places instanceof SimpleRangeValue) {
-          return new CellError(ErrorType.VALUE)
-        }
-        coercedPlaces = coerceScalarToNumber(places)
+    return this.commonArgumentsHandling(ast, formulaAddress, (numberToRound: number, places: number): number => {
+      const placesMultiplier = Math.pow(10, places)
+      if (numberToRound < 0) {
+        return -Math.round(-numberToRound * placesMultiplier) / placesMultiplier
       } else {
-        coercedPlaces = 0
+        return Math.round(numberToRound * placesMultiplier) / placesMultiplier
       }
-
-      const coercedNumberToRound = coerceScalarToNumber(numberToRound)
-      if (coercedNumberToRound instanceof CellError) {
-        return coercedNumberToRound
-      } else if (coercedPlaces instanceof CellError) {
-        return coercedPlaces
-      } else {
-        const placesMultiplier = Math.pow(10, coercedPlaces)
-        if (coercedNumberToRound < 0) {
-          return -Math.round(-coercedNumberToRound * placesMultiplier) / placesMultiplier
-        } else {
-          return Math.round(coercedNumberToRound * placesMultiplier) / placesMultiplier
-        }
-      }
-    }
+    })
   }
 
   public trunc(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
@@ -153,6 +80,37 @@ export class RoundingPlugin extends FunctionPlugin {
         } else {
           return Math.floor(coercedNumberToRound)
         }
+      }
+    }
+  }
+
+  private commonArgumentsHandling(ast: ProcedureAst, formulaAddress: SimpleCellAddress, roundingFunction: RoundingFunction): CellValue {
+    if (ast.args.length < 1 || ast.args.length > 2) {
+      return new CellError(ErrorType.NA)
+    } else {
+      const numberToRound = this.evaluateAst(ast.args[0], formulaAddress)
+      if (numberToRound instanceof SimpleRangeValue) {
+        return new CellError(ErrorType.VALUE)
+      }
+
+      let coercedPlaces
+      if (ast.args[1]) {
+        const places = this.evaluateAst(ast.args[1], formulaAddress)
+        if (places instanceof SimpleRangeValue) {
+          return new CellError(ErrorType.VALUE)
+        }
+        coercedPlaces = coerceScalarToNumber(places)
+      } else {
+        coercedPlaces = 0
+      }
+
+      const coercedNumberToRound = coerceScalarToNumber(numberToRound)
+      if (coercedNumberToRound instanceof CellError) {
+        return coercedNumberToRound
+      } else if (coercedPlaces instanceof CellError) {
+        return coercedPlaces
+      } else {
+        return roundingFunction(coercedNumberToRound, coercedPlaces)
       }
     }
   }
