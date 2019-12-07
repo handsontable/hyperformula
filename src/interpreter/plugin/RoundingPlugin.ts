49e4a25c670a87cd5b073dc8874898b16b4961a1
Rafał Łasocha <me@swistak35.com>
2019-12-07 18:33:25 +0100

    Add function ROUNDDOWN

--- src/interpreter/plugin/RoundingPlugin.ts
+++ src/interpreter/plugin/RoundingPlugin.ts
@@ -9,6 +9,9 @@ export class RoundingPlugin extends FunctionPlugin {
     roundup: {
       translationKey: 'ROUNDUP',
     },
+    rounddown: {
+      translationKey: 'ROUNDDOWN',
+    },
   }
 
   public roundup(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
@@ -42,6 +45,42 @@ export class RoundingPlugin extends FunctionPlugin {
           return -Math.ceil(-coercedNumberToRound * placesMultiplier) / placesMultiplier
         } else {
           return Math.ceil(coercedNumberToRound * placesMultiplier) / placesMultiplier
+        }
+      }
+    }
+  }
+
+  public rounddown(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
+    if (ast.args.length < 1 || ast.args.length > 2) {
+      return new CellError(ErrorType.NA)
+    } else {
+      const numberToRound = this.evaluateAst(ast.args[0], formulaAddress)
+      if (numberToRound instanceof SimpleRangeValue) {
+        return new CellError(ErrorType.VALUE)
+      }
+
+      let coercedPlaces
+      if (ast.args[1]) {
+        const places = this.evaluateAst(ast.args[1], formulaAddress)
+        if (places instanceof SimpleRangeValue) {
+          return new CellError(ErrorType.VALUE)
+        }
+        coercedPlaces = coerceScalarToNumber(places)
+      } else {
+        coercedPlaces = 0
+      }
+
+      const coercedNumberToRound = coerceScalarToNumber(numberToRound)
+      if (coercedNumberToRound instanceof CellError) {
+        return coercedNumberToRound
+      } else if (coercedPlaces instanceof CellError) {
+        return coercedPlaces
+      } else {
+        const placesMultiplier = Math.pow(10, coercedPlaces)
+        if (coercedNumberToRound < 0) {
+          return -Math.floor(-coercedNumberToRound * placesMultiplier) / placesMultiplier
+        } else {
+          return Math.floor(coercedNumberToRound * placesMultiplier) / placesMultiplier
         }
       }
     }
