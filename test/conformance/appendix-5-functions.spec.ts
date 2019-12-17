5a21f97fd28372b36d8a39bfd23ddc50bacf0c69
Wojciech Czerniak <wojciech.czerniak@gmail.com>
2019-12-17 10:54:51 +0100

    Add QA acceptance tests for appendix 5 functions

---
+++ test/conformance/appendix-5-functions.spec.ts
@@ -0,0 +1,22 @@
+import { HyperFormula } from '../../src';
+import { adr } from '../testUtils';
+
+function createEngine(data: any[][]) {
+  let engine = HyperFormula.buildFromArray(data);
+
+  return {
+    getCellValue(cellAddress: string) {
+      return engine.getCellValue(adr(cellAddress));
+    }
+  }
+}
+
+describe('Quality Assurance tests', () => {
+  it('support SUM function', () => {
+    const engine = createEngine([
+      ['=SUM(1,2)']
+    ]);
+
+    expect(engine.getCellValue('A1')).toBe(2);
+  });
+});
