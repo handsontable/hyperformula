dbf263a24093710360a2997b59f56daf9655b846
Rafał Łasocha <me@swistak35.com>
2019-12-20 02:05:39 +0100

    Refactoring: Change ivars to more clearly depict what they store

--- src/DependencyGraph/SheetMapping.ts
+++ src/DependencyGraph/SheetMapping.ts
@@ -17,8 +17,8 @@ class Sheet {
 }
 
 export class SheetMapping {
-  private readonly mapping: Map<string, Sheet> = new Map()
-  private readonly reversedMapping: Map<number, Sheet> = new Map()
+  private readonly mappingFromCanonicalName: Map<string, Sheet> = new Map()
+  private readonly mappingFromId: Map<number, Sheet> = new Map()
   private readonly sheetNamePrefix: string = 'Sheet'
   private lastSheetId = -1
 
@@ -28,31 +28,31 @@ export class SheetMapping {
 
   public addSheet(sheetName: string = `${this.sheetNamePrefix}${this.lastSheetId + 2}`): number {
     const newSheetCanonicalName = canonicalize(sheetName)
-    if (this.mapping.has(newSheetCanonicalName)) {
+    if (this.mappingFromCanonicalName.has(newSheetCanonicalName)) {
       throw new Error(`Sheet ${sheetName} already exists`)
     }
 
     this.lastSheetId++
     const sheet = new Sheet(this.lastSheetId, sheetName)
-    this.reversedMapping.set(sheet.id, sheet)
-    this.mapping.set(sheet.canonicalName, sheet)
+    this.mappingFromId.set(sheet.id, sheet)
+    this.mappingFromCanonicalName.set(sheet.canonicalName, sheet)
     return sheet.id
   }
 
   public removeSheet(sheetId: number) {
-    const sheet = this.reversedMapping.get(sheetId)
+    const sheet = this.mappingFromId.get(sheetId)
     if (sheet === undefined) {
       throw new Error(`Sheet with id ${sheetId} doesn't exist`)
     }
     if (sheetId == this.lastSheetId) {
       --this.lastSheetId
     }
-    this.mapping.delete(sheet.canonicalName)
-    this.reversedMapping.delete(sheetId)
+    this.mappingFromCanonicalName.delete(sheet.canonicalName)
+    this.mappingFromId.delete(sheetId)
   }
 
   public fetch = (sheetName: string): number => {
-    const sheet = this.mapping.get(canonicalize(sheetName))
+    const sheet = this.mappingFromCanonicalName.get(canonicalize(sheetName))
     if (sheet === undefined) {
       throw new Error(`Sheet ${sheetName} doesn't exist`)
     }
@@ -60,7 +60,7 @@ export class SheetMapping {
   }
 
   public get = (sheetName: string): number | undefined => {
-    const sheet = this.mapping.get(canonicalize(sheetName))
+    const sheet = this.mappingFromCanonicalName.get(canonicalize(sheetName))
     if (sheet) {
       return sheet.id
     } else {
@@ -69,7 +69,7 @@ export class SheetMapping {
   }
 
   public name = (sheetId: number): string => {
-    const sheet = this.reversedMapping.get(sheetId)
+    const sheet = this.mappingFromId.get(sheetId)
     if (sheet === undefined) {
       throw new Error(`Sheet with id ${sheetId} doesn't exist`)
     }
@@ -77,7 +77,7 @@ export class SheetMapping {
   }
 
   public getName(sheetId: number): string | undefined {
-    const sheet = this.reversedMapping.get(sheetId)
+    const sheet = this.mappingFromId.get(sheetId)
     if (sheet) {
       return sheet.displayName
     } else {
@@ -86,25 +86,25 @@ export class SheetMapping {
   }
 
   public* names(): IterableIterator<string> {
-    for (const sheet of this.mapping.values()) {
+    for (const sheet of this.mappingFromCanonicalName.values()) {
       yield sheet.displayName
     }
   }
 
   public numberOfSheets(): number {
-    return this.mapping.size
+    return this.mappingFromCanonicalName.size
   }
 
   public hasSheetWithId(sheetId: number): boolean {
-    return this.reversedMapping.has(sheetId)
+    return this.mappingFromId.has(sheetId)
   }
 
   public hasSheetWithName(sheetName: string): boolean {
-    return this.mapping.has(canonicalize(sheetName))
+    return this.mappingFromCanonicalName.has(canonicalize(sheetName))
   }
 
   public renameSheet(sheetId: number, newName: string): void {
-    const sheet = this.reversedMapping.get(sheetId)
+    const sheet = this.mappingFromId.get(sheetId)
     if (sheet === undefined) {
       throw new Error(`Sheet with id ${sheetId} doesn't exist`)
     }
@@ -112,21 +112,21 @@ export class SheetMapping {
     if (currentName === newName) {
       return
     }
-    const sheetWithThisCanonicalName = this.mapping.get(canonicalize(newName))
+    const sheetWithThisCanonicalName = this.mappingFromCanonicalName.get(canonicalize(newName))
     if (sheetWithThisCanonicalName && sheetWithThisCanonicalName.id !== sheetId) {
       throw new Error(`Sheet '${newName}' already exists`)
     }
 
     const currentCanonicalName = sheet.canonicalName
     sheet.displayName = newName
-    this.mapping.delete(currentCanonicalName)
-    this.mapping.set(sheet.canonicalName, sheet)
-    this.reversedMapping.delete(sheetId)
-    this.reversedMapping.set(sheetId, sheet)
+    this.mappingFromCanonicalName.delete(currentCanonicalName)
+    this.mappingFromCanonicalName.set(sheet.canonicalName, sheet)
+    this.mappingFromId.delete(sheetId)
+    this.mappingFromId.set(sheetId, sheet)
   }
 
   public destroy(): void {
-    this.mapping.clear()
-    this.reversedMapping.clear()
+    this.mappingFromCanonicalName.clear()
+    this.mappingFromId.clear()
   }
 }
