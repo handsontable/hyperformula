2f852a4cd09f345aea44c5e5e6aadbf2df244495
Rafał Łasocha <me@swistak35.com>
2019-12-23 20:53:31 +0100

    Add E function

--- src/interpreter/plugin/MathConstantsPlugin.ts
+++ src/interpreter/plugin/MathConstantsPlugin.ts
@@ -3,15 +3,23 @@ import {ProcedureAst} from '../../parser'
 import {FunctionPlugin} from './FunctionPlugin'
 
 const PI = parseFloat(Math.PI.toFixed(14))
+const E = parseFloat(Math.E.toFixed(14))
 
 export class MathConstantsPlugin extends FunctionPlugin {
   public static implementedFunctions = {
     pi: {
       translationKey: 'PI',
     },
+    e: {
+      translationKey: 'E',
+    },
   }
 
   public pi(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
     return PI
+  }
+
+  public e(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
+    return E
   }
 }
