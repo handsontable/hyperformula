b439462055785268bf26a9e5b575018ce8769329
Jakub Kowalski <kowalski_jakub@hotmail.com>
2020-01-11 08:45:48 +0100

    add test from issue

---
+++ test/interpreter/error-literals.spec.ts
@@ -0,0 +1,16 @@
+import {CellError, HyperFormula} from '../../src'
+import {ErrorType} from '../../src/Cell'
+import '../testConfig'
+import {adr} from '../testUtils'
+
+it('Errors should be parsed and propagated', () => {
+  const engine = HyperFormula.buildFromArray([
+    ['#DIV/0!', '=A1', '=ISERROR(B1)', '=ISERROR(#DIV/0!)', '=#DIV/0!'],
+  ])
+
+  expect(engine.getCellValue(adr('A1'))).toBe(new CellError(ErrorType.DIV_BY_ZERO))
+  expect(engine.getCellValue(adr('B1'))).toBe(new CellError(ErrorType.DIV_BY_ZERO))
+  expect(engine.getCellValue(adr('C1'))).toBe(true)
+  expect(engine.getCellValue(adr('D1'))).toBe(true)
+  expect(engine.getCellValue(adr('E1'))).toBe(new CellError(ErrorType.DIV_BY_ZERO))
+})
