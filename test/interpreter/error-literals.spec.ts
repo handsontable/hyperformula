e4be40ca1bc75e742b007d6cbbcf4a736f6c9e04
Jakub Kowalski <kowalski_jakub@hotmail.com>
2020-01-11 09:31:12 +0100

    rearrange test

--- test/interpreter/error-literals.spec.ts
+++ test/interpreter/error-literals.spec.ts
@@ -3,14 +3,18 @@ import {ErrorType} from '../../src/Cell'
 import '../testConfig'
 import {adr} from '../testUtils'
 
-it('Errors should be parsed and propagated', () => {
-  const engine = HyperFormula.buildFromArray([
-    ['#DIV/0!', '=A1', '=ISERROR(B1)', '=ISERROR(#DIV/0!)', '=#DIV/0!'],
-  ])
+describe('Error literals', () => {
+  it('Errors should be parsed and propagated', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['#DIV/0!', '=A1', '=#DIV/0!'],
+      ['=ISERROR(A1)', '=ISERROR(B1)', '=ISERROR(C1)', '=ISERROR(#DIV/0!)']
+    ])
 
-  expect(engine.getCellValue(adr('A1'))).toEqual(new CellError(ErrorType.DIV_BY_ZERO))
-  expect(engine.getCellValue(adr('B1'))).toEqual(new CellError(ErrorType.DIV_BY_ZERO))
-  expect(engine.getCellValue(adr('C1'))).toEqual(true)
-  expect(engine.getCellValue(adr('D1'))).toEqual(true)
-  expect(engine.getCellValue(adr('E1'))).toEqual(new CellError(ErrorType.DIV_BY_ZERO))
+    expect(engine.getCellValue(adr('A1'))).toEqual(new CellError(ErrorType.DIV_BY_ZERO))
+    expect(engine.getCellValue(adr('B1'))).toEqual(new CellError(ErrorType.DIV_BY_ZERO))
+    expect(engine.getCellValue(adr('C1'))).toEqual(new CellError(ErrorType.DIV_BY_ZERO))
+    expect(engine.getCellValue(adr('A2'))).toEqual(true)
+    expect(engine.getCellValue(adr('B2'))).toEqual(true)
+    expect(engine.getCellValue(adr('C2'))).toEqual(true)
+  })
 })
