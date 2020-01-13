9bbc6a29ae18e9b34f90fd44495b5b6828a63557
Przemek <przemek@navalgo.com>
2020-01-13 12:35:38 +0100

    initial tests for date arithm

---
+++ test/interpreter/date-arithmetic.spec.ts
@@ -0,0 +1,37 @@
+import {Config} from '../../src'
+import {HyperFormula} from '../../src'
+import {CellError, EmptyValue, ErrorType} from '../../src/Cell'
+import {dateNumberRepresentation} from '../../src/interpreter/coerce'
+import '../testConfig'
+import {adr} from '../testUtils'
+
+describe('Date arithmetic', () => {
+  it('subtract two dates', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['02/02/2020', '02/06/2019', '=A1-B1'],
+    ])
+
+    expect(engine.getCellValue(adr('C1'))).toBe(245)
+  })
+  it('compare two dates', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['02/02/2020', '02/06/2019', '=A1>B1'],
+    ])
+
+    expect(engine.getCellValue(adr('C1'))).toBe(true)
+  })
+  it('sum date with number', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['02/02/2020', '2', '=A1+B1'],
+    ])
+
+    expect(engine.getCellValue(adr('C1'))).toBe('04/02/2020',)
+  })
+  it('sum date with boolean', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['02/02/2020', '=TRUE()', '=A1+B1'],
+    ])
+
+    expect(engine.getCellValue(adr('C1'))).toBe('03/02/2020',)
+  })
+})
