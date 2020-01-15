6495b15f9a0caf3e792b717c361e2716b9837af7
Przemek <przemek@navalgo.com>
2020-01-15 16:01:42 +0100

    atan2()

--- src/interpreter/plugin/TrigonometryPlugin.ts
+++ src/interpreter/plugin/TrigonometryPlugin.ts
@@ -1,5 +1,7 @@
 import {CellError, CellValue, ErrorType, SimpleCellAddress} from '../../Cell'
 import {ProcedureAst} from '../../parser'
+import {coerceScalarToNumber} from '../coerce'
+import {SimpleRangeValue} from '../InterpreterValue'
 import {FunctionPlugin} from './FunctionPlugin'
 
 /**
@@ -25,6 +27,9 @@ export class TrigonometryPlugin extends FunctionPlugin {
     },
     atan: {
       translationKey: 'ATAN',
+    },
+    atan2: {
+      translationKey: 'ATAN2',
     },
     ctg: {
       translationKey: 'COT',
@@ -82,6 +87,31 @@ export class TrigonometryPlugin extends FunctionPlugin {
       return Math.atan(coercedArg)
     })
   }
+
+  public atan2(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
+    if (ast.args.length !== 2) {
+      return new CellError(ErrorType.NA)
+    }
+
+    const arg1 = this.evaluateAst(ast.args[0], formulaAddress)
+    if (arg1 instanceof SimpleRangeValue) {
+      return new CellError(ErrorType.VALUE)
+    }
+    const arg2 = this.evaluateAst(ast.args[1], formulaAddress)
+    if(arg2 instanceof SimpleRangeValue) {
+      return new CellError(ErrorType.VALUE)
+    }
+    const coercedArg1 = coerceScalarToNumber(arg1)
+    if (coercedArg1 instanceof CellError)  {
+      return coercedArg1
+    }
+    const coercedArg2 = coerceScalarToNumber(arg2)
+    if(coercedArg2 instanceof CellError) {
+      return coercedArg2
+    }
+    return Math.atan2(coercedArg1, coercedArg2)
+  }
+
 
   public ctg(ast: ProcedureAst, formulaAddress: SimpleCellAddress): CellValue {
     return this.templateWithOneCoercedToNumberArgument(ast, formulaAddress, (coercedArg) => {
