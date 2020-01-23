aca26d8e27837035a96cf1425c9dc174544badb5
ndemytruk <ndemytruk@gmail.com>
2020-01-23 11:49:31 +0100

    Change: minor typos, xit instead of it

--- test/conformance/function-date.spec.ts
+++ test/conformance/function-date.spec.ts
@@ -6,15 +6,15 @@ import { adr, dateNumberToString } from '../testUtils'
 // Data and test scenarios were part of the working draft for OpenFormula standard
 // https://www.oasis-open.org/committees/download.php/16826/openformula-spec-20060221.html#DATE
 
-// function createEngine(data: any[][]) {
-//   let engine = HyperFormula.buildFromArray(data);
+function createEngine(data: any[][]) {
+  let engine = HyperFormula.buildFromArray(data);
 
-//   return {
-//     getCellValue(cellAddress: string) {
-//       return engine.getCellValue(adr(cellAddress));
-//     }
-//   }
-// }
+  return {
+    getCellValue(cellAddress: string) {
+      return engine.getCellValue(adr(cellAddress));
+    }
+  }
+}
 
 describe('Acceptance tests DATE function', () => {
   it('should suport simple date', () => {
@@ -23,28 +23,23 @@ describe('Acceptance tests DATE function', () => {
     ])
 
     expect(dateNumberToString(engine.getCellValue(adr('A1')))).toEqual('01/31/2005');
-
   });
 
-  it('should support nonleap year 1990 ', () => {
+  it('should support nonleap year 1900 ', () => {
     const engine = HyperFormula.buildFromArray([
       ['=DATE(1900,02,29)'],
-
     ]);
-    expect(dateNumberToString(engine.getCellValue(adr('A1')))).toEqual('12/30/1899');
 
+    expect(dateNumberToString(engine.getCellValue(adr('A1')))).toEqual('12/30/1899');
   });
 
   //bug to report
-  it('should suport leap years', () => {
+  xit('should suport leap years', () => {
     const engine = HyperFormula.buildFromArray([
       ['=DATE(2020,29,02)'],
-      ['=DATE(2021,29,02)'],
     ]);
 
-    expect(dateNumberToString(engine.getCellValue(adr('A1')))).toEqual('12/30/1899');
-    expect(dateNumberToString(engine.getCellValue(adr('A2')))).toEqual('12/30/1899');
-
+    expect(dateNumberToString(engine.getCellValue(adr('A1')))).toEqual('29/02/2020');
   });
 
 
