72af2fe921ab461545726302119881883f1f27c5
Przemek <przemek@navalgo.com>
2020-02-06 11:11:06 +0100

    testutils

--- test/testUtils.spec.ts
+++ test/testUtils.spec.ts
@@ -3,8 +3,8 @@ import {dateNumberToString} from './testUtils'
 
 describe('test utils', () => {
   it ('#dateNumberToString should return properly formatted  date', () => {
-    expect(dateNumberToString(0, Config.defaultConfig.dateFormat)).toEqual('12/30/1899')
-    expect(dateNumberToString(2, Config.defaultConfig.dateFormat)).toEqual('01/01/1900')
-    expect(dateNumberToString(43465, Config.defaultConfig.dateFormat)).toEqual('12/31/2018')
+    expect(dateNumberToString(0, new Config())).toEqual('12/30/1899')
+    expect(dateNumberToString(2, new Config())).toEqual('01/01/1900')
+    expect(dateNumberToString(43465, new Config())).toEqual('12/31/2018')
   })
 })
