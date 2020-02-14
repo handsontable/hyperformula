350adddc3ad5cfc85569459b3ce402cc50af8434
Przemek <przemek@navalgo.com>
2020-02-14 11:17:43 +0100

    failing test

---
+++ test/interpreter/cyclical-deps.spec.ts
@@ -0,0 +1,17 @@
+import {HyperFormula} from '../../src'
+import {ErrorType} from '../../src/Cell'
+import {adr, detailedError} from '../testUtils'
+
+describe('Error literals', () => {
+  it('Errors should be parsed and propagated', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=A2', '=A1'],
+      ['=ISERROR(A1)', '=ISERROR(B1)']
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqual(detailedError(ErrorType.CYCLE))
+    expect(engine.getCellValue(adr('B1'))).toEqual(detailedError(ErrorType.CYCLE))
+    expect(engine.getCellValue(adr('A2'))).toEqual(true)
+    expect(engine.getCellValue(adr('B2'))).toEqual(true)
+  })
+})
