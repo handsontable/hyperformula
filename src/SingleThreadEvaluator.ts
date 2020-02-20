c1352f91a40325b7577254259be1ec6cc4a917cd
Przemysław Uznański <40573492+izulin@users.noreply.github.com>
2020-02-20 12:09:10 +0100

    Update src/SingleThreadEvaluator.ts
    Co-Authored-By: Wojciech Czerniak <wojciech.czerniak@gmail.com>

--- src/SingleThreadEvaluator.ts
+++ src/SingleThreadEvaluator.ts
@@ -27,7 +27,6 @@ export class SingleThreadEvaluator implements Evaluator {
   public run() {
     this.stats.start(StatType.TOP_SORT)
     const { sorted, cycled } = this.dependencyGraph.topSortWithScc()
-    // const { sorted, cycled } = this.dependencyGraph.topologicalSort()
     this.stats.end(StatType.TOP_SORT)
 
     this.stats.measure(StatType.EVALUATION, () => {
