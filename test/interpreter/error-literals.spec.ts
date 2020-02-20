daea105d9f5b886be0cc0e666782fb522f2925a1
Przemysław Uznański <40573492+izulin@users.noreply.github.com>
2020-02-20 16:44:09 +0100

    Update test/interpreter/error-literals.spec.ts
    Co-Authored-By: Wojciech Czerniak <wojciech.czerniak@gmail.com>

--- test/interpreter/error-literals.spec.ts
+++ test/interpreter/error-literals.spec.ts
@@ -38,7 +38,7 @@ describe('Error literals', () => {
     expect(engine.getCellValue(adr('G1'))).toEqual(detailedError(ErrorType.NA)) // LTE 
     expect(engine.getCellValue(adr('H1'))).toEqual(detailedError(ErrorType.NA)) // NOT EQUAL
     expect(engine.getCellValue(adr('I1'))).toEqual(detailedError(ErrorType.NA)) // ADD
-    expect(engine.getCellValue(adr('J1'))).toEqual(detailedError(ErrorType.NA)) //SUB
+    expect(engine.getCellValue(adr('J1'))).toEqual(detailedError(ErrorType.NA)) // SUB
     expect(engine.getCellValue(adr('K1'))).toEqual(detailedError(ErrorType.NA)) // MULT
     expect(engine.getCellValue(adr('L1'))).toEqual(detailedError(ErrorType.NA)) // DIV
     expect(engine.getCellValue(adr('M1'))).toEqual(detailedError(ErrorType.NA)) // EXP
