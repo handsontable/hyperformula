16ea4a8557d34de24a9c2795fc0c72e730ede26f
Jakub Kowalski <kowalski_jakub@hotmail.com>
2020-02-20 13:20:29 +0100

    More tokenizer specs

--- test/parser/white-spaces.spec.ts
+++ test/parser/white-spaces.spec.ts
@@ -1,10 +1,10 @@
 import {Config} from '../../src'
 import {SheetMapping} from '../../src/DependencyGraph'
 import {enGB} from '../../src/i18n'
-import {buildLexerConfig, FormulaLexer, ParserWithCaching} from '../../src/parser'
-import {AbsoluteCell, EqualsOp, RangeSeparator, WhiteSpace} from '../../src/parser/LexerConfig'
+import {buildLexerConfig, FormulaLexer} from '../../src/parser'
+import {EqualsOp, RangeSeparator, WhiteSpace} from '../../src/parser/LexerConfig'
 import {processWhitespaces} from '../../src/parser/ParserWithCaching'
-import {adr, expect_array_with_same_content} from '../testUtils'
+import {expect_array_with_same_content} from '../testUtils'
 
 describe('tokenizeFormula', () => {
   const config = new Config()
@@ -23,22 +23,49 @@ describe('tokenizeFormula', () => {
     expect(tokens.map(token => token.tokenType).find(tokenType => tokenType === WhiteSpace)).toBe(undefined)
   })
 
-  it('should skip whiespaces inside range', () => {
+  it('should skip whitespace inside range', () => {
     const tokens = lexer.tokenizeFormula('=A1: A1').tokens
     const tokenTypes = tokens.map(token => token.tokenType.name)
     expect_array_with_same_content(tokenTypes, ['EqualsOp', 'RelativeCell', 'RangeSeparator', 'RelativeCell'])
   })
 
-  it('should skip whiespaces inside range 2', () => {
+  it('should skip whitespace inside range 2', () => {
     const tokens = lexer.tokenizeFormula('=A1 :A1').tokens
     const tokenTypes = tokens.map(token => token.tokenType.name)
     expect_array_with_same_content(tokenTypes, ['EqualsOp', 'RelativeCell', 'RangeSeparator', 'RelativeCell'])
   })
 
-  it('should skip whiespaces inside range 3', () => {
+  it('should skip whitespace inside range 3', () => {
     const tokens = lexer.tokenizeFormula('=A1 : A1').tokens
     const tokenTypes = tokens.map(token => token.tokenType.name)
     expect_array_with_same_content(tokenTypes, ['EqualsOp', 'RelativeCell', 'RangeSeparator', 'RelativeCell'])
+  })
+
+  it('should treat space as whitespace', () => {
+    const tokens = lexer.tokenizeFormula('= 1').tokens
+    expect(tokens[1].tokenType).toEqual(WhiteSpace)
+  })
+
+  it('should treat tabulator (U+0009) as whitespace', () => {
+    const tokens = lexer.tokenizeFormula('=\t1').tokens
+    expect(tokens[1].tokenType).toEqual(WhiteSpace)
+  })
+
+  it('should treat carriage return (U+000D) as whitespace', () => {
+    const tokens = lexer.tokenizeFormula('=\r1').tokens
+    expect(tokens[1].tokenType).toEqual(WhiteSpace)
+  })
+
+  it('should treat line feed (U+000A) as whitespace ', () => {
+    const tokens = lexer.tokenizeFormula('=\n1').tokens
+    expect(tokens[1].tokenType).toEqual(WhiteSpace)
+  })
+
+  it('should treat multiple whitespaces as one token', () => {
+    const tokens = lexer.tokenizeFormula('=\n\t\r 1').tokens
+    expect(tokens.length).toEqual(3)
+    expect(tokens[1].tokenType).toEqual(WhiteSpace)
+    expect(tokens[1].image).toEqual('\n\t\r ')
   })
 })
 
