17767a1a1c0e11f8559f9d92d1b5e06fa10753a8
ndemytruk <ndemytruk@gmail.com>
2020-02-21 13:15:29 +0100

    Add: test and pending test for #195

--- test/conformance/infinity.spec.ts
+++ test/conformance/infinity.spec.ts
@@ -26,7 +26,7 @@ describe('Quality assurance of operators', () => {
 
     });
 
-    xit('EXP(1000000000) should thrown #NUM! error', () => {
+    xit('EXP(1000000000) should thrown #NUM! error', () => { //pending for #195
         const engine = createEngine([
             [1000000000],
             ['=EXP(A1)']
@@ -43,7 +43,7 @@ describe('Quality assurance of operators', () => {
         expect(engine.getCellValue('A1')).toEqual(new DetailedCellError(new CellError(ErrorType.NUM), '#NUM!'));
     });
 
-    xit('1000^500 should thrown #NUM! error', () => {
+    xit('1000^500 should thrown #NUM! error', () => { //pending for #195
         const engine = createEngine([
             ['=1000^500']
         ]);
@@ -59,13 +59,22 @@ describe('Quality assurance of operators', () => {
         expect(engine.getCellValue('A1')).toEqual(-1.0000000000000006e+308);
     });
 
-    xit('too small -1*10^309 should return #NUM!', () => {
+    xit('too small -1*10^309 should return #NUM!', () => { //pending for #195
         const engine = createEngine([
             ['=-1*10 ^309']
         ]);
 
         expect(engine.getCellValue('A1')).toEqual(new DetailedCellError(new CellError(ErrorType.NUM), '#NUM!')); //return -Infinity
     });
+
+    xit('too big number should return #NUM!', () => { //pending for #195
+        const engine = createEngine([
+            [`=9.99*10 ^308`]
+        ]);
+
+        expect(engine.getCellValue('A1')).toEqual(new DetailedCellError(new CellError(ErrorType.NUM), '#NUM!')); //return Infinity
+    });
+
 
 
 })
