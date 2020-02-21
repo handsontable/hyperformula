47bc2e5ea784f90bdbd30535f23b9eb447530128
Przemek <przemek@navalgo.com>
2020-02-21 12:06:40 +0100

    lint

--- test/volatile-functions.spec.ts
+++ test/volatile-functions.spec.ts
@@ -1,7 +1,7 @@
 import {HyperFormula} from '../src'
 import {simpleCellAddress} from '../src/Cell'
 import './testConfig'
-import {adr, detailedError} from './testUtils'
+import {adr} from './testUtils'
 
 describe('Interpreter - function RAND', () => {
   it('works', () => {
