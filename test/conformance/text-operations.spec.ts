2296d5d964a9b307a40e358a84e5938a361a0661
ndemytruk <ndemytruk@gmail.com>
2020-03-03 14:05:20 +0100

    Add test with caseSensitive

--- test/conformance/text-operations.spec.ts
+++ test/conformance/text-operations.spec.ts
@@ -17,8 +17,8 @@ function createEngine(data: any[][]) {
   }
 }
 
-describe('Text arithmetic', () => {
-  it('"Aaa", "Aaa" should be supported by all comparison operators', () => {
+describe('Text arithmetic and logical comparision', () => { //pending on PR #203
+  it('"Aaa", "Aaa" should be supported by all operators', () => {
     const engine = HyperFormula.buildFromArray([
       ['Aaa', 'Aaa', ...data],
     ], new Config({ caseSensitive : true}))
@@ -40,7 +40,7 @@ describe('Text arithmetic', () => {
     expect(engine.getCellValue(adr('Q1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // PERCENTAGE
   })
 
-  it('"aaa", "aaa" should be supported by all comparison operators', () => {
+  it('"aaa", "aaa" should be supported by all operators', () => {
     const engine = HyperFormula.buildFromArray([
       ['aaa', 'aaa', ...data],
     ], new Config({ caseSensitive : true}))
@@ -62,7 +62,7 @@ describe('Text arithmetic', () => {
     expect(engine.getCellValue(adr('Q1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // PERCENTAGE
   })
 
-  it('"AAa", "AAa" should be supported by all comparison operators', () => {
+  it('"AAa", "AAa" should be supported by all operators', () => {
     const engine = HyperFormula.buildFromArray([
       ['AAa', 'AAa', ...data],
     ], new Config({ caseSensitive : true}))
@@ -84,7 +84,7 @@ describe('Text arithmetic', () => {
     expect(engine.getCellValue(adr('Q1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // PERCENTAGE
   })
 
-  it('"ąęćńóśżź", "ąęćńóśżź" should be supported by all comparison operators', () => {
+  it('"ąęćńóśżź", "ąęćńóśżź" should be supported by all operators', () => {
     const engine = HyperFormula.buildFromArray([
       ['ąęćńóśżź', 'ąęćńóśżź', ...data],
     ], new Config({ caseSensitive : true, language: plPL}))
@@ -106,7 +106,7 @@ describe('Text arithmetic', () => {
     expect(engine.getCellValue(adr('Q1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) // PERCENTAGE
   })
 
-  xit('"jaźŃ", "ąęćńóśżź" should be supported by all comparison operators', () => { //pending on #225
+  xit('"jaźŃ", "ąęćńóśżź" should be supported by all operators', () => { //pending on #225
     const engine = HyperFormula.buildFromArray([
       ['jaźŃ', 'ąęćńóśżź', ...data],
     ], new Config({ caseSensitive : true, language: plPL}))
@@ -115,7 +115,7 @@ describe('Text arithmetic', () => {
     //expect(engine.getCellValue(adr('D1'))).toEqual(true) // GT
     //expect(engine.getCellValue(adr('E1'))).toEqual(false) // LT
     //expect(engine.getCellValue(adr('F1'))).toEqual(true) // GTE
-    //expect(engine.getCellValue(adr('G1'))).toEqual(false) // LTE
+    //expect(engine.getCellValue(adr('G1'))).toEqual(false) // LTEgs
     expect(engine.getCellValue(adr('H1'))).toEqual(true) // NOT EQUAL
     expect(engine.getCellValue(adr('I1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) // ADD
     expect(engine.getCellValue(adr('J1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) // SUB
@@ -128,7 +128,7 @@ describe('Text arithmetic', () => {
     expect(engine.getCellValue(adr('Q1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) // PERCENTAGE
   })
 
-  it('"Żółcią", "ąęćńóśżź" should be supported by all comparison operators', () => {
+  it('"Żółcią", "ąęćńóśżź" should be supported by all operatorswith case', () => {
     const engine = HyperFormula.buildFromArray([
       ['Żółcią', 'ąęćńóśżź', ...data],
     ], new Config({ caseSensitive : true, language: plPL}))
@@ -149,5 +149,121 @@ describe('Text arithmetic', () => {
     expect(engine.getCellValue(adr('P1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) // UNARY MINUS 
     expect(engine.getCellValue(adr('Q1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) // PERCENTAGE
   })
+
+  it('"AAa", "aaa" should be supported by all operators without case sensitive', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['AAa', 'aaa', ...data],
+    ], new Config({ caseSensitive : false}))
+
+    expect(engine.getCellValue(adr('C1'))).toEqual(true)  // EQUAL
+    expect(engine.getCellValue(adr('D1'))).toEqual(false) // GT
+    expect(engine.getCellValue(adr('E1'))).toEqual(false) // LT
+    expect(engine.getCellValue(adr('F1'))).toEqual(true) // GTE
+    expect(engine.getCellValue(adr('G1'))).toEqual(true) // LTE
+    expect(engine.getCellValue(adr('H1'))).toEqual(false) // NOT EQUAL
+    expect(engine.getCellValue(adr('I1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // ADD
+    expect(engine.getCellValue(adr('J1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // SUB
+    expect(engine.getCellValue(adr('K1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // MULT
+    expect(engine.getCellValue(adr('L1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //DIV
+    expect(engine.getCellValue(adr('M1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //EXP 
+    expect(engine.getCellValue(adr('N1'))).toEqual('AAaaaa') // CONCAT
+  })
+
+  it('"AAa", "aaa" should be supported by all operators without case sensitive', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['aaa', 'AAa', ...data],
+    ], new Config({ caseSensitive : false}))
+
+    expect(engine.getCellValue(adr('C1'))).toEqual(true)  // EQUAL
+    expect(engine.getCellValue(adr('D1'))).toEqual(false) // GT
+    //expect(engine.getCellValue(adr('E1'))).toEqual(true) // LT
+    //expect(engine.getCellValue(adr('F1'))).toEqual(false) // GTE
+    expect(engine.getCellValue(adr('G1'))).toEqual(true) // LTE
+    expect(engine.getCellValue(adr('H1'))).toEqual(false) // NOT EQUAL
+    expect(engine.getCellValue(adr('I1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // ADD
+    expect(engine.getCellValue(adr('J1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // SUB
+    expect(engine.getCellValue(adr('K1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // MULT
+    expect(engine.getCellValue(adr('L1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //DIV
+    expect(engine.getCellValue(adr('M1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //EXP 
+    expect(engine.getCellValue(adr('N1'))).toEqual('aaaAAa') // CONCAT
+  })
+
+  xit('"AAa", "aaa" should be supported by all operators with case sensitive', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['AAa', 'aaa', ...data],
+    ], new Config({ caseSensitive : true }))
+
+    expect(engine.getCellValue(adr('C1'))).toEqual(false)  // EQUAL
+    //expect(engine.getCellValue(adr('D1'))).toEqual(true) // GT
+    //expect(engine.getCellValue(adr('E1'))).toEqual(false) // LT
+    //expect(engine.getCellValue(adr('F1'))).toEqual(true) // GTE
+    //expect(engine.getCellValue(adr('G1'))).toEqual(false) // LTE
+    expect(engine.getCellValue(adr('H1'))).toEqual(true) // NOT EQUAL
+    expect(engine.getCellValue(adr('I1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // ADD
+    expect(engine.getCellValue(adr('J1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // SUB
+    expect(engine.getCellValue(adr('K1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // MULT
+    expect(engine.getCellValue(adr('L1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //DIV
+    expect(engine.getCellValue(adr('M1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //EXP 
+    expect(engine.getCellValue(adr('N1'))).toEqual('AAaaaa') // CONCAT
+  })
+
+  it('"AAa", "aaa" should be supported by all operators with case sensitive', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['aaa', 'AAa', ...data],
+    ], new Config({ caseSensitive : true }))
+
+    expect(engine.getCellValue(adr('C1'))).toEqual(false)  // EQUAL
+    expect(engine.getCellValue(adr('D1'))).toEqual(true) // GT
+    expect(engine.getCellValue(adr('E1'))).toEqual(false) // LT
+    expect(engine.getCellValue(adr('F1'))).toEqual(true) // GTE
+    expect(engine.getCellValue(adr('G1'))).toEqual(false) // LTE
+    expect(engine.getCellValue(adr('H1'))).toEqual(true) // NOT EQUAL
+    expect(engine.getCellValue(adr('I1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // ADD
+    expect(engine.getCellValue(adr('J1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // SUB
+    expect(engine.getCellValue(adr('K1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // MULT
+    expect(engine.getCellValue(adr('L1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //DIV
+    expect(engine.getCellValue(adr('M1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //EXP 
+    expect(engine.getCellValue(adr('N1'))).toEqual('aaaAAa') // CONCAT
+  })
+  
+  it('"A", "a" should be supported by all operators with case sensitive', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['A', 'a', ...data],
+    ], new Config({ caseSensitive : true }))
+
+    expect(engine.getCellValue(adr('C1'))).toEqual(false)  // EQUAL
+    //expect(engine.getCellValue(adr('D1'))).toEqual(true) // GT
+    //expect(engine.getCellValue(adr('E1'))).toEqual(false) // LT
+    //expect(engine.getCellValue(adr('F1'))).toEqual(true) // GTE
+    //expect(engine.getCellValue(adr('G1'))).toEqual(false) // LTE
+    expect(engine.getCellValue(adr('H1'))).toEqual(true) // NOT EQUAL
+    expect(engine.getCellValue(adr('I1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // ADD
+    expect(engine.getCellValue(adr('J1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // SUB
+    expect(engine.getCellValue(adr('K1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // MULT
+    expect(engine.getCellValue(adr('L1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //DIV
+    expect(engine.getCellValue(adr('M1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //EXP 
+    expect(engine.getCellValue(adr('N1'))).toEqual('Aa') // CONCAT
+  })
+
+  it('"A", "a" should be supported by all operators without case sensitive', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['A', 'a', ...data],
+    ], new Config({ caseSensitive : false }))
+
+    expect(engine.getCellValue(adr('C1'))).toEqual(true)  // EQUAL
+    expect(engine.getCellValue(adr('D1'))).toEqual(false) // GT
+    expect(engine.getCellValue(adr('E1'))).toEqual(false) // LT
+    expect(engine.getCellValue(adr('F1'))).toEqual(true) // GTE
+    expect(engine.getCellValue(adr('G1'))).toEqual(true) // LTE
+    expect(engine.getCellValue(adr('H1'))).toEqual(false) // NOT EQUAL
+    expect(engine.getCellValue(adr('I1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // ADD
+    expect(engine.getCellValue(adr('J1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // SUB
+    expect(engine.getCellValue(adr('K1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // MULT
+    expect(engine.getCellValue(adr('L1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //DIV
+    expect(engine.getCellValue(adr('M1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //EXP 
+  })
+
+
+  
 })
 
