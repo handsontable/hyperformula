391e7ca607501435aadca3feb365b1e0cc0071aa
ndemytruk <ndemytruk@gmail.com>
2020-03-03 14:41:04 +0100

    Add test with Turkish name handsontable/#6059

--- test/conformance/text-operations.spec.ts
+++ test/conformance/text-operations.spec.ts
@@ -263,6 +263,26 @@ describe('Text arithmetic and logical comparision', () => { //pending on PR #203
     expect(engine.getCellValue(adr('M1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //EXP 
   })
 
+  xit('Turkish name İnanç', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['İnanç', 'aaaaa', ...data],
+    ], new Config({ caseSensitive : true }))
+
+    expect(engine.getCellValue(adr('C1'))).toEqual(false)  // EQUAL
+    expect(engine.getCellValue(adr('D1'))).toEqual(false) // GT
+    expect(engine.getCellValue(adr('E1'))).toEqual(false) // LT
+    expect(engine.getCellValue(adr('F1'))).toEqual(true) // GTE
+    expect(engine.getCellValue(adr('G1'))).toEqual(true) // LTE
+    expect(engine.getCellValue(adr('H1'))).toEqual(true) // NOT EQUAL
+    expect(engine.getCellValue(adr('I1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // ADD
+    expect(engine.getCellValue(adr('J1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // SUB
+    expect(engine.getCellValue(adr('K1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // MULT
+    expect(engine.getCellValue(adr('L1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //DIV
+    expect(engine.getCellValue(adr('M1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //EXP
+    expect(engine.getCellValue(adr('N1'))).toEqual('İnançaaaaa') // CONCAT 
+  })
+
+  
 
   
 })
