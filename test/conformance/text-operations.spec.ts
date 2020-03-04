091e900eb0f4daec20e78d98c34dbad0c5f35f1a
ndemytruk <ndemytruk@gmail.com>
2020-03-04 14:15:05 +0100

    Add more tests with Chinese compared to simple strings like 'Aaa'

--- test/conformance/text-operations.spec.ts
+++ test/conformance/text-operations.spec.ts
@@ -380,7 +380,85 @@ describe('Text arithmetic and logical comparision', () => { //pending on PR #203
     expect(engine.getCellValue(adr('N1'))).toEqual('發发') // CONCAT 
   })
 
-  
+  it('"country" in Chinese traditional vs. simplified', () => { //https://r12a.github.io/scripts/chinese/
+    const engine = HyperFormula.buildFromArray([
+      ['国', '國', ...data],
+    ], new Config({ caseSensitive : false, language: plPL }))
+    
+    expect(engine.getCellValue(adr('C1'))).toEqual(false) // EQUAL
+    expect(engine.getCellValue(adr('D1'))).toEqual(false) // GT
+    expect(engine.getCellValue(adr('E1'))).toEqual(true) // LT
+    expect(engine.getCellValue(adr('F1'))).toEqual(false) // GTE
+    expect(engine.getCellValue(adr('G1'))).toEqual(true) // LTE
+    expect(engine.getCellValue(adr('H1'))).toEqual(true) // NOT EQUAL
+    expect(engine.getCellValue(adr('I1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) // ADD
+    expect(engine.getCellValue(adr('J1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) // SUB
+    expect(engine.getCellValue(adr('K1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) // MULT
+    expect(engine.getCellValue(adr('L1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) //DIV
+    expect(engine.getCellValue(adr('M1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) //EXP
+    expect(engine.getCellValue(adr('N1'))).toEqual('国國') // CONCAT
+    expect(engine.getCellValue(adr('O1'))).toEqual('国') // UNARY PLUS
+  })
+    
+  it('"the world" in Chinese same string ', () => { //https://r12a.github.io/scripts/chinese/
+    const engine = HyperFormula.buildFromArray([
+      ['界', '界', ...data],
+    ], new Config({ caseSensitive : false, language: plPL }))
+    
+    expect(engine.getCellValue(adr('C1'))).toEqual(true) // EQUAL
+    expect(engine.getCellValue(adr('D1'))).toEqual(false) // GT
+    expect(engine.getCellValue(adr('E1'))).toEqual(false) // LT
+    expect(engine.getCellValue(adr('F1'))).toEqual(true) // GTE
+    expect(engine.getCellValue(adr('G1'))).toEqual(true) // LTE
+    expect(engine.getCellValue(adr('H1'))).toEqual(false) // NOT EQUAL
+    expect(engine.getCellValue(adr('I1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) // ADD
+    expect(engine.getCellValue(adr('J1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) // SUB
+    expect(engine.getCellValue(adr('K1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) // MULT
+    expect(engine.getCellValue(adr('L1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) //DIV
+    expect(engine.getCellValue(adr('M1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#ARG!')) //EXP
+    expect(engine.getCellValue(adr('N1'))).toEqual('界界') // CONCAT
+    expect(engine.getCellValue(adr('O1'))).toEqual('界') // UNARY PLUS
+  })
+    
+  it('"名人" in Chinese compared to `Aa` with case sensetive ', () => { //https://r12a.github.io/scripts/chinese/
+    const engine = HyperFormula.buildFromArray([
+      ['名人', 'Aa', ...data],
+    ], new Config({ caseSensitive : true }))
+    
+    expect(engine.getCellValue(adr('C1'))).toEqual(false) // EQUAL
+    expect(engine.getCellValue(adr('D1'))).toEqual(true) // GT
+    expect(engine.getCellValue(adr('E1'))).toEqual(false) // LT
+    expect(engine.getCellValue(adr('F1'))).toEqual(true) // GTE
+    expect(engine.getCellValue(adr('G1'))).toEqual(false) // LTE
+    expect(engine.getCellValue(adr('H1'))).toEqual(true) // NOT EQUAL
+    expect(engine.getCellValue(adr('I1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // ADD
+    expect(engine.getCellValue(adr('J1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // SUB
+    expect(engine.getCellValue(adr('K1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // MULT
+    expect(engine.getCellValue(adr('L1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //DIV
+    expect(engine.getCellValue(adr('M1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //EXP
+    expect(engine.getCellValue(adr('N1'))).toEqual('名人Aa') // CONCAT
+    expect(engine.getCellValue(adr('O1'))).toEqual('名人') // UNARY PLUS
+  })
+    
+  it('"名人" in Chinese compared to `Aa` without case sensetive ', () => { //https://r12a.github.io/scripts/chinese/
+    const engine = HyperFormula.buildFromArray([
+      ['名人', 'Aa', ...data],
+    ], new Config({ caseSensitive : false }))
+    
+    expect(engine.getCellValue(adr('C1'))).toEqual(false) // EQUAL
+    expect(engine.getCellValue(adr('D1'))).toEqual(true) // GT
+    expect(engine.getCellValue(adr('E1'))).toEqual(false) // LT
+    expect(engine.getCellValue(adr('F1'))).toEqual(true) // GTE
+    expect(engine.getCellValue(adr('G1'))).toEqual(false) // LTE
+    expect(engine.getCellValue(adr('H1'))).toEqual(true) // NOT EQUAL
+    expect(engine.getCellValue(adr('I1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // ADD
+    expect(engine.getCellValue(adr('J1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // SUB
+    expect(engine.getCellValue(adr('K1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) // MULT
+    expect(engine.getCellValue(adr('L1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //DIV
+    expect(engine.getCellValue(adr('M1'))).toEqual(new DetailedCellError(new CellError(ErrorType.VALUE), '#VALUE!')) //EXP
+    expect(engine.getCellValue(adr('N1'))).toEqual('名人Aa') // CONCAT
+    expect(engine.getCellValue(adr('O1'))).toEqual('名人') // UNARY PLUS
+  })
 
   
 })
