c2277d8190988b7680f98c8338318a47eb472f22
Anna Cieslik <scarletfog@gmail.com>
2020-03-10 17:36:29 +0100

    Added event tags

--- src/Emitter.ts
+++ src/Emitter.ts
@@ -1,34 +1,50 @@
 import { ExportedChange } from './CellValue'
 
 export enum Events {
-  /**
-   * @event Events#SheetAdded
-   */
   SheetAdded = 'sheetAdded',
-  /**
- * @event Events#SheetRemoved
- */
   SheetRemoved = 'sheetRemoved',
-  /**
- * @event Events#SheetRenamed
- */
   SheetRenamed = 'sheetRenamed',
-  /**
- * @event Events#NamedExpressionAdded
- */
   NamedExpressionAdded = 'namedExpressionAdded',
-  /**
- * @event Events#NamedExpressionRemoved
- */
   NamedExpressionRemoved = 'namedExpressionRemoved',
-  /**
- * @event Events#ValuesUpdated
- */
   ValuesUpdated = 'valuesUpdated',
 }
+
+/**
+  * @event SheetAddedHandler
+  * @param {string} addedSheetDisplayName
+  */
 export type SheetAddedHandler = (addedSheetDisplayName: string) => any
+
+/**
+  * @event SheetRemovedHandler
+  * @param {string} removedSheetDisplayName
+  * @param {ExportedChange[]} changes  
+  */
 export type SheetRemovedHandler = (removedSheetDisplayName: string, changes: ExportedChange[]) => any
+
+/**
+  * @event SheetRenamedHandler
+  * @param {string} oldDisplayName
+  * @param {string} newDisplayName  
+  */
 export type SheetRenamedHandler = (oldDisplayName: string, newDisplayName: string) => any
+
+/**
+  * @event NamedExpressionAddedHandler
+  * @param {string} namedExpressionName
+  * @param {ExportedChange[]} changes  
+  */
 export type NamedExpressionAddedHandler = (namedExpressionName: string, changes: ExportedChange[]) => any
+
+/**
+  * @event NamedExpressionRemovedHandler
+  * @param {string} namedExpressionName
+  * @param {ExportedChange[]} changes
+  */
 export type NamedExpressionRemovedHandler = (namedExpressionName: string, changes: ExportedChange[]) => any
+
+/**
+  * @event ValuesUpdatedHandler
+  * @param {ExportedChange[]} changes
+  */
 export type ValuesUpdatedHandler = (changes: ExportedChange[]) => any
