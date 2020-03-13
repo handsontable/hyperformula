c9492d59338157c2e28a65b5b31becaa65a43701
Przemysław Uznański <40573492+izulin@users.noreply.github.com>
2020-03-13 13:13:04 +0100

    Update src/StringHelper.ts
    Co-Authored-By: Wojciech Czerniak <wojciech.czerniak@gmail.com>

--- src/StringHelper.ts
+++ src/StringHelper.ts
@@ -1,10 +1,8 @@
 import {Config} from './Config'
 
 export function collatorFromConfig(config: Config): Intl.Collator {
-
   const sensitivity = config.caseSensitive ? (config.accentSensitive ? 'variant' : 'case') : (config.accentSensitive ? 'accent' : 'base' )
   const caseFirst = config.caseFirst
   const ignorePunctuation = config.ignorePunctuation
   return new Intl.Collator(config.localeLang, {sensitivity, caseFirst, ignorePunctuation})
 }
-
