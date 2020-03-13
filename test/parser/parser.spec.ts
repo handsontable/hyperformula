6da717463a44f4f3cd343afde955d05e71b88db4
Przemek <przemek@navalgo.com>
2020-03-13 15:33:56 +0100

    linter

--- test/parser/parser.spec.ts
+++ test/parser/parser.spec.ts
@@ -471,13 +471,6 @@ describe('Parsing errors', () => {
     expect(errors[0].type).toBe(ParsingErrorType.ParserError)
   })
 
-  it('should throw', () => {
-    const parser = new ParserWithCaching(new Config(), new SheetMapping(enGB).get)
-
-    const ast1 = parser.parse('=A1B1', CellAddress.absolute(0, 0, 0)).ast
-    const ast2 = parser.parse('=1', CellAddress.absolute(0, 0, 0)).ast
-  })
-
   // weird error
   it('cell range with unexisting end sheet', () => {
     const sheetMapping = new SheetMapping(enGB)
