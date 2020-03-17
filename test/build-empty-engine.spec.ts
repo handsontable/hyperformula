e470a4a6fa26435057fa1401a7b49e84ab1673ec
Przemek <przemek@navalgo.com>
2020-03-17 15:41:41 +0100

    tests

--- test/build-empty-engine.spec.ts
+++ test/build-empty-engine.spec.ts
@@ -1,9 +1,15 @@
 import {HyperFormula} from '../src'
 import './testConfig.ts'
+import {Config} from '../src/Config'
 
 describe('Building empty engine', () => {
   it('works', () => {
     const engine = HyperFormula.buildEmpty()
     expect(engine).toBeInstanceOf(HyperFormula)
+  })
+  xit('accepts config params', () => {
+    const config = new Config()
+    const engine = HyperFormula.buildEmpty(config)
+    expect(engine.config).toBe(config)
   })
 })
--- test/build-engine-from-arrays.spec.ts
+++ test/build-engine-from-arrays.spec.ts
@@ -1,5 +1,6 @@
 import {HyperFormula} from '../src'
 import './testConfig.ts'
+import {Config} from '../src/Config'
 
 describe('Building engine from arrays', () => {
   it('works', () => {
@@ -15,5 +16,22 @@ describe('Building engine from arrays', () => {
     const engine = HyperFormula.buildFromArray([])
 
     expect(engine.getAllSheetsDimensions()).toEqual({'Sheet1': {'height': 0, 'width': 0}})
+  })
+
+  xit('#buildFromSheets accepts config', () => {
+    const config = new Config()
+    const engine = HyperFormula.buildFromSheets({
+      Sheet1: [],
+      Sheet2: [],
+    }, config)
+
+    expect(engine.config).toBe(config)
+  })
+
+  xit('#buildFromSheet accepts config', () => {
+    const config = new Config()
+    const engine = HyperFormula.buildFromArray([], config)
+
+    expect(engine.config).toBe(config)
   })
 })
