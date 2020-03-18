cf3ec0c069e144fbc2bda2018966da9d0aa5d098
ndemytruk <ndemytruk@gmail.com>
2020-03-18 15:19:58 +0100

    Add more tests for floating point and smartRounding, fix typos

--- test/conformance/floating-point.spec.ts
+++ test/conformance/floating-point.spec.ts
@@ -30,12 +30,12 @@ describe('Quality assurance of floating point', () => {
   it('addition of small numbers with smartRounding', () => {
     const engine = HyperFormula.buildFromArray([
       ['0.000123456789', '1', '=A1+B1'],
-    ], new Config({ smartRounding: true }))
+    ], new Config({ smartRounding: false }))
   
     expect(engine.getCellValue(adr('C1'))).toEqual(1.000123456789)  
   })
 
-  xit('addition of small numbers with smartRounding', () => {
+  xit('addition of small numbers with smartRounding', () => { //pending on #257
     const engine = HyperFormula.buildFromArray([
       ['0.000123456789', '1', '=A1+B1'],
     ], new Config({ smartRounding: true }))
@@ -46,11 +46,42 @@ describe('Quality assurance of floating point', () => {
   xit('addition of small numbers with smartRounding and with precisionRounding=15', () => {
     const engine = HyperFormula.buildFromArray([
       ['0.000123456789', '1', '=A1+B1'],
-    ], new Config({ precisionRounding: 15, smartRounding: false }))
+    ], new Config({ precisionRounding: 15, smartRounding: true }))
   
-    expect(engine.getCellValue(adr('C1'))).toEqual(1.000123457)  //as GS and E
+    expect(engine.getCellValue(adr('C1'))).toEqual(1.000123457)  //as GS and E, now return 1.000123456789
   })
 
+  it('adding a negative number with smartRounding', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=(43.1-43.2)+1'],
+    ], new Config({ smartRounding: true }))
+  
+    expect(engine.getCellValue(adr('A1'))).toEqual(0.9)  //as GS and E
+  })
+
+  it('adding a negative number without smartRounding', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=(43.1-43.2)+1'],
+    ], new Config({ smartRounding: false }))
+  
+    expect(engine.getCellValue(adr('A1'))).toEqual(0.8999999999999986)  //as GS and E
+  })
+
+  it('example when a value reaches zero without smartRounding', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=1.333+1.225-1.333-1.225'],
+    ], new Config({ smartRounding: false }))
+  
+    expect(engine.getCellValue(adr('A1'))).toEqual(-2.220446049250313E-16)  //as Gnumeric and E with scientific format
+  })
+
+  it('Example when a value reaches zero with smartRounding', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=1.333+1.225-1.333-1.225'],
+    ], new Config({ smartRounding: true }))
+  
+    expect(engine.getCellValue(adr('A1'))).toEqual(0)  //E and GS retun 0
+  })
 
 })
 
