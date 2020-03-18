ee770dd77b275f451cf0d58c7b79c7dc3bcda1cb
Przemysław Uznański <40573492+izulin@users.noreply.github.com>
2020-03-18 15:04:20 +0100

    Pu/issue 217 (#255)
    * islogical, isnumber
    
    * bugfix + istext + isnontext
    
    * translation
    
    * .
    
    * coercions in ranges
    
    * numeric aggregation fixed with coercions
    
    * linter

--- src/interpreter/plugin/NumericAggregationPlugin.ts
+++ src/interpreter/plugin/NumericAggregationPlugin.ts
@@ -2,8 +2,13 @@ import assert from 'assert'
 import {AbsoluteCellRange, DIFFERENT_SHEETS_ERROR} from '../../AbsoluteCellRange'
 import {CellError, EmptyValue, ErrorType, InternalCellValue, SimpleCellAddress} from '../../Cell'
 import {Maybe} from '../../Maybe'
-import {AstNodeType, CellRangeAst, ProcedureAst} from '../../parser'
-import {coerceNonDateScalarToMaybeNumber, coerceToRange} from '../coerce'
+import {AstNodeType, CellRangeAst, CellReferenceAst, ProcedureAst} from '../../parser'
+import {
+  coerceNonDateScalarToMaybeNumber,
+  coerceScalarToNumberOrError,
+  coerceToMaybeNumber,
+  coerceToRange
+} from '../coerce'
 import { SimpleRangeValue} from '../InterpreterValue'
 import {max, maxa, min, mina, nonstrictadd} from '../scalar'
 import {FunctionPlugin} from './FunctionPlugin'
@@ -97,6 +102,10 @@ export class NumericAggregationPlugin extends FunctionPlugin {
     },
   }
 
+  private coerceToNumber(arg: InternalCellValue): InternalCellValue {
+    return coerceScalarToNumberOrError(arg, this.interpreter.dateHelper)
+  }
+
   /**
    * Corresponds to SUM(Number1, Number2, ...).
    *
@@ -106,14 +115,14 @@ export class NumericAggregationPlugin extends FunctionPlugin {
    * @param formulaAddress
    */
   public sum(ast: ProcedureAst, formulaAddress: SimpleCellAddress): InternalCellValue {
-    return this.reduce(ast, formulaAddress, 0, 'SUM', nonstrictadd, idMap)
+    return this.reduce(ast, formulaAddress, 0, 'SUM', nonstrictadd, idMap, (arg) => this.coerceToNumber(arg))
   }
 
   public sumsq(ast: ProcedureAst, formulaAddress: SimpleCellAddress): InternalCellValue {
     if (ast.args.length < 1) {
       return new CellError(ErrorType.NA)
     }
-    return this.reduce(ast, formulaAddress, 0, 'SUMSQ', nonstrictadd, square)
+    return this.reduce(ast, formulaAddress, 0, 'SUMSQ', nonstrictadd, square, (arg) => this.coerceToNumber(arg))
   }
 
   public countblank(ast: ProcedureAst, formulaAddress: SimpleCellAddress): InternalCellValue {
@@ -144,7 +153,7 @@ export class NumericAggregationPlugin extends FunctionPlugin {
     if (ast.args.length < 1) {
       return new CellError(ErrorType.NA)
     }
-    const value = this.reduce(ast, formulaAddress, Number.NEGATIVE_INFINITY, 'MAX', max, idMap)
+    const value = this.reduce(ast, formulaAddress, Number.NEGATIVE_INFINITY, 'MAX', max, idMap, (arg) => this.coerceToNumber(arg))
 
     return zeroForInfinite(value)
   }
@@ -153,7 +162,7 @@ export class NumericAggregationPlugin extends FunctionPlugin {
     if (ast.args.length < 1) {
       return new CellError(ErrorType.NA)
     }
-    const value = this.reduce(ast, formulaAddress, Number.NEGATIVE_INFINITY, 'MAXA', maxa, idMap)
+    const value = this.reduce(ast, formulaAddress, Number.NEGATIVE_INFINITY, 'MAXA', maxa, idMap, (arg) => this.coerceToNumber(arg))
 
     return zeroForInfinite(value)
   }
@@ -170,7 +179,7 @@ export class NumericAggregationPlugin extends FunctionPlugin {
     if (ast.args.length < 1) {
       return new CellError(ErrorType.NA)
     }
-    const value = this.reduce(ast, formulaAddress, Number.POSITIVE_INFINITY, 'MIN', min, idMap)
+    const value = this.reduce(ast, formulaAddress, Number.POSITIVE_INFINITY, 'MIN', min, idMap, (arg) => this.coerceToNumber(arg))
 
     return zeroForInfinite(value)
   }
@@ -179,7 +188,7 @@ export class NumericAggregationPlugin extends FunctionPlugin {
     if (ast.args.length < 1) {
       return new CellError(ErrorType.NA)
     }
-    const value = this.reduce(ast, formulaAddress, Number.POSITIVE_INFINITY, 'MINA', mina, idMap)
+    const value = this.reduce(ast, formulaAddress, Number.POSITIVE_INFINITY, 'MINA', mina, idMap, (arg) => this.coerceToNumber(arg))
 
     return zeroForInfinite(value)
   }
@@ -192,7 +201,7 @@ export class NumericAggregationPlugin extends FunctionPlugin {
       return left + right
     }, (arg): number => {
       return (typeof arg === 'number') ? 1 : 0
-    })
+    }, (arg) => this.coerceToNumber(arg))
 
     return value
   }
@@ -205,7 +214,7 @@ export class NumericAggregationPlugin extends FunctionPlugin {
       return left + right
     }, (arg): number => {
       return (arg === EmptyValue) ? 0 : 1
-    })
+    }, (arg) => this.coerceToNumber(arg))
 
     return value
   }
@@ -231,7 +240,7 @@ export class NumericAggregationPlugin extends FunctionPlugin {
       } else {
         return AverageResult.empty
       }
-    })
+    }, (arg) => this.coerceToNumber(arg))
 
     if (result instanceof CellError) {
       return result
@@ -266,7 +275,7 @@ export class NumericAggregationPlugin extends FunctionPlugin {
           return AverageResult.single(coercedArg)
         }
       }
-    })
+    }, (arg) => this.coerceToNumber(arg))
 
     if (result instanceof CellError) {
       return result
@@ -283,19 +292,25 @@ export class NumericAggregationPlugin extends FunctionPlugin {
    * @param initialAccValue - initial accumulator value for reducing function
    * @param functionName - function name to use as cache key
    * @param reducingFunction - reducing function
+   * @param mapFunction
    * */
-  private reduce<T>(ast: ProcedureAst, formulaAddress: SimpleCellAddress, initialAccValue: T, functionName: string, reducingFunction: BinaryOperation<T>, mapFunction: MapOperation<T>): T {
+  private reduce<T>(ast: ProcedureAst, formulaAddress: SimpleCellAddress, initialAccValue: T, functionName: string, reducingFunction: BinaryOperation<T>, mapFunction: MapOperation<T>, coerceFunction: (arg: InternalCellValue) => InternalCellValue): T {
     return ast.args.reduce((acc: T, arg) => {
       let value
       if (arg.type === AstNodeType.CELL_RANGE) {
         value = this.evaluateRange(arg, formulaAddress, acc, functionName, reducingFunction, mapFunction)
       } else {
         value = this.evaluateAst(arg, formulaAddress)
-        if (value instanceof SimpleRangeValue) {
+        if(value instanceof SimpleRangeValue) {
           value = this.reduceRange(Array.from(value.valuesFromTopLeftCorner()).map(mapFunction), initialAccValue, reducingFunction)
+        } else if (arg.type === AstNodeType.CELL_REFERENCE) {
+          value = mapFunction(value)
+          value = reducingFunction(initialAccValue, value)
         } else {
+          value = coerceScalarToNumberOrError(value, this.interpreter.dateHelper)
           value = mapFunction(value)
         }
+
       }
 
       return reducingFunction(acc, value)
