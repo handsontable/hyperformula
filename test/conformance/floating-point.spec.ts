c86a1b39282eed043794f3f21a6293627994d63d
ndemytruk <ndemytruk@gmail.com>
2020-03-18 15:25:49 +0100

    Clean up

--- test/conformance/floating-point.spec.ts
+++ test/conformance/floating-point.spec.ts
@@ -1,13 +1,10 @@
-import { HyperFormula, DetailedCellError } from '../../src'
+import { HyperFormula } from '../../src'
 import '../testConfig'
 import { adr } from '../testUtils'
-import { CellError, ErrorType } from '../../src/Cell'
-import { EmptyValue } from '../../src/Cell'
+
 import { Config } from '../../src/Config'
 
 // Test cases based on https://github.com/MicrosoftDocs/OfficeDocs-Support/blob/public/Office/Client/excel/floating-point-arithmetic-inaccurate-result.md
-const data =
-    ['=A1=B1', '=A1>B1', '=A1<B1', '=A1>=B1', '=A1<=B1', '=A1<>B1', '=A1+B1', '=A1-B1', '=A1*B1', '=A1/B1', '=A1^B1', '=A1&B1', '=+A1', '=-A1', '=A1%']
 
 function createEngine(data: any[][]) {
   const engine = HyperFormula.buildFromArray(data)
