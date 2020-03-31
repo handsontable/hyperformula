2e5287ab4a4ff38fe07b659c99b6da0fb225eef2
Rafał Łasocha <me@swistak35.com>
2020-03-31 21:30:31 +0200

    We abort the cut before the operation

--- src/CrudOperations.ts
+++ src/CrudOperations.ts
@@ -103,8 +103,8 @@ export class CrudOperations {
   }
 
   public moveCells(sourceLeftCorner: SimpleCellAddress, width: number, height: number, destinationLeftCorner: SimpleCellAddress): void {
-    this.operations.moveCells(sourceLeftCorner, width, height, destinationLeftCorner)
     this.clipboardOperations.abortCut()
+    this.operations.moveCells(sourceLeftCorner, width, height, destinationLeftCorner)
   }
 
   public moveRows(sheet: number, startRow: number, numberOfRows: number, targetRow: number): void {
