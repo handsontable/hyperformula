5667f68537bc32d51a06ee22abd4af44b6e58a4a
Anna Cieslik <scarletfog@gmail.com>
2020-04-08 10:01:48 +0200

    Update tests about errors

--- test/unsupported-types.spec.ts
+++ test/unsupported-types.spec.ts
@@ -103,6 +103,6 @@ describe( 'unsupported types should result in error', () => {
     // eslint-disable-next-line
     // @ts-ignore
     expect( () => engine.setCellContents(adr('A1'), [1])
-    ).toThrow('Expected an array of arrays or a raw cell value.')
+    ).toThrow('Invalid arguments')
   })
 })
