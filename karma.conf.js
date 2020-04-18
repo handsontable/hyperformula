9dfb5aac3fc2093498293e5a9d57d2c78d2d4a07
Piotr Laszczkowski <swistach@gmail.com>
2020-04-18 18:45:01 +0200

    #219 - Adds additional reporter to the karma's configuration

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots', 'progress'],
 
 
     // web server port
