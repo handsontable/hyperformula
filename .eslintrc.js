ae271761b0f3cf26e9fa90ec11b2cbd61928f874
budnix <571316+budnix@users.noreply.github.com>
2020-04-20 15:04:37 +0200

    Fix building the lib process
    Fixes a files and directory structures to match to the paths defined in
    the package.json file. Fixes individual scripts for building the lib.
    Now before compiling the lib compile task is executed before.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   ],
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.json', './tsconfig.test.json'],
   },
   extends: [
     'eslint:recommended',
