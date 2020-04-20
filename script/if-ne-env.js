8fdfe30363d87ca3598b8e13f2f12a32d8da7fec
budnix <571316+budnix@users.noreply.github.com>
2020-04-20 15:52:10 +0200

    Make npm scripts friendly for Windows CMD

---
+++ script/if-ne-env.js
@@ -0,0 +1,22 @@
+/**
+ * If not equal environment script. Checks if the passed environment variables
+ * are not equal with the variables stored in the system. If detected, the script
+ * is terminated with an error otherwise terminates without error.
+ */
+const { parse } = require('querystring')
+
+const [ /* node bin */, /* path to this script */, conditionToCheck ] = process.argv
+
+if (!conditionToCheck) {
+  throw Error('Missing condition to check')
+}
+
+const queryObject = parse(conditionToCheck)
+
+Object.keys(queryObject).forEach((queryParam) => {
+  if (process.env[queryParam] === queryObject[queryParam]) {
+    process.exit(0)
+  }
+})
+
+process.exit(1)
