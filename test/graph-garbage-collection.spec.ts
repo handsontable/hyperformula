f8057abf31101abf3da17b8c5da0719d29d8e535
izulin <izulin@gmail.com>
2020-05-14 18:52:08 +0200

    .

--- test/graph-garbage-collection.spec.ts
+++ test/graph-garbage-collection.spec.ts
@@ -61,26 +61,36 @@ function randomInteger(min: number, max: number) {
 describe('larger tests', () => {
   it('repeat the same crud', () => {
     const engine = HyperFormula.buildFromArray([])
-    const config = engine.getConfig()
-    const maxCols = config.maxColumns
-    const maxRows = config.maxRows
-    console.log(engine.dependencyGraph.graph.nodesCount(), engine.dependencyGraph.rangeMapping.getMappingSize(0))
-    for (let i = 0; i < 1000; i++) {
-      const col1 = randomInteger(0, 20)//maxCols)
-      const row1 = randomInteger(0, 20)//maxRows)
-      const col2 = randomInteger(0, 20)//maxCols)
-      const row2 = randomInteger(0, 20)//maxRows)
-      const startAddress = engine.simpleCellAddressToString({sheet: 0, row: Math.min(row1,row2), col: Math.min(col1,col2)}, 0)
-      const endAddress = engine.simpleCellAddressToString({sheet: 0, row: Math.max(row1,row2), col: Math.max(col1,col2) + 1}, 0)
-      const formula = '=SUM(' + startAddress + ':' + endAddress + ')'
-      // console.log('set formula:', 'engine.setCellContents({ sheet: 0, col: 0, row: 0 }, "' + formula + '")', i / 30000 * 100, '%')
-      const cellAddress = {sheet: 0, col: 0, row: 0}
-      engine.setCellContents(cellAddress, formula)
-      // console.log('get formula', engine.getCellSerialized(cellAddress));
+    for(let tmp = 0; tmp < 25; tmp++) {
+      for (let x = 0; x < 10; x++) {
+        for (let y = 0; y < 10; y++) {
+          const col1 = randomInteger(2, 7)
+          const row1 = randomInteger(2, 7)
+          const col2 = col1 + randomInteger(-2, 2)
+          const row2 = row1 + randomInteger(-2, 2)
+          const startAddress = engine.simpleCellAddressToString({
+            sheet: 0,
+            row: Math.min(row1, row2),
+            col: Math.min(col1, col2)
+          }, 0)
+          const endAddress = engine.simpleCellAddressToString({
+            sheet: 0,
+            row: Math.max(row1, row2),
+            col: Math.max(col1, col2)
+          }, 0)
+          const formula = '=SUM(' + startAddress + ':' + endAddress + ')'
+          engine.setCellContents({sheet: 0, col: x, row: y}, formula)
+        }
+      }
     }
-    console.log('end')
+    for(let x=0; x<10; x++) {
+      for(let y=0; y<10; y++) {
+        engine.setCellContents({sheet: 0, col: x, row: y}, null)
+      }
+    }
     console.log(engine.dependencyGraph.graph.nodesCount(), engine.dependencyGraph.rangeMapping.getMappingSize(0))
     engine.dependencyGarbageCollect()
-    console.log(engine.dependencyGraph.graph.nodesCount(), engine.dependencyGraph.rangeMapping.getMappingSize(0))
+    expect(engine.dependencyGraph.graph.nodesCount()).toBe(0)
+    expect(engine.dependencyGraph.rangeMapping.getMappingSize(0)).toBe(0)
   })
 })
