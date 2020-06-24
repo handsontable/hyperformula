1751085595c48e42353663bd0c850e09bfd056b4
Wojciech Szymański <141330+wszymanski@users.noreply.github.com>
2020-06-24 12:13:27 +0200

    Reverted some changes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
   title: 'HyperFormula (v' + HyperFormula.version + ')',
   description: 'HyperFormula is an open-source, high-performance calculation engine for spreadsheets and web applications.',
   head: [
-    // Google Tag Manager, extra snippet in the custom `Layout.vue` file.
+    // Google Tag Manager.
     ['script', {}, `
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
