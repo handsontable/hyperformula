b2daaa09b3f2671721ce71402900b202b34c171b
Jan Siegel <js.ziggle@gmail.com>
2020-06-30 11:54:50 +0200

    - Add checks for the module-system-specific language bundle locations

--- script/check-publish-package.js
+++ script/check-publish-package.js
@@ -33,6 +33,9 @@ const FILES_CHECKLIST = [
   'es/HyperFormula.js',
   'typings/index.d.ts',
   'typings/HyperFormula.d.ts',
+  'dist/languages/plPL.js',
+  'es/i18n/languages/plPL.js',
+  'commonjs/i18n/languages/plPL.js',
 ]
 
 process.stdin.resume()
