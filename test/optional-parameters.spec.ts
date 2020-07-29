3e1e6d2688e3a2e861803f2c3dd53951844b5dd8
izulin <izulin@gmail.com>
2020-07-29 15:53:27 +0200

    fixed tests

--- test/optional-parameters.spec.ts
+++ test/optional-parameters.spec.ts
@@ -10,32 +10,19 @@ class FooPlugin extends FunctionPlugin {
   public static implementedFunctions = {
     'FOO': {
       method: 'foo',
+      parameters: {
+        list: [
+          { argumentType: 'string', defaultValue: 'default1'},
+          { argumentType: 'string', defaultValue: 'default2'},
+        ],
+      },
     },
   }
 
   public foo(ast: ProcedureAst, formulaAddress: SimpleCellAddress) {
-    if (ast.args.length > 2) {
-      return new CellError(ErrorType.NA)
-    }
-
-    const arg1 = ast.args[0].type !== AstNodeType.EMPTY ? this.evaluateAst(ast.args[0], formulaAddress) : 'default1'
-    if (arg1 instanceof SimpleRangeValue) {
-      return new CellError(ErrorType.VALUE)
-    }
-    const arg2 = ast.args[1].type !== AstNodeType.EMPTY ? this.evaluateAst(ast.args[1], formulaAddress) : 'default2'
-    if (arg2 instanceof SimpleRangeValue) {
-      return new CellError(ErrorType.VALUE)
-    }
-
-    const coercedArg1 = coerceScalarToString(arg1)
-    if (coercedArg1 instanceof CellError)  {
-      return coercedArg1
-    }
-    const coercedArg2 = coerceScalarToString(arg2)
-    if (coercedArg2 instanceof CellError) {
-      return coercedArg2
-    }
-    return coercedArg1 + '+' + coercedArg2
+    return this.runFunction(ast.args, formulaAddress, this.parameters('FOO'),
+      (arg1, arg2) => arg1+'+'+arg2
+    )
   }
 }
 
@@ -48,24 +35,38 @@ describe('Nonexistent parameters', () => {
       ['=foo( ,2)'],
       ['=foo(1,)'],
       ['=foo( , )'],
+      ['=foo(1)'],
+      ['=foo()'],
     ], {functionPlugins: [FooPlugin]})
 
     expect(engine.getCellValue(adr('A1'))).toBe('1+2')
-    expect(engine.getCellValue(adr('A2'))).toBe('default1+2')
-    expect(engine.getCellValue(adr('A3'))).toBe('default1+2')
-    expect(engine.getCellValue(adr('A4'))).toBe('1+default2')
-    expect(engine.getCellValue(adr('A5'))).toBe('default1+default2')
+    expect(engine.getCellValue(adr('A2'))).toBe('+2')
+    expect(engine.getCellValue(adr('A3'))).toBe('+2')
+    expect(engine.getCellValue(adr('A4'))).toBe('1+')
+    expect(engine.getCellValue(adr('A5'))).toBe('+')
+    expect(engine.getCellValue(adr('A6'))).toBe('1+default2')
+    expect(engine.getCellValue(adr('A7'))).toBe('default1+default2')
   })
 
-  it('typical function do not accept', () => {
+  it('log fails with coerce to 0', () => {
     const engine = HyperFormula.buildFromArray([
-      ['=DATE(,1,1900)'],
       ['=LOG(10,)'],
-      ['=SUM(,1)']
     ])
 
     expect(engine.getCellValue(adr('A1'))).toEqual(detailedError(ErrorType.NUM))
-    expect(engine.getCellValue(adr('A2'))).toEqual(detailedError(ErrorType.NUM))
-    expect(engine.getCellValue(adr('A3'))).toEqual(detailedError(ErrorType.NUM))
   })
+
+  it('other function coerce EmptyValue', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=DATE(,1,1900)'],
+      ['=SUM(,1)'],
+      ['=CONCATENATE(,"abcd")']
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqual(1901)
+    expect(engine.getCellValue(adr('A2'))).toEqual(detailedError(ErrorType.NUM)) //TODO when SUM() is fixed, it should evaluate to 1
+    expect(engine.getCellValue(adr('A3'))).toEqual('abcd')
+  })
+
+
 })
