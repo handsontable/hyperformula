c199f9abe3b59bac8365392bb6e2de4da476459b
izulin <izulin@gmail.com>
2020-08-13 14:07:06 +0200

    effective() implementation

---
+++ test/interpreter/function-effect.spec.ts
@@ -0,0 +1,24 @@
+import {ErrorType, HyperFormula} from '../../src'
+import {adr, detailedError} from '../testUtils'
+
+describe('Function EFFECT', () => {
+  it('should return #NA! error with the wrong number of arguments', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=EFFECT(1)', '=EFFECT(1, 1, 1)'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqual(detailedError(ErrorType.NA))
+    expect(engine.getCellValue(adr('B1'))).toEqual(detailedError(ErrorType.NA))
+  })
+
+  it('should calculate the correct value with correct arguments and defaults', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=EFFECT(2%, 1)', '=EFFECT(2%, 2)', '=EFFECT(2%, 2.9)', '=EFFECT(2%, 24)'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toBeCloseTo(0.02,9)
+    expect(engine.getCellValue(adr('B1'))).toBeCloseTo(0.0201,9)
+    expect(engine.getCellValue(adr('C1'))).toBeCloseTo(0.0201, 9)
+    expect(engine.getCellValue(adr('D1'))).toBeCloseTo(0.0201928431045086, 9)
+  })
+})
