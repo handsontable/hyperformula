af94d152918b7b670709d0bd636f299fda9b9f5b
izulin <izulin@gmail.com>
2020-08-29 00:47:26 +0200

    precision

--- test/interpreter/function-pv.spec.ts
+++ test/interpreter/function-pv.spec.ts
@@ -17,11 +17,11 @@ describe('Function PV', () => {
       ['=PV(-99%, 24, 100)', '=PV(-1, 24, 100, 400)', '=PV(-2, 24, 100, 400, 1)'],
     ])
 
-    expect(engine.getCellValue(adr('A1'))).toBeCloseTo(-1891.39256)
-    expect(engine.getCellValue(adr('B1'))).toBeCloseTo(-2140.081155)
-    expect(engine.getCellValue(adr('C1'))).toBeCloseTo(-2177.909007)
-    expect(engine.getCellValue(adr('A2'))).toBeCloseTo(-1.01010101010099e+50)
+    expect(engine.getCellValue(adr('A1'))).toBeCloseTo(-1891.39256, 6)
+    expect(engine.getCellValue(adr('B1'))).toBeCloseTo(-2140.081155, 6)
+    expect(engine.getCellValue(adr('C1'))).toBeCloseTo(-2177.909007, 6)
+    expect(engine.getCellValue(adr('A2'))).toBeCloseTo(-1.01010101010099e+50, 6)
     expect(engine.getCellValue(adr('B2'))).toEqual(detailedError(ErrorType.DIV_BY_ZERO))
-    expect(engine.getCellValue(adr('C2'))).toBeCloseTo(-400)
+    expect(engine.getCellValue(adr('C2'))).toBeCloseTo(-400, 6)
   })
 })
