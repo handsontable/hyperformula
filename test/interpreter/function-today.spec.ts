ce96496511c28503bf6199e5b5058c4d3667cee9
izulin <izulin@gmail.com>
2020-08-31 15:43:41 +0200

    better concat + refactor + cleanup

--- test/interpreter/function-today.spec.ts
+++ test/interpreter/function-today.spec.ts
@@ -2,11 +2,6 @@ import {HyperFormula} from '../../src'
 import {ErrorType} from '../../src/Cell'
 import {adr, detailedError} from '../testUtils'
 
-function sleepFor( sleepDuration: number ){
-  const now = new Date().getTime()
-  while(new Date().getTime() < now + sleepDuration){ /* do nothing */ }
-}
-
 describe('Interpreter - function TODAY', () => {
   it('works',  () => {
     const engine =  HyperFormula.buildFromArray([
