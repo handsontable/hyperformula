b70430431d0451923d1dd1a369d37a6d3882f53c
Jakub Kowalski <kowalski_jakub@hotmail.com>
2020-10-02 16:53:22 +0200

    Move jest tsconfig to tests

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,7 @@ module.exports = {
   // A set of global variables that need to be available in all test environments
   globals: {
     "ts-jest": {
-      "tsConfig": "tsconfig.jest.json"
+      "tsConfig": "./test/tsconfig.json"
     }
   },
 
