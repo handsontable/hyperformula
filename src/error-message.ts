7152125df78399667aef12f17285613fd4a53ce7
Przemysław Uznański <40573492+izulin@users.noreply.github.com>
2020-10-03 12:08:24 +0200

    5 datetime functions (#534)
    * function interval
    
    * day counting
    
    * tests
    
    * more tests
    
    * docs
    
    * networkdays translations
    
    * networkdays.intl translations
    
    * refactor
    
    * more tests
    
    * now with binary search
    
    * workday implementation
    
    * tests
    
    * translations
    
    * linter
    
    * docs
    
    * .
    
    * .
    
    * .
    
    * corrected logic
    
    * refactor
    
    * refactor
    
    * linter
    
    * special case

--- src/error-message.ts
+++ src/error-message.ts
@@ -65,6 +65,7 @@ export class ErrorMessage {
   public static CharacterCodeBounds = 'Character code out of bounds.'
   public static NonZero = 'Argument cannot be 0.'
   public static LessThanOne = 'Argument cannot be less than 1.'
+  public static WeekendString = 'Incorrect weekend bitmask string.'
   public static FunctionName = (arg: string) => `Function name ${arg} not recognized.`
   public static NamedExpressionName = (arg: string) => `Named expression ${arg} not recognized.`
   public static LicenseKey = (arg: string) => `License key is ${arg}.`
