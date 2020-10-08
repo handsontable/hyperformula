8ac47452b78b1c7d069207819fe2cbd25e1d2c1d
Wojciech Czerniak <wojciech.czerniak@gmail.com>
2020-10-08 11:18:26 +0200

    Restore ColumnsSpan.spec.ts

--- test/ColumnsSpan.spec.ts
+++ test/ColumnsSpan.spec.ts
@@ -1,6 +1,6 @@
 import {ColumnsSpan} from '../src/Span'
 
-xdescribe('ColumnsSpan', () => {
+describe('ColumnsSpan', () => {
   it('raise error when starting column is less than 0', () => {
     expect(() => {
       new ColumnsSpan(0, -1, 0)
