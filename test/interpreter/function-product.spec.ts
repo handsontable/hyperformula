3a3727edb7b00ba16212c2debe066cf8871dc8b5
izulin <izulin@gmail.com>
2020-10-09 14:38:02 +0200

    .

--- test/interpreter/function-product.spec.ts
+++ test/interpreter/function-product.spec.ts
@@ -1,6 +1,6 @@
 import {ErrorType, HyperFormula} from '../../src'
-import {adr, detailedError} from '../testUtils'
 import {ErrorMessage} from '../../src/error-message'
+import {adr, detailedError} from '../testUtils'
 
 describe('Function PRODUCT', () => {
   it('should take at least one argument', () => {
@@ -15,8 +15,10 @@ describe('Function PRODUCT', () => {
       const engine = HyperFormula.buildFromArray([
         ['=PRODUCT(2, 3)'],
         ['=PRODUCT(B2:D2, E2, F2)', 2, 3, 'foo', 'bar', 4],
+        ['=PRODUCT(5, "foo")']
       ])
       expect(engine.getCellValue(adr('A1'))).toEqual(6)
       expect(engine.getCellValue(adr('A2'))).toEqual(24)
+      expect(engine.getCellValue(adr('A3'))).toEqual(detailedError(ErrorType.VALUE,ErrorMessage.NumberCoercion))
   })
 })
