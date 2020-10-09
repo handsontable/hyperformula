74daa88d656afa0537942439797e9f0d0e23889f
Przemysław Uznański <40573492+izulin@users.noreply.github.com>
2020-10-09 15:16:42 +0200

    Update test/interpreter/function-mirr.spec.ts
    Co-authored-by: Wojciech Czerniak <wojciech.czerniak@gmail.com>

--- test/interpreter/function-mirr.spec.ts
+++ test/interpreter/function-mirr.spec.ts
@@ -18,13 +18,28 @@ describe('Function MIRR', () => {
       ['=MIRR(B1:C1,1,1)', 1, -1],
       ['=MIRR(B2:C2,1,1)', -1, 1],
       ['=MIRR(B3:E3,0.2,0.1)', -1, 0, -1, 1],
-      ['=MIRR(B4:E4,0.2,0.1)', -1, 0, -1, -1],
-      ['=MIRR(B5:G5,0.2,0.1)', -1, 0, 'abcd', true, -1, 1],
+      ['=MIRR(B3:E3,0.2,0.1)', -1, -1, 1],
     ])
     expect(engine.getCellValue(adr('A1'))).toEqual(3)
     expect(engine.getCellValue(adr('A2'))).toEqual(0)
     expect(engine.getCellValue(adr('A3'))).toBeCloseTo(-0.161201673643132, 6)
-    expect(engine.getCellValue(adr('A4'))).toEqual(detailedError(ErrorType.DIV_BY_ZERO))
-    expect(engine.getCellValue(adr('A5'))).toBeCloseTo(-0.161201673643132, 6)
+    expect(engine.getCellValue(adr('A4'))).toBeCloseTo(-0.161201673643132, 6) // different value without 0
+  })
+  
+  it('should return #DIV/0! if at least one positive and one negative values condtion is not met', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=MIRR(B1:E1,0.2,0.1)', -1, 0, -1, -1],
+      ['=MIRR(B2:E2,0.2,0.1)', 1, 0, 1, 1],
+    ])
+    expect(engine.getCellValue(adr('A1'))).toEqual(detailedError(ErrorType.DIV_BY_ZERO))
+    expect(engine.getCellValue(adr('A2'))).toEqual(detailedError(ErrorType.DIV_BY_ZERO))
+  })
+  
+  it('should ignore text, boolean and empty values', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=MIRR(B1:H1,0.2,0.1)', -1, 0, 'abcd', true, null, -1, 1],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toBeCloseTo(-0.161201673643132, 6)
   })
 })
