b271ea4ec82d233ce88c8fbb516537b7594eac0c
izulin <izulin@gmail.com>
2020-10-14 22:12:24 +0200

    tests for aliases

--- test/interpreter/function-varp.spec.ts
+++ test/interpreter/function-varp.spec.ts
@@ -1,43 +1,10 @@
-import {ErrorType, HyperFormula} from '../../src'
-import {adr, detailedError} from '../testUtils'
-import {ErrorMessage} from '../../src/error-message'
+import {HyperFormula} from '../../src'
 
-describe('Function VAR.P', () => {
-  it('should take at least one argument', () => {
-    const engine = HyperFormula.buildFromArray([
-      ['=VAR.P()'],
-    ])
-
-    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.WrongArgNumber))
-  })
-
-  it('should calculate variance (population)', () => {
-    const engine = HyperFormula.buildFromArray([
-      ['=VAR.P(2, 3)'],
-      ['=VAR.P(1)'],
-    ])
-    expect(engine.getCellValue(adr('A1'))).toEqual(0.25)
-    expect(engine.getCellValue(adr('A2'))).toEqual(0)
-  })
-
-  it('should coerce explicit argument to numbers', () => {
-    const engine = HyperFormula.buildFromArray([
-      ['=VAR.P(2, 3, 4, TRUE(), FALSE(), "1",)'],
-    ])
-    expect(engine.getCellValue(adr('A1'))).toBeCloseTo(1.95918367346939, 6) //inconsistency with product #1
-  })
-
-  it('should ignore non-numeric values in ranges, including ignoring logical values and text representation of numbers', () => {
-    const engine = HyperFormula.buildFromArray([
-      ['=VAR.P(B1:I1)', 2, 3, 4, true, false, 'a', '\'1', null],
-    ])
-    expect(engine.getCellValue(adr('A1'))).toBeCloseTo(0.666666666666667, 6)
-  })
-
-  it('should propagate errors', () => {
-    const engine = HyperFormula.buildFromArray([
-      ['=VAR.P(B1:I1)', 2, 3, 4, '=NA()', false, 'a', '\'1', null],
-    ])
-    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA))
+describe('Function VARP', () => {
+  it('should be an alias of VAR.P', () => {
+    const engine = HyperFormula.buildEmpty()
+    const metadata1 = engine.getFunctionPlugin('VARP')?.implementedFunctions!['VARP']
+    const metadata2 = engine.getFunctionPlugin('VAR.P')?.implementedFunctions!['VAR.P']
+    expect(metadata1).toEqual(metadata2)
   })
 })
