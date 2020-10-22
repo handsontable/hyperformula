5a5d877d80a938fea26496ee8856f79541c13915
Przemysław Uznański <40573492+izulin@users.noreply.github.com>
2020-10-22 20:38:55 +0200

    Update test/interpreter/function-sqrtpi.spec.ts
    Co-authored-by: Wojciech Czerniak <wojciech.czerniak@gmail.com>

--- test/interpreter/function-sqrtpi.spec.ts
+++ test/interpreter/function-sqrtpi.spec.ts
@@ -12,6 +12,7 @@ describe('Function SQRTPI', () => {
     expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.WrongArgNumber))
     expect(engine.getCellValue(adr('B1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.WrongArgNumber))
   })
+
   it('works', () => {
     const engine = HyperFormula.buildFromArray([
       ['=SQRTPI(0)'],
