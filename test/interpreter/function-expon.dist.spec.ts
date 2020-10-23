2da480446d0a23f85df344863e2e92ddd7aebb3f
izulin <izulin@gmail.com>
2020-10-23 13:52:33 +0200

    .

--- test/interpreter/function-expon.dist.spec.ts
+++ test/interpreter/function-expon.dist.spec.ts
@@ -24,26 +24,38 @@ describe('Function EXPON.DIST', () => {
 
     expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.NumberCoercion))
     expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.NumberCoercion))
-    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.NumberCoercion))
+    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.WrongType))
   })
 
   it('should work as cdf', () => {
     const engine = HyperFormula.buildFromArray([
       ['=EXPON.DIST(1, 1, TRUE())'],
-      ['=EXPON.DIST(10, 2, TRUE())'],
+      ['=EXPON.DIST(3, 2, TRUE())'],
     ])
 
-    expect(engine.getCellValue(adr('A1'))).toBeCloseTo(0.32105562956522493, 6)
-    expect(engine.getCellValue(adr('A2'))).toBeCloseTo(1.9941790884215962, 6)
+    expect(engine.getCellValue(adr('A1'))).toBeCloseTo(0.632120558828558, 6)
+    expect(engine.getCellValue(adr('A2'))).toBeCloseTo(0.997521247823334, 6)
   })
 
   it('should work as pdf', () => {
     const engine = HyperFormula.buildFromArray([
       ['=EXPON.DIST(1, 1, FALSE())'],
-      ['=EXPON.DIST(10, 2, FALSE())'],
+      ['=EXPON.DIST(3, 2, FALSE())'],
     ])
 
-    expect(engine.getCellValue(adr('A1'))).toBeCloseTo(0.32105562956522493, 6)
-    expect(engine.getCellValue(adr('A2'))).toBeCloseTo(1.9941790884215962, 6)
+    expect(engine.getCellValue(adr('A1'))).toBeCloseTo(0.367879441171442, 6)
+    expect(engine.getCellValue(adr('A2'))).toBeCloseTo(0.00495750435333272, 6)
+  })
+
+  it('checks bounds', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=EXPON.DIST(0, 1, FALSE())'],
+      ['=EXPON.DIST(-0.00001, 1, FALSE())'],
+      ['=EXPON.DIST(1, 0, FALSE())'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqual(1)
+    expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.NUM, ErrorMessage.ValueSmall))
+    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.NUM, ErrorMessage.ValueSmall))
   })
 })
