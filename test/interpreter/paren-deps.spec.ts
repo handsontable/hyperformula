577334ba682c1ee08a9d0c59d4703a56e42625b9
izulin <izulin@gmail.com>
2020-11-02 16:00:59 +0100

    more paren

--- test/interpreter/paren-deps.spec.ts
+++ test/interpreter/paren-deps.spec.ts
@@ -5,15 +5,15 @@ describe('dependencies with parenthesis', () => {
   it('should be collected when required', () => {
     const engine = HyperFormula.buildFromArray([
       ['=SUM(1)'],
-      ['=(A1)+(A3)'],
+      ['=(A1)+((A3))'],
       ['=SUM(1)'],
     ])
     expect(engine.getCellValue(adr('A2'))).toEqual(2)
   })
 
-  it('should work for itself', () => {
+  it('should not build ref for special function', () => {
     const engine = HyperFormula.buildFromArray([
-      ['=COLUMN((A1))']
+      ['=COLUMN((((A1))))']
     ])
 
     expect(engine.getCellValue(adr('A1'))).toEqual(1)
