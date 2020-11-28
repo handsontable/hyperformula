4845d7aca38399f952ccbf0a43b3ee6457a8547d
izulin <izulin@gmail.com>
2020-11-28 17:10:01 +0100

    typing

--- src/interpreter/plugin/FunctionPlugin.ts
+++ src/interpreter/plugin/FunctionPlugin.ts
@@ -12,7 +12,7 @@ import {SearchStrategy} from '../../Lookup/SearchStrategy'
 import {Maybe} from '../../Maybe'
 import {Ast, AstNodeType, ProcedureAst} from '../../parser'
 import {Serialization} from '../../Serialization'
-import {coerceScalarToBoolean, coerceScalarToString, coerceToRange} from '../ArithmeticHelper'
+import {coerceScalarToBoolean, coerceScalarToString, coerceToRange, complex} from '../ArithmeticHelper'
 import {Interpreter} from '../Interpreter'
 import {InterpreterValue, SimpleRangeValue} from '../InterpreterValue'
 
@@ -178,7 +178,7 @@ export abstract class FunctionPlugin {
 
   public coerceScalarToNumberOrError = (arg: InternalScalarValue): number | CellError => this.interpreter.arithmeticHelper.coerceScalarToNumberOrError(arg)
 
-  public coerceToType(arg: InterpreterValue, coercedType: FunctionArgument): Maybe<InterpreterValue> {
+  public coerceToType(arg: InterpreterValue, coercedType: FunctionArgument): Maybe<InterpreterValue | complex> {
     if (arg instanceof SimpleRangeValue) {
       switch(coercedType.argumentType) {
         case ArgumentTypes.RANGE:
@@ -226,7 +226,6 @@ export abstract class FunctionPlugin {
           }
           return coerceToRange(arg)
         case ArgumentTypes.COMPLEX:
-          // @ts-ignore
           return this.interpreter.arithmeticHelper.coerceScalarToComplex(arg)
       }
     }
@@ -247,7 +246,7 @@ export abstract class FunctionPlugin {
       scalarValues = args.map((ast) => [this.evaluateAst(ast, formulaAddress), false])
     }
 
-    const coercedArguments: Maybe<InterpreterValue>[] = []
+    const coercedArguments: Maybe<InterpreterValue | complex>[] = []
 
     let argCoerceFailure: Maybe<CellError> = undefined
     if (functionDefinition.repeatLastArgs === undefined && argumentDefinitions.length < scalarValues.length) {
