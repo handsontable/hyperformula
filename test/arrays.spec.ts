8617f6b20f1b89ccaab2591655cbe1425f8659a6
izulin <izulin@gmail.com>
2021-03-21 01:14:43 +0100

    index test

--- test/arrays.spec.ts
+++ test/arrays.spec.ts
@@ -1,5 +1,5 @@
-import {HyperFormula} from '../src'
-import {adr} from './testUtils'
+import {ErrorType, HyperFormula} from '../src'
+import {adr, detailedErrorWithOrigin} from './testUtils'
 
 describe('OPs', () => {
   it('unary op', () => {
@@ -15,5 +15,24 @@ describe('OPs', () => {
   it('index', () => {
     const engine = HyperFormula.buildFromArray([[1,2,3],['=INDEX(2*A1:C1+3,1,1)']], {arrays: true})
     expect(engine.getCellValue(adr('A2'))).toEqual(5)
+  })
+
+  it('binary op + index', () => {
+    const engine = HyperFormula.buildFromArray([
+      [1,2,3],
+      [4,5,6],
+      [7,8,9],
+      ['=INDEX(A1:C2+A1:B3,1,1)', '=INDEX(A1:C2+A1:B3,1,2)', '=INDEX(A1:C2+A1:B3,1,3)'],
+      ['=INDEX(A1:C2+A1:B3,2,1)', '=INDEX(A1:C2+A1:B3,2,2)', '=INDEX(A1:C2+A1:B3,2,3)'],
+      ['=INDEX(A1:C2+A1:B3,3,1)', '=INDEX(A1:C2+A1:B3,3,2)', '=INDEX(A1:C2+A1:B3,3,3)'],
+    ], {arrays: true})
+    expect(engine.getSheetValues(0)).toEqual(
+      [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+      [2,4,detailedErrorWithOrigin(ErrorType.NA, "Sheet1!C4")],
+      [8,10,detailedErrorWithOrigin(ErrorType.NA, "Sheet1!C5")],
+      [detailedErrorWithOrigin(ErrorType.NA, "Sheet1!A6"),detailedErrorWithOrigin(ErrorType.NA, "Sheet1!B6"),detailedErrorWithOrigin(ErrorType.NA, "Sheet1!C6")]])
   })
 })
