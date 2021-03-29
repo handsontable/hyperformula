e1b6746a1309fd07823aa955baa38fef2996c43d
izulin <izulin@gmail.com>
2021-03-29 17:31:32 +0200

    test

---
+++ test/escaping-support.spec.ts
@@ -0,0 +1,10 @@
+import {HyperFormula} from '../src'
+import {adr} from './testUtils'
+
+describe('escaped formulas', () => {
+  it('should serialize properly', () => {
+    const engine = HyperFormula.buildFromArray([['\'=SUM(2,2)']], {licenseKey: 'non-commercial-and-evaluation'})
+    expect(engine.getCellSerialized(adr('A1'))).toEqual('\'=SUM(2,2)')
+    expect(engine.getCellValue(adr('A1'))).toEqual('\'=SUM(2,2)')
+  })
+})
