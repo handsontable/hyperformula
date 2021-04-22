d2cb7edd7a22bd37fb24a96ea76e01775b325d73
izulin <izulin@gmail.com>
2021-04-22 14:08:49 +0200

    doc

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+- Fixed issues with scoped named expression. (#646, #641)
+
 ## [0.5.0] - 2021-04-15
 
 ### Changed
--- test/scoped-namedExpressions.spec.ts
+++ test/scoped-namedExpressions.spec.ts
@@ -5,17 +5,27 @@ describe('scoped named expressions', () => {
   it('should be removed when sheet is removed', () => {
     const engine = HyperFormula.buildFromSheets({'Sheet1': [[]], 'Sheet2': [[]]})
     engine.addNamedExpression('TRUE', true, 'Sheet1')
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
     // @ts-ignore
     expect(engine._namedExpressions.getAllNamedExpressionsNames()).toEqual(['TRUE'])
     engine.removeSheet('Sheet1')
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
     // @ts-ignore
     expect(engine._namedExpressions.getAllNamedExpressionsNames()).toEqual([])
   })
 
-  it('removal should work with undo', () => {
+  it('removal should work with undo of sheet', () => {
     const engine = HyperFormula.buildFromArray([['=TRUE']])
     engine.addNamedExpression('TRUE', true, 'Sheet1')
     engine.removeSheet('Sheet1')
+    engine.undo()
+    expect(engine.getCellValue(adr('A1'))).toEqual(true)
+  })
+
+  it('removal should work with undo of named expression', () => {
+    const engine = HyperFormula.buildFromArray([['=TRUE']])
+    engine.addNamedExpression('TRUE', true, 'Sheet1')
+    engine.removeNamedExpression('TRUE', 'Sheet1')
     engine.undo()
     expect(engine.getCellValue(adr('A1'))).toEqual(true)
   })
