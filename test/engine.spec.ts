5babee126871b186c6b4b9474f0817e0b16e2edc
izulin <izulin@gmail.com>
2021-05-27 15:24:39 +0200

    extra tests

--- test/engine.spec.ts
+++ test/engine.spec.ts
@@ -776,12 +776,30 @@ describe('Graph dependency topological ordering module', () => {
 })
 
 describe('#getFillRangeData', () => {
-  it('should properly apply wrap-around', () => {
-    const engine = HyperFormula.buildFromArray([[1, '=A1'], ['=$A$1', '2']])
+  it('should properly apply wrap-around #1', () => {
+    const engine = HyperFormula.buildFromArray([[], [undefined, 1, '=A1'], [undefined, '=$A$1', '2']])
 
     expect(engine.getFillRangeData(
-      AbsoluteCellRange.fromCoordinates(0, 0, 0, 1, 1),
-      AbsoluteCellRange.fromCoordinates(0, 1, 1, 3, 3))
+      AbsoluteCellRange.fromCoordinates(0, 1, 1, 2, 2),
+      AbsoluteCellRange.fromCoordinates(0, 2, 2, 4, 4))
     ).toEqual([['2', '=$A$1', '2'], ['=A3', 1, '=C3'], ['2', '=$A$1', '2']])
+  })
+
+  it('should properly apply wrap-around #2', () => {
+    const engine = HyperFormula.buildFromArray([[], [undefined, 1, '=A1'], [undefined, '=$A$1', '2']])
+
+    expect(engine.getFillRangeData(
+      AbsoluteCellRange.fromCoordinates(0, 1, 1, 2, 2),
+      AbsoluteCellRange.fromCoordinates(0, 1, 1, 3, 3))
+    ).toEqual([[1, '=A1', 1], ['=$A$1', '2', '=$A$1'], [1, '=A3', 1]])
+  })
+
+  it('should properly apply wrap-around #3', () => {
+    const engine = HyperFormula.buildFromArray([[], [undefined, 1, '=A1'], [undefined, '=$A$1', '2']])
+
+    expect(engine.getFillRangeData(
+      AbsoluteCellRange.fromCoordinates(0, 1, 1, 2, 2),
+      AbsoluteCellRange.fromCoordinates(0, 0, 0, 2, 2))
+    ).toEqual([['2', '=$A$1', '2'], ['=#REF!', 1, '=A1'], ['2', '=$A$1', '2'] ])
   })
 })
