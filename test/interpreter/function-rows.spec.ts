3050d9f74e98d57474ecfb1d3b5d674348124587
Przemysław Uznański <40573492+izulin@users.noreply.github.com>
2021-05-28 16:14:40 +0200

    rows/columns fixed to work with arrays (#677)
    * parsing of matrices
    
    * size prediction
    
    * evaluation, parsing, unparsing
    
    * tests
    
    * doc
    
    * linter
    
    * compiles
    
    * linter
    
    * small refactor
    
    * kind of works
    
    * cleanup
    
    * linter
    
    * extra config options
    
    * tests
    
    * config
    
    * rows/columns fixed to work with arrays
    
    * doc
    
    * Update CHANGELOG.md
    
    * docs
    
    * compiles

--- test/interpreter/function-rows.spec.ts
+++ test/interpreter/function-rows.spec.ts
@@ -17,34 +17,50 @@ describe('Function ROWS', () => {
     expect(engine.getCellValue(adr('A1'))).toEqual(2)
   })
 
-  // Inconsistency with Product 1
-  it('doesnt work with scalars', () => {
-    const engine = HyperFormula.buildFromArray([['=ROWS(A1)']])
+  it('works for row range', () => {
+    const engine = HyperFormula.buildFromArray([['=ROWS(1:3)']])
 
-    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.CellRangeExpected))
+    expect(engine.getCellValue(adr('A1'))).toEqual(3)
   })
 
-  // Inconsistency with Product 1
-  it('propagate only direct errors', () => {
+  it('works for column range', () => {
+    const engine = HyperFormula.buildFromArray([['=ROWS(A:C)']])
+
+    expect(engine.getCellValue(adr('A1'))).toEqual(engine.getConfig().maxRows)
+  })
+
+  it('works for array', () => {
+    const engine = HyperFormula.buildFromArray([['=ROWS({1;2;3})']])
+
+    expect(engine.getCellValue(adr('A1'))).toEqual(3)
+  })
+
+  it('works with cell reference', () => {
+    const engine = HyperFormula.buildFromArray([['=ROWS(A1)']])
+
+    expect(engine.getCellValue(adr('A1'))).toEqual(1)
+  })
+
+  it('propagates only direct errors', () => {
     const engine = HyperFormula.buildFromArray([
       ['=4/0'],
       ['=ROWS(4/0)'],
       ['=ROWS(A1)'],
     ])
 
-    expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.CellRangeExpected))
-    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.CellRangeExpected))
+    expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.DIV_BY_ZERO))
+    expect(engine.getCellValue(adr('A3'))).toEqual(1)
   })
 
   // Inconsistency with Product 1
-  it('doesnt work with formulas', () => {
+  it('works with formulas', () => {
     const engine = HyperFormula.buildFromArray([
       ['1', '1'],
       ['1', '1'],
       ['=ROWS(MMULT(A1:B2, A1:B2))'],
     ])
 
-    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.CellRangeExpected))
+    expect(engine.getCellValue(adr('A3'))).toEqual(2)
   })
 
   it('should work when adding column', () => {
