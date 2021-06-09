53a5af553a2f20b6388012f215fd1649c6c99443
izulin <izulin@gmail.com>
2021-06-10 01:02:59 +0200

    cleanup

--- src/Cell.ts
+++ src/Cell.ts
@@ -195,22 +195,6 @@ export function isSimpleCellAddress(obj: any): obj is SimpleCellAddress {
   }
 }
 
-export function isSimpleColumnAddress(obj: any): obj is SimpleColumnAddress {
-  if( obj && (typeof obj === 'object' || typeof obj === 'function')) {
-    return 'col' in obj && typeof obj.col === 'number' && !('row' in obj) && 'sheet' in obj && typeof obj.sheet === 'number'
-  } else {
-    return false
-  }
-}
-
-export function isSimpleRowAddress(obj: any): obj is SimpleRowAddress {
-  if( obj && (typeof obj === 'object' || typeof obj === 'function')) {
-    return !('col' in obj) && 'row' in obj && typeof obj.row === 'number' && 'sheet' in obj && typeof obj.sheet === 'number'
-  } else {
-    return false
-  }
-}
-
 export const absoluteSheetReference = (address: AddressWithSheet, baseAddress: SimpleCellAddress): number => {
   return address.sheet === null ? baseAddress.sheet : address.sheet
 }
