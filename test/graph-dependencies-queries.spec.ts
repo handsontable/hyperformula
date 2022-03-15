de3bf460a58c30020172ffe2c9da7a7f71d791b3
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2022-03-15 14:31:59 +0100

    Add test 'getCellDependents should return reversed dependencies across sheets'

--- test/graph-dependencies-queries.spec.ts
+++ test/graph-dependencies-queries.spec.ts
@@ -3,35 +3,61 @@ import {simpleCellRange} from '../src/AbsoluteCellRange'
 import {adr} from './testUtils'
 
 describe('address queries', () => {
-  it('reverse dependencies should work', () => {
-    const engine = HyperFormula.buildFromArray([
-      [1, 2, 3],
-      ['=SUM(A1:B1)', '=SUMSQ(A1:B1)'],
-      ['=A2+B2'],
-    ])
-    expect(engine.getCellDependents(adr('A1'))).toEqual([simpleCellRange(adr('A1'), adr('B1'))])
-    expect(engine.getCellDependents(adr('D1'))).toEqual([])
-    expect(engine.getCellDependents(adr('A2'))).toEqual([adr('A3')])
-    expect(engine.getCellDependents(adr('B2'))).toEqual([adr('A3')])
-    expect(engine.getCellDependents(adr('A3'))).toEqual([])
+  describe('getCellDependents', () => {
+    it('should return reversed dependencies', () => {
+      const engine = HyperFormula.buildFromArray([
+        [1, 2, 3],
+        ['=SUM(A1:B1)', '=SUMSQ(A1:B1)'],
+        ['=A2+B2'],
+      ])
+      expect(engine.getCellDependents(adr('A1'))).toEqual([simpleCellRange(adr('A1'), adr('B1'))])
+      expect(engine.getCellDependents(adr('D1'))).toEqual([])
+      expect(engine.getCellDependents(adr('A2'))).toEqual([adr('A3')])
+      expect(engine.getCellDependents(adr('B2'))).toEqual([adr('A3')])
+      expect(engine.getCellDependents(adr('A3'))).toEqual([])
 
-    expect(engine.getCellDependents(simpleCellRange(adr('A1'), adr('B1')))).toEqual([adr('A2'), adr('B2')])
-    expect(engine.getCellDependents(simpleCellRange(adr('A3'), adr('B3')))).toEqual([])
+      expect(engine.getCellDependents(simpleCellRange(adr('A1'), adr('B1')))).toEqual([adr('A2'), adr('B2')])
+      expect(engine.getCellDependents(simpleCellRange(adr('A3'), adr('B3')))).toEqual([])
+    })
+
+    it('should return reversed dependencies across sheets', () => {
+      const engine = HyperFormula.buildFromSheets(
+        {
+          'DataSheet': [[1, 2, 3, '=A1']],
+          'DependentSheet': [['=DataSheet!A1', '=DataSheet!A1', '=DataSheet!A1+DataSheet!B1', '=A1']],
+        }
+      )
+
+      const dataSheetId = engine.getSheetId('DataSheet')
+      const dependentSheetId = engine.getSheetId('DependentSheet')
+
+      expect(engine.getCellDependents(adr('A1', dataSheetId))).toEqual([
+        adr('D1', dataSheetId),
+        adr('A1', dependentSheetId),
+        adr('B1', dependentSheetId),
+        adr('C1', dependentSheetId)
+      ])
+      expect(engine.getCellDependents(adr('B1', dataSheetId))).toEqual([adr('C1', dependentSheetId)])
+      expect(engine.getCellDependents(adr('C1', dataSheetId))).toEqual([])
+      expect(engine.getCellDependents(adr('D1', dataSheetId))).toEqual([])
+    })
   })
 
-  it('dependencies should work', () => {
-    const engine = HyperFormula.buildFromArray([
-      [1, 2, 3],
-      ['=SUM(A1:B1)', '=SUMSQ(A1:B1)'],
-      ['=A2+B2'],
-    ])
-    expect(engine.getCellPrecedents(adr('A1'))).toEqual([])
-    expect(engine.getCellPrecedents(adr('D1'))).toEqual([])
-    expect(engine.getCellPrecedents(adr('A2'))).toEqual([simpleCellRange(adr('A1'), adr('B1'))])
-    expect(engine.getCellPrecedents(adr('B2'))).toEqual([simpleCellRange(adr('A1'), adr('B1'))])
-    expect(engine.getCellPrecedents(adr('A3'))).toEqual([adr('A2'), adr('B2')])
+  describe('getCellPrecedents', () => {
+    it('should return dependencies', () => {
+      const engine = HyperFormula.buildFromArray([
+        [1, 2, 3],
+        ['=SUM(A1:B1)', '=SUMSQ(A1:B1)'],
+        ['=A2+B2'],
+      ])
+      expect(engine.getCellPrecedents(adr('A1'))).toEqual([])
+      expect(engine.getCellPrecedents(adr('D1'))).toEqual([])
+      expect(engine.getCellPrecedents(adr('A2'))).toEqual([simpleCellRange(adr('A1'), adr('B1'))])
+      expect(engine.getCellPrecedents(adr('B2'))).toEqual([simpleCellRange(adr('A1'), adr('B1'))])
+      expect(engine.getCellPrecedents(adr('A3'))).toEqual([adr('A2'), adr('B2')])
 
-    expect(engine.getCellPrecedents(simpleCellRange(adr('A1'), adr('B1')))).toEqual([adr('A1'), adr('B1')])
-    expect(engine.getCellPrecedents(simpleCellRange(adr('A3'), adr('B3')))).toEqual([])
+      expect(engine.getCellPrecedents(simpleCellRange(adr('A1'), adr('B1')))).toEqual([adr('A1'), adr('B1')])
+      expect(engine.getCellPrecedents(simpleCellRange(adr('A3'), adr('B3')))).toEqual([])
+    })
   })
 })
