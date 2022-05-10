cf006ea8cc97c46c9ce317152d9efac09add17d1
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2022-05-10 12:57:33 +0100

    Change search algorithm to return the page iff it contains all the query strings in any order (but not necessarily as whole words)

--- docs/.vuepress/plugins/search-box/match-query.js
+++ docs/.vuepress/plugins/search-box/match-query.js
@@ -16,36 +16,12 @@ export default (query, page, additionalStr = null) => {
 }
 
 const matchTest = (query, domain) => {
-  const escapeRegExp = str => str.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')
-
-  // eslint-disable-next-line no-control-regex
-  const nonASCIIRegExp = new RegExp('[^\x00-\x7F]')
-
+  const lowerCaseDomain = domain.toLowerCase()
   const words = query
+    .toLowerCase()
     .split(/\s+/g)
     .map(str => str.trim())
     .filter(str => !!str)
 
-  if (!nonASCIIRegExp.test(query)) {
-    // if the query only has ASCII chars, treat as English
-    const hasTrailingSpace = query.endsWith(' ')
-    const searchRegex = new RegExp(
-      words
-      .map((word, index) => {
-        if (words.length === index + 1 && !hasTrailingSpace) {
-          // The last word - ok with the word being "startswith"-like
-          return `(?=.*\\b${escapeRegExp(word)})`
-        } else {
-          // Not the last word - expect the whole word exactly
-          return `(?=.*\\b${escapeRegExp(word)}\\b)`
-        }
-      })
-      .join('') + '.+',
-      'gi'
-    )
-    return searchRegex.test(domain)
-  } else {
-    // if the query has non-ASCII chars, treat as other languages
-    return words.some(word => domain.toLowerCase().indexOf(word) > -1)
-  }
+  return words.every(word => lowerCaseDomain.indexOf(word) > -1)
 }
