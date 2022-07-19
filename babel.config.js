8cdcb975925606c507a392d55b01d7e97580bcdf
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2022-07-19 12:56:46 +0200

    Change babel configuration to polyfill only the UMD bundles

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,30 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', {
-      modules: false,
-      useBuiltIns: 'usage',
-      corejs: '3.22',
-    }]
-  ],
   plugins: [
-    ['transform-inline-environment-variables']
+    ['transform-inline-environment-variables'],
   ],
   env: {
     // Environment for transpiling files to be compatible with UMD.
-    commonjs: {
+    dist: {
+      presets: [
+        ['@babel/preset-env', {
+          modules: false,
+          useBuiltIns: 'usage',
+          corejs: '3.23',
+        }]
+      ],
       plugins: [
         ['@babel/plugin-transform-runtime', {
           corejs: false,
           helpers: false,
           regenerator: false,
           useESModules: false,
+          version: '^7.18.9',
         }],
         ['@babel/plugin-transform-modules-commonjs', { loose: true }]
       ]
     },
     // Environment for transpiling files to be compatible with CommonJS.
-    commonjs_dist: {
+    commonjs: {
       plugins: [
         ['@babel/plugin-transform-modules-commonjs', { loose: true }],
       ],
