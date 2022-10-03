aa6c3008ade399dd01e8459e6d1c862948edd4e3
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2022-10-03 13:13:47 +0200

    Add unit tests for function MATCH with MatchType = 1 and MatchType = -1

--- test/interpreter/function-match.spec.ts
+++ test/interpreter/function-match.spec.ts
@@ -106,7 +106,7 @@ describe('Function MATCH', () => {
         expect(engine.getCellValue(adr('A1'))).toEqual(2)
       })
 
-      it('column - doesn\'t return result from outside the search range', () => {
+      it('doesn\'t return result from outside the search range', () => {
         const engine = HyperFormula.buildFromArray([
           ['=MATCH(103, A3:A6, 0)'],
           ['103', '103'],
@@ -158,7 +158,7 @@ describe('Function MATCH', () => {
         expect(engine.getCellValue(adr('A1'))).toEqual(2)
       })
 
-      it('column - doesn\'t return result from outside the search range', () => {
+      it('doesn\'t return result from outside the search range', () => {
         const engine = HyperFormula.buildFromArray([
           ['=MATCH(103, B2:E2, 0)'],
           ['103', '200', '200', '200', '200', '103'],
@@ -210,21 +210,233 @@ describe('Function MATCH', () => {
     it('works with dates', () => {
       const engine = HyperFormula.buildFromArray(
         [
-          ['01/01/2012', '01/01/2012', '=MATCH(B1, A1:A4, 0)'],
-          ['=EOMONTH(A1,1)', '29/02/2012', '=MATCH(B2, A1:A4, 0)'],
-          ['=EOMONTH(A2,1)'],
-          ['=EOMONTH(A3,1)'],
-        ],
-        {
-          licenseKey: 'gpl-v3',
-        }
+          ['01/04/2012', '01/01/2012', '=MATCH(B1, A1:A4, 0)'],
+          ['01/01/2012', '01/02/2012', '=MATCH(B2, A1:A4, 0)'],
+          ['01/02/2012'],
+          ['01/03/2012'],
+        ]
       )
-      expect(engine.getCellValue({ sheet: 0, row: 0, col: 2 })).toEqual(1)
-      expect(engine.getCellValue({ sheet: 0, row: 1, col: 2 })).toEqual(2)
+      expect(engine.getCellValue(adr('C1'))).toEqual(2)
+      expect(engine.getCellValue(adr('C2'))).toEqual(3)
     })
   })
 
   describe('when MatchType = 1', () => {
+    describe('when search range is vertical', () => {
+      it('returns the lower bound if the range is sorted ascending', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(113, A2:A5, 1)'],
+          ['100'],
+          ['110'],
+          ['120'],
+          ['130'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(2)
+      })
+
+      it('returns the exact match if present in the range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(120, A2:A5, 1)'],
+          ['100'],
+          ['110'],
+          ['120'],
+          ['130'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(3)
+      })
+
+      it('returns the last match if there are duplicates in the search range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(110, A2:A5, 1)'],
+          ['110'],
+          ['110'],
+          ['110'],
+          ['110'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(4)
+      })
+
+      it('returns the last value if all are smaller than the search value', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(1000, A2:A5, 1)'],
+          ['100'],
+          ['110'],
+          ['120'],
+          ['130'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(4)
+      })
+
+      it('returns an error if all are greater than the search value', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(113, A2:A5, 1)'],
+          ['200'],
+          ['210'],
+          ['220'],
+          ['230'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.ValueNotFound))
+      })
+
+      it('returns the relative position in the range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(113, A6:A9, 1)'],
+          [''],
+          [''],
+          [''],
+          [''],
+          ['100'],
+          ['110'],
+          ['120'],
+          ['130'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(2)
+      })
+
+      it('assumes that the range is sorted ascending', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(113, A2:A5, 1)'],
+          ['100'],
+          ['120'],
+          ['110'],
+          ['113'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(1)
+      })
+
+      it('doesn\'t return result from outside the search range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(103, A3:A6, 1)'],
+          ['103', '103'],
+          ['200', '103'],
+          ['200', '103'],
+          ['200', '103'],
+          ['200', '103'],
+          ['103', '103'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.ValueNotFound))
+      })
+    })
+
+    describe('when search range is horizontal', () => {
+      it('returns the lower bound if the range is sorted ascending', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(113, A2:D2, 1)'],
+          ['100', '110', '120', '130'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(2)
+      })
+
+      it('returns the exact match if present in the range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(120, A2:D2, 1)'],
+          ['100', '110', '120', '130'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(3)
+      })
+
+      it('returns the last match if there are duplicates in the search range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(110, A2:D2, 1)'],
+          ['110', '110', '110', '110'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(4)
+      })
+
+      it('returns the last value if all are smaller than the search value', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(1000, A2:D2, 1)'],
+          ['100', '110', '120', '130'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(4)
+      })
+
+      it('returns an error if all are greater than the search value', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(1, A2:D2, 1)'],
+          ['100', '110', '120', '130'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.ValueNotFound))
+      })
+
+      it('returns the relative position in the range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(112, E2:H2, 1)'],
+          ['', '', '', '', '100', '110', '120', '130'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(2)
+      })
+
+      it('assumes that the range is sorted ascending', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(113, A2:D2, 1)'],
+          ['100', '130', '110', '113'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(1)
+      })
+
+      it('doesn\'t return result from outside the search range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(103, B2:E2, 1)'],
+          ['103', '200', '200', '200', '200', '103'],
+          ['103', '103', '103', '103', '103', '103'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.ValueNotFound))
+      })
+    })
+
+    it('works for strings, is not case sensitive', () => {
+      const engine = HyperFormula.buildFromArray([
+        ['=MATCH("C", A2:A5, 1)'],
+        ['a'],
+        ['b'],
+        ['d'],
+        ['e'],
+      ])
+
+      expect(engine.getCellValue(adr('A1'))).toEqual(2)
+    })
+
+    it('works for strings, is not case sensitive even if config defines case sensitivity', () => {
+      const engine = HyperFormula.buildFromArray([
+        ['=MATCH("C", A2:A5, 1)'],
+        ['a'],
+        ['b'],
+        ['d'],
+        ['e'],
+      ], { caseSensitive: true })
+
+      expect(engine.getCellValue(adr('A1'))).toEqual(2)
+    })
+
+    it('works with dates', () => {
+      const engine = HyperFormula.buildFromArray(
+        [
+          ['01/01/2012', '01/02/2012', '=MATCH(B1, A1:A4, 1)'],
+          ['01/02/2012', '10/02/2012', '=MATCH(B2, A1:A4, 1)'],
+          ['01/03/2012'],
+          ['01/04/2012'],
+        ]
+      )
+      expect(engine.getCellValue(adr('C1'))).toEqual(2)
+      expect(engine.getCellValue(adr('C2'))).toEqual(3)
+    })
 
     it('uses binsearch', () => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -232,36 +444,250 @@ describe('Function MATCH', () => {
       resetSpy(spy)
 
       const engine = HyperFormula.buildFromArray([
-        ['=MATCH(400, A2:A5, 1)'],
+        ['=MATCH(113, A2:A5, 1)'],
         ['100'],
-        ['200'],
-        ['300'],
-        ['400'],
-        ['500'],
+        ['110'],
+        ['120'],
+        ['130'],
       ])
 
+      expect(engine.getCellValue(adr('A1'))).toEqual(2)
       expect(spy).not.toHaveBeenCalled()
-      expect(engine.getCellValue(adr('A1'))).toEqual(4)
     })
   })
 
   describe('when MatchType = -1', () => {
+    describe('when search range is vertical', () => {
+      it('returns the upper bound if the range is sorted descending', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(113, A2:A5, -1)'],
+          ['130'],
+          ['120'],
+          ['110'],
+          ['100'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(2)
+      })
+
+      it('returns the exact match if present in the range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(120, A2:A5, -1)'],
+          ['130'],
+          ['120'],
+          ['110'],
+          ['100'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(2)
+      })
+
+      it('returns the last match if there are duplicates in the search range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(110, A2:A5, -1)'],
+          ['110'],
+          ['110'],
+          ['110'],
+          ['110'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(4)
+      })
+
+      it('returns the last value if all are greater than the search value', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(1, A2:A5, -1)'],
+          ['130'],
+          ['120'],
+          ['110'],
+          ['100'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(4)
+      })
+
+      it('returns an error if all are smaller than the search value', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(1000, A2:A5, -1)'],
+          ['130'],
+          ['120'],
+          ['110'],
+          ['100'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.ValueNotFound))
+      })
+
+      it('returns the relative position in the range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(113, A6:A9, -1)'],
+          [''],
+          [''],
+          [''],
+          [''],
+          ['130'],
+          ['120'],
+          ['110'],
+          ['100'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(2)
+      })
+
+      it('assumes that the range is sorted ascending', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(113, A2:A5, -1)'],
+          ['130'],
+          ['100'],
+          ['120'],
+          ['113'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(1)
+      })
+
+      it('doesn\'t return result from outside the search range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(103, A3:A6, -1)'],
+          ['103', '103'],
+          ['200', '103'],
+          ['200', '103'],
+          ['200', '103'],
+          ['200', '103'],
+          ['103', '103'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.ValueNotFound))
+      })
+    })
+
+    describe('when search range is horizontal', () => {
+      it('returns the lower bound if the range is sorted ascending', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(113, A2:D2, -1)'],
+          ['130', '120', '110', '100'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(2)
+      })
+
+      it('returns the exact match if present in the range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(120, A2:D2, -1)'],
+          ['130', '120', '110', '100'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(2)
+      })
+
+      it('returns the last match if there are duplicates in the search range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(110, A2:D2, -1)'],
+          ['110', '110', '110', '110'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(4)
+      })
+
+      it('returns the last value if all are greater than the search value', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(1, A2:D2, -1)'],
+          ['130', '120', '110', '100'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(4)
+      })
+
+      it('returns an error if all are smaller than the search value', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(1000, A2:D2, -1)'],
+          ['130', '120', '110', '100'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.ValueNotFound))
+      })
+
+      it('returns the relative position in the range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(112, E2:H2, -1)'],
+          ['', '', '', '', '130', '120', '110', '100'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(2)
+      })
+
+      it('assumes that the range is sorted ascending', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(113, A2:D2, -1)'],
+          ['130', '100', '120', '113'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqual(1)
+      })
+
+      it('doesn\'t return result from outside the search range', () => {
+        const engine = HyperFormula.buildFromArray([
+          ['=MATCH(103, B2:E2, -1)'],
+          ['103', '200', '200', '200', '200', '103'],
+          ['103', '103', '103', '103', '103', '103'],
+        ])
+
+        expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.ValueNotFound))
+      })
+    })
+
+    it('works for strings, is not case sensitive', () => {
+      const engine = HyperFormula.buildFromArray([
+        ['=MATCH("C", A2:A5, -1)'],
+        ['e'],
+        ['d'],
+        ['b'],
+        ['a'],
+      ])
+
+      expect(engine.getCellValue(adr('A1'))).toEqual(2)
+    })
+
+    it('works for strings, is not case sensitive even if config defines case sensitivity', () => {
+      const engine = HyperFormula.buildFromArray([
+        ['=MATCH("C", A2:A5, -1)'],
+        ['e'],
+        ['d'],
+        ['b'],
+        ['a'],
+      ], { caseSensitive: true })
+
+      expect(engine.getCellValue(adr('A1'))).toEqual(2)
+    })
+
+    it('works with dates', () => {
+      const engine = HyperFormula.buildFromArray(
+        [
+          ['01/04/2012', '01/02/2012', '=MATCH(B1, A1:A4, -1)'],
+          ['01/03/2012', '10/02/2012', '=MATCH(B2, A1:A4, -1)'],
+          ['01/02/2012'],
+          ['01/01/2012'],
+        ]
+      )
+      expect(engine.getCellValue(adr('C1'))).toEqual(3)
+      expect(engine.getCellValue(adr('C2'))).toEqual(2)
+    })
+
     it('uses binsearch', () => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const spy = spyOn(DependencyGraph.prototype as any, 'computeListOfValuesInRange')
       resetSpy(spy)
 
       const engine = HyperFormula.buildFromArray([
-        ['=MATCH(400, A2:A5, -1)'],
-        ['500'],
-        ['400'],
-        ['300'],
-        ['200'],
+        ['=MATCH(113, A2:A5, -1)'],
+        ['130'],
+        ['120'],
+        ['110'],
         ['100'],
       ])
 
-      expect(spy).not.toHaveBeenCalled()
       expect(engine.getCellValue(adr('A1'))).toEqual(2)
+      expect(spy).not.toHaveBeenCalled()
     })
   })
 
