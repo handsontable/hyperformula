ff28485d23fd4069bd6cf77cfe6f7aca60377095
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2023-02-16 10:08:48 +0100

    Add VertexMissingInAddressMapping error

--- src/errors.ts
+++ src/errors.ts
@@ -394,3 +394,12 @@ export class AliasAlreadyExisting extends Error {
     super(`Alias id ${name} in plugin ${pluginName} already defined as a function or alias.`)
   }
 }
+
+/**
+ * Error thrown when no vertex was found for specified address in AddressMapping object.
+ */
+export class VertexMissingInAddressMapping extends Error {
+  constructor(address: SimpleCellAddress) {
+    super(`Vertex missing in AddressMapping for address { sheet: ${address.sheet}, col: ${address.col}, row: ${address.row} }`)
+  }
+}
