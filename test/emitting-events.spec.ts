9fbedd681dd017d4312ee7f04ab5a9adf6675ba6
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2023-04-04 16:04:49 +0200

    Add tests for all methods that should not trigger _CellValueRead event

--- test/emitting-events.spec.ts
+++ test/emitting-events.spec.ts
@@ -6,7 +6,7 @@ import {
   NamedExpressionDoesNotExistError,
 } from '../src'
 import {Events} from '../src/Emitter'
-import {adr, detailedErrorWithOrigin} from './testUtils'
+import {adr, detailedErrorWithOrigin, resetSpy} from './testUtils'
 import {simpleCellRange} from '../src/AbsoluteCellRange'
 
 describe('Events', () => {
@@ -208,170 +208,179 @@ describe('Events', () => {
   })
 
   describe('_CellValueRead', () => {
-    it('fires when user calls getCellValue', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]])
+    it('fires on read operations', () => {
+      const readOperations = [
+        (engine: HyperFormula) => engine.getCellValue(adr('A1')),
+        (engine: HyperFormula) => engine.getSheetValues(0),
+        (engine: HyperFormula) => engine.getAllSheetsValues(),
+        (engine: HyperFormula) => engine.copy(simpleCellRange(adr('A1'), adr('C1'))),
+        (engine: HyperFormula) => engine.cut(simpleCellRange(adr('A1'), adr('C1'))),
+        (engine: HyperFormula) => engine.getRangeValues(simpleCellRange(adr('A1'), adr('C1'))),
+        (engine: HyperFormula) => engine.calculateFormula('=A1', 0),
+        (engine: HyperFormula) => {
+          engine.addNamedExpression('ABC', '=Sheet1!$A$1')
+          engine.getNamedExpressionValue('ABC')
+        },
+      ]
 
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
+      readOperations.forEach(operation => {
+        const engine = HyperFormula.buildFromArray([[1, 2, '=SUM(A1:B1)']])
+        const onCellValueRead = jasmine.createSpy()
+        engine.on(Events._CellValueRead, onCellValueRead)
 
-      engine.getCellValue(adr('A1'))
-      expect(onCellValueRead).toHaveBeenCalled()
+        operation(engine)
+
+        expect(onCellValueRead).toHaveBeenCalled()
+        resetSpy(onCellValueRead)
+      })
     })
 
-    it('fires when user calls getSheetValues', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]])
+    it('does not fire on other operations', () => {
+      const otherOperations = [
+        (engine: HyperFormula) => engine.addNamedExpression('ABC', '=Sheet1!$A$1'),
+        (engine: HyperFormula) => engine.isItPossibleToAddNamedExpression('ABC', '=Sheet1!$A$1'),
+        (engine: HyperFormula) => engine.updateConfig({ maxRows: 100 }),
+        (engine: HyperFormula) => engine.setCellContents(adr('A1'), 42),
+        (engine: HyperFormula) => engine.setSheetContent(0, [[42]]),
+        (engine: HyperFormula) => engine.addSheet('test'),
+        (engine: HyperFormula) => engine.isItPossibleToAddSheet('test'),
+        (engine: HyperFormula) => engine.addRows(0, [0, 1]),
+        (engine: HyperFormula) => engine.addColumns(0, [0, 1]),
+        (engine: HyperFormula) => engine.getSheetId('Sheet1'),
+        (engine: HyperFormula) => engine.doesSheetExist('Sheet1'),
+        (engine: HyperFormula) => engine.getSheetName(0),
+        () => HyperFormula.defaultConfig,
+        () => HyperFormula.getRegisteredLanguagesCodes(),
+        () => HyperFormula.getLanguage('enGB'),
+        () => HyperFormula.unregisterFunction('SUM'),
+        () => HyperFormula.getFunctionPlugin('SUM'),
+        (engine: HyperFormula) => engine.getFunctionPlugin('SUM'),
+        () => HyperFormula.unregisterAllFunctions(),
+        () => HyperFormula.getAllFunctionPlugins(),
+        (engine: HyperFormula) => engine.getAllFunctionPlugins(),
+        (engine: HyperFormula) => engine.getCellFormula(adr('A1')),
+        (engine: HyperFormula) => engine.getCellSerialized(adr('C1')),
+        (engine: HyperFormula) => engine.getSheetFormulas(0),
+        (engine: HyperFormula) => engine.getSheetSerialized(0),
+        (engine: HyperFormula) => engine.getAllSheetsDimensions(),
+        (engine: HyperFormula) => engine.getSheetDimensions(0),
+        (engine: HyperFormula) => engine.getAllSheetsFormulas(),
+        (engine: HyperFormula) => engine.getAllSheetsSerialized(),
+        (engine: HyperFormula) => engine.getConfig(),
+        (engine: HyperFormula) => engine.getStats(),
+        (engine: HyperFormula) => engine.isThereSomethingToUndo(),
+        (engine: HyperFormula) => engine.isThereSomethingToRedo(),
+        (engine: HyperFormula) => engine.isItPossibleToSetCellContents(adr('A1')),
+        (engine: HyperFormula) => engine.setRowOrder(0, [0]),
+        (engine: HyperFormula) => engine.isItPossibleToSetRowOrder(0, [0]),
+        (engine: HyperFormula) => engine.swapRowIndexes(0, [[0, 0]]),
+        (engine: HyperFormula) => engine.isItPossibleToSwapRowIndexes(0, [[0, 0]]),
+        (engine: HyperFormula) => engine.swapColumnIndexes(0, [[0, 1], [1, 0]]),
+        (engine: HyperFormula) => engine.isItPossibleToSwapColumnIndexes(0, [[0, 1], [1, 0]]),
+        (engine: HyperFormula) => engine.setColumnOrder(0, [0, 2, 1]),
+        (engine: HyperFormula) => engine.isItPossibleToSetColumnOrder(0, [0, 2, 1]),
+        (engine: HyperFormula) => engine.isItPossibleToAddRows(0, [1, 1]),
+        (engine: HyperFormula) => engine.addRows(0, [1, 1]),
+        (engine: HyperFormula) => engine.isItPossibleToRemoveRows(0, [0, 1]),
+        (engine: HyperFormula) => engine.removeRows(0, [0, 1]),
+        (engine: HyperFormula) => engine.addColumns(0, [1, 1]),
+        (engine: HyperFormula) => engine.isItPossibleToAddColumns(0, [1, 1]),
+        (engine: HyperFormula) => engine.isItPossibleToRemoveColumns(0, [0, 1]),
+        (engine: HyperFormula) => engine.removeColumns(0, [0, 1]),
+        (engine: HyperFormula) => engine.moveCells({ start: adr('A1'), end: adr('A1') }, adr('C42')),
+        (engine: HyperFormula) => engine.isItPossibleToMoveCells({ start: adr('A1'), end: adr('A1') }, adr('C42')),
+        (engine: HyperFormula) => engine.isItPossibleToMoveRows(0, 0, 1, 2),
+        (engine: HyperFormula) => engine.moveRows(0, 0, 1, 2),
+        (engine: HyperFormula) => engine.isItPossibleToMoveColumns(0, 0, 1, 2),
+        (engine: HyperFormula) => engine.moveColumns(0, 0, 1, 2),
+        (engine: HyperFormula) => engine.isClipboardEmpty(),
+        (engine: HyperFormula) => engine.clearClipboard(),
+        (engine: HyperFormula) => engine.clearRedoStack(),
+        (engine: HyperFormula) => engine.clearUndoStack(),
+        (engine: HyperFormula) => engine.getRangeFormulas(simpleCellRange(adr('A1'), adr('C1'))),
+        (engine: HyperFormula) => engine.getRangeSerialized(simpleCellRange(adr('A1'), adr('C1'))),
+        (engine: HyperFormula) => engine.getFillRangeData( {start: {sheet: 0, row: 0, col: 0}, end: {sheet: 0, row: 1, col: 1}}, {start: {sheet: 0, row: 1, col: 1}, end: {sheet: 0, row: 3, col: 3}}),
+        (engine: HyperFormula) => engine.isItPossibleToRemoveSheet(0),
+        (engine: HyperFormula) => engine.removeSheet(0),
+        (engine: HyperFormula) => engine.isItPossibleToClearSheet(0),
+        (engine: HyperFormula) => engine.clearSheet(0),
+        (engine: HyperFormula) => engine.isItPossibleToReplaceSheetContent(0, [['50'], ['60']]),
+        (engine: HyperFormula) => engine.getCellDependents(adr('A1')),
+        (engine: HyperFormula) => engine.getCellPrecedents(adr('A1')),
+        (engine: HyperFormula) => engine.getSheetNames(),
+        (engine: HyperFormula) => engine.getCellType({ sheet: 0, col: 1, row: 0 }),
+        (engine: HyperFormula) => engine.getCellValueType({ sheet: 0, col: 1, row: 0 }),
+        (engine: HyperFormula) => engine.getCellValueDetailedType({ sheet: 0, col: 1, row: 0 }),
+        (engine: HyperFormula) => engine.getCellValueFormat({ sheet: 0, col: 1, row: 0 }),
+        (engine: HyperFormula) => engine.doesCellHaveFormula({ sheet: 0, col: 1, row: 0 }),
+        (engine: HyperFormula) => engine.doesCellHaveSimpleValue({ sheet: 0, col: 1, row: 0 }),
+        (engine: HyperFormula) => engine.isCellEmpty({ sheet: 0, col: 1, row: 0 }),
+        (engine: HyperFormula) => engine.isCellPartOfArray({ sheet: 0, col: 1, row: 0 }),
+        (engine: HyperFormula) => engine.countSheets(),
+        (engine: HyperFormula) => engine.isItPossibleToRenameSheet(0, 'MySheet0'),
+        (engine: HyperFormula) => engine.renameSheet(0, 'MySheet0'),
+        (engine: HyperFormula) => engine.suspendEvaluation(),
+        (engine: HyperFormula) => engine.resumeEvaluation(),
+        (engine: HyperFormula) => engine.isEvaluationSuspended(),
+        (engine: HyperFormula) => engine.listNamedExpressions(),
+        (engine: HyperFormula) => engine.normalizeFormula('=SUM(1,2,3)'),
+        (engine: HyperFormula) => engine.validateFormula('=SUM(1,2,3)'),
+        (engine: HyperFormula) => engine.getRegisteredFunctionNames(),
+        (engine: HyperFormula) => engine.numberToDateTime(43845.1),
+        (engine: HyperFormula) => engine.numberToDate(43845),
+        (engine: HyperFormula) => engine.numberToTime(1.1),
+        (engine: HyperFormula) => {
+          const adr = engine.simpleCellAddressFromString('A1', 0)
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          engine.simpleCellAddressToString(adr!, 0)
+        },
+        (engine: HyperFormula) => {
+          const range = engine.simpleCellRangeFromString('A1:B2', 0)
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          engine.simpleCellRangeToString(range!, 0)
+        },
+        (engine: HyperFormula) => {
+          engine.addNamedExpression('ABC', '=Sheet1!$A$1')
+          engine.getNamedExpression('ABC')
+        },
+        (engine: HyperFormula) => {
+          engine.addNamedExpression('ABC', '=Sheet1!$A$1')
+          engine.getNamedExpressionFormula('ABC')
+        },
+        (engine: HyperFormula) => {
+          engine.addNamedExpression('ABC', '=Sheet1!$A$1')
+          engine.getAllNamedExpressionsSerialized()
+        },
+        (engine: HyperFormula) => {
+          engine.addNamedExpression('ABC', '=Sheet1!$A$1')
+          engine.isItPossibleToChangeNamedExpression('ABC', '=Sheet1!$A$1+100')
+        },
+        (engine: HyperFormula) => {
+          engine.addNamedExpression('ABC', '=Sheet1!$A$1')
+          engine.changeNamedExpression('ABC', '=Sheet1!$A$1+100')
+        },
+        (engine: HyperFormula) => {
+          engine.addNamedExpression('ABC', '=Sheet1!$A$1')
+          engine.removeNamedExpression('ABC')
+        },
+        (engine: HyperFormula) => {
+          engine.addNamedExpression('ABC', '=Sheet1!$A$1')
+          engine.isItPossibleToRemoveNamedExpression('ABC')
+        },
+      ]
 
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
+      otherOperations.forEach(operation => {
+        const engine = HyperFormula.buildFromArray([[
+          1, 2, '=A1+B1'
+        ]])
+        const onCellValueRead = jasmine.createSpy()
+        engine.on(Events._CellValueRead, onCellValueRead)
 
-      engine.getSheetValues(0)
-      expect(onCellValueRead).toHaveBeenCalled()
-    })
+        operation(engine)
 
-    it('fires when user calls getAllSheetsValues', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]])
+        expect(onCellValueRead).not.toHaveBeenCalled()
+      })
 
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
-
-      engine.getAllSheetsValues()
-      expect(onCellValueRead).toHaveBeenCalled()
-    })
-
-    it('fires when user calls copy', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]])
-
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
-
-      engine.copy(simpleCellRange(adr('A1'), adr('C1')))
-      expect(onCellValueRead).toHaveBeenCalled()
-    })
-
-    it('fires when user calls cut', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]])
-
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
-
-      engine.cut(simpleCellRange(adr('A1'), adr('C1')))
-      expect(onCellValueRead).toHaveBeenCalled()
-    })
-
-    it('fires when user calls getRangeValues', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]])
-
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
-
-      engine.getRangeValues(simpleCellRange(adr('A1'), adr('C1')))
-      expect(onCellValueRead).toHaveBeenCalled()
-    })
-
-    it('fires when user calls getNamedExpressionValue', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]])
-
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
-
-      engine.addNamedExpression('ABC', '=Sheet1!$A$1')
-      engine.getNamedExpressionValue('ABC')
-      expect(onCellValueRead).toHaveBeenCalled()
-    })
-
-    it('fires when user calls calculateFormula', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]])
-
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
-
-      engine.calculateFormula('=A1', 0)
-      expect(onCellValueRead).toHaveBeenCalled()
-    })
-
-    it('does not fire when user calls addNamedExpression', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]])
-
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
-
-      engine.addNamedExpression('ABC', '=Sheet1!$A$1')
-      expect(onCellValueRead).not.toHaveBeenCalled()
-    })
-
-    it('does not fire when user calls updateConfig', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]], { maxRows: 10 })
-
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
-
-      engine.updateConfig({ maxRows: 100 })
-      expect(onCellValueRead).not.toHaveBeenCalled()
-    })
-
-    it('does not fire when user sets cell/sheet contents', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]])
-
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
-
-      engine.setCellContents(adr('A1'), 42)
-      engine.setSheetContent(0, [[42]])
-      expect(onCellValueRead).not.toHaveBeenCalled()
-    })
-
-    it('does not fire when user adds sheet/row/column', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]])
-
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
-
-      engine.addSheet('test')
-      engine.addRows(0, [0, 1])
-      engine.addColumns(0, [0, 1])
-      expect(onCellValueRead).not.toHaveBeenCalled()
-    })
-
-    it('does not fire when user calls helper methods', () => {
-      const engine = HyperFormula.buildFromArray([[
-        1, 2, '=A1+B1'
-      ]])
-
-      const onCellValueRead = jasmine.createSpy()
-      engine.on(Events._CellValueRead, onCellValueRead)
-
-      engine.getSheetId('Sheet1')
-      const adr = engine.simpleCellAddressFromString('A1', 0)
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      engine.simpleCellAddressToString(adr!, 0)
-      const range = engine.simpleCellRangeFromString('A1:B2', 0)
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      engine.simpleCellRangeToString(range!, 0)
-      expect(onCellValueRead).not.toHaveBeenCalled()
     })
   })
 })
