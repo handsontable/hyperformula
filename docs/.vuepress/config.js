3579344dae7b76f8ae4c87923f1514c668c5f4e3
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2023-04-12 12:13:20 +0200

    Add favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,7 +21,15 @@ module.exports = {
       })(window,document,'script','dataLayer','GTM-N59TZXR');
     `],
     // Google Console
-    ['meta', { name: 'google-site-verification', content: 'MZpSOa8SNvFLRRGwUQpYVZ78kIHQoPVdVbafHhJ_d4Q' }]
+    ['meta', { name: 'google-site-verification', content: 'MZpSOa8SNvFLRRGwUQpYVZ78kIHQoPVdVbafHhJ_d4Q' }],
+    // Favicon
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/favicon/favicon-32x32.png' }],
+    ['link', { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/favicon/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/favicon/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', color: '#5bbad5', href: '/favicon/safari-pinned-tab.svg' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#da532c' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
   ],
   base: '/',
   plugins: [
