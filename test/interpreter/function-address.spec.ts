b14c9e48d9b123aaa0299f9350801c48fdc15139
Chris Thilgen <thilgen@gmail.com>
2023-05-07 12:52:45 -0700

    add support for negative rows+cols; more tests

--- test/interpreter/function-address.spec.ts
+++ test/interpreter/function-address.spec.ts
@@ -90,10 +90,210 @@ describe('ADDRESS', () => {
 
     expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.WrongArgNumber))
     expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.WrongArgNumber))
-    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.NUM, ErrorMessage.ValueSmall))
+    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
     expect(engine.getCellValue(adr('A4'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.NumberCoercion))
     expect(engine.getCellValue(adr('A5'))).toEqualError(detailedError(ErrorType.NUM, ErrorMessage.ValueSmall))
     expect(engine.getCellValue(adr('A6'))).toEqualError(detailedError(ErrorType.NUM, ErrorMessage.ValueLarge))
     expect(engine.getCellValue(adr('A7'))).toEqualError(detailedError(ErrorType.NAME, ErrorMessage.NamedExpressionName('true')))
+  })
+})
+
+describe('ADDRESS - Compatability Checks', () => {
+  it('row negative - col negative', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=ADDRESS(-1, -1, 1, FALSE())'],
+      ['=ADDRESS(-1, -1, 1, TRUE())'],
+      ['=ADDRESS(-1, -1, 2, FALSE())'],
+      ['=ADDRESS(-1, -1, 2, TRUE())'],
+      ['=ADDRESS(-1, -1, 3, FALSE())'],
+      ['=ADDRESS(-1, -1, 3, TRUE())'],
+      ['=ADDRESS(-1, -1, 4, FALSE())'],
+      ['=ADDRESS(-1, -1, 4, TRUE())'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A4'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A5'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A6'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A7'))).toEqual('R[-1]C[-1]')
+    expect(engine.getCellValue(adr('A8'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+  })
+
+  it('row negative - col zero', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=ADDRESS(-1, 0, 1, FALSE())'],
+      ['=ADDRESS(-1, 0, 1, TRUE())'],
+      ['=ADDRESS(-1, 0, 2, FALSE())'],
+      ['=ADDRESS(-1, 0, 2, TRUE())'],
+      ['=ADDRESS(-1, 0, 3, FALSE())'],
+      ['=ADDRESS(-1, 0, 3, TRUE())'],
+      ['=ADDRESS(-1, 0, 4, FALSE())'],
+      ['=ADDRESS(-1, 0, 4, TRUE())'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A4'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A5'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A6'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A7'))).toEqual('R[-1]C')
+    expect(engine.getCellValue(adr('A8'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+  })
+
+  it('row negative - col one', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=ADDRESS(-1, 1, 1, FALSE())'],
+      ['=ADDRESS(-1, 1, 1, TRUE())'],
+      ['=ADDRESS(-1, 1, 2, FALSE())'],
+      ['=ADDRESS(-1, 1, 2, TRUE())'],
+      ['=ADDRESS(-1, 1, 3, FALSE())'],
+      ['=ADDRESS(-1, 1, 3, TRUE())'],
+      ['=ADDRESS(-1, 1, 4, FALSE())'],
+      ['=ADDRESS(-1, 1, 4, TRUE())'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A4'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A5'))).toEqual('R[-1]C1')
+    expect(engine.getCellValue(adr('A6'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A7'))).toEqual('R[-1]C[1]')
+    expect(engine.getCellValue(adr('A8'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+  })
+
+  it('row zero - col negative', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=ADDRESS(0, -1, 1, FALSE())'],
+      ['=ADDRESS(0, -1, 1, TRUE())'],
+      ['=ADDRESS(0, -1, 2, FALSE())'],
+      ['=ADDRESS(0, -1, 2, TRUE())'],
+      ['=ADDRESS(0, -1, 3, FALSE())'],
+      ['=ADDRESS(0, -1, 3, TRUE())'],
+      ['=ADDRESS(0, -1, 4, FALSE())'],
+      ['=ADDRESS(0, -1, 4, TRUE())'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A4'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A5'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A6'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A7'))).toEqual('RC[-1]')
+    expect(engine.getCellValue(adr('A8'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+  })
+
+  it('row zero - col zero', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=ADDRESS(0, 0, 1, FALSE())'],
+      ['=ADDRESS(0, 0, 1, TRUE())'],
+      ['=ADDRESS(0, 0, 2, FALSE())'],
+      ['=ADDRESS(0, 0, 2, TRUE())'],
+      ['=ADDRESS(0, 0, 3, FALSE())'],
+      ['=ADDRESS(0, 0, 3, TRUE())'],
+      ['=ADDRESS(0, 0, 4, FALSE())'],
+      ['=ADDRESS(0, 0, 4, TRUE())'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A4'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A5'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A6'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A7'))).toEqual('RC')
+    expect(engine.getCellValue(adr('A8'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+  })
+
+  it('row zero - col one', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=ADDRESS(0, 1, 1, FALSE())'],
+      ['=ADDRESS(0, 1, 1, TRUE())'],
+      ['=ADDRESS(0, 1, 2, FALSE())'],
+      ['=ADDRESS(0, 1, 2, TRUE())'],
+      ['=ADDRESS(0, 1, 3, FALSE())'],
+      ['=ADDRESS(0, 1, 3, TRUE())'],
+      ['=ADDRESS(0, 1, 4, FALSE())'],
+      ['=ADDRESS(0, 1, 4, TRUE())'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A3'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A4'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A5'))).toEqual('RC1')
+    expect(engine.getCellValue(adr('A6'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A7'))).toEqual('RC[1]')
+    expect(engine.getCellValue(adr('A8'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+  })
+
+  it('row one - col negative', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=ADDRESS(1, -1, 1, FALSE())'],
+      ['=ADDRESS(1, -1, 1, TRUE())'],
+      ['=ADDRESS(1, -1, 2, FALSE())'],
+      ['=ADDRESS(1, -1, 2, TRUE())'],
+      ['=ADDRESS(1, -1, 3, FALSE())'],
+      ['=ADDRESS(1, -1, 3, TRUE())'],
+      ['=ADDRESS(1, -1, 4, FALSE())'],
+      ['=ADDRESS(1, -1, 4, TRUE())'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A3'))).toEqual('R1C[-1]')
+    expect(engine.getCellValue(adr('A4'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A5'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A6'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A7'))).toEqual('R[1]C[-1]')
+    expect(engine.getCellValue(adr('A8'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+  })
+
+  it('row one - col zero', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=ADDRESS(1, 0, 1, FALSE())'],
+      ['=ADDRESS(1, 0, 1, TRUE())'],
+      ['=ADDRESS(1, 0, 2, FALSE())'],
+      ['=ADDRESS(1, 0, 2, TRUE())'],
+      ['=ADDRESS(1, 0, 3, FALSE())'],
+      ['=ADDRESS(1, 0, 3, TRUE())'],
+      ['=ADDRESS(1, 0, 4, FALSE())'],
+      ['=ADDRESS(1, 0, 4, TRUE())'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A2'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A3'))).toEqual('R1C')
+    expect(engine.getCellValue(adr('A4'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A5'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A6'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+    expect(engine.getCellValue(adr('A7'))).toEqual('R[1]C')
+    expect(engine.getCellValue(adr('A8'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.LessThanOne))
+  })
+
+  it('row one - col one', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=ADDRESS(1, 1, 1, FALSE())'],
+      ['=ADDRESS(1, 1, 1, TRUE())'],
+      ['=ADDRESS(1, 1, 2, FALSE())'],
+      ['=ADDRESS(1, 1, 2, TRUE())'],
+      ['=ADDRESS(1, 1, 3, FALSE())'],
+      ['=ADDRESS(1, 1, 3, TRUE())'],
+      ['=ADDRESS(1, 1, 4, FALSE())'],
+      ['=ADDRESS(1, 1, 4, TRUE())'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqual('R1C1')
+    expect(engine.getCellValue(adr('A2'))).toEqual('$A$1')
+    expect(engine.getCellValue(adr('A3'))).toEqual('R1C[1]')
+    expect(engine.getCellValue(adr('A4'))).toEqual('A$1')
+    expect(engine.getCellValue(adr('A5'))).toEqual('R[1]C1')
+    expect(engine.getCellValue(adr('A6'))).toEqual('$A1')
+    expect(engine.getCellValue(adr('A7'))).toEqual('R[1]C[1]')
+    expect(engine.getCellValue(adr('A8'))).toEqual('A1')
   })
 })
