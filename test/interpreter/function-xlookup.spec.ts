e9cabd6279ed1c37e4cbf62ad49f88079e3fa2d9
Selim Youssry <selimyoussry@gmail.com>
2024-06-16 16:56:09 +0000

    ISS-1 sapiologie/hyperformula Working XLOOKUP without support for optional parameters

--- test/interpreter/function-xlookup.spec.ts
+++ test/interpreter/function-xlookup.spec.ts
@@ -4,20 +4,34 @@
  */
 
 import { HyperFormula } from './../../src'
-import {adr} from '../testUtils'
+import { adr } from '../testUtils'
+import { AbsoluteCellRange } from '../../src/AbsoluteCellRange'
 
 
 describe('Function XLOOKUP', () => {
-    it('should find value in range (official example 1)', () => {
-        const engine = HyperFormula.buildFromArray([
-            ['China', 'CN', '+86'],
-            ['India', 'IN', '+91'],
-            ['United States', 'US', '+1'],
-            ['Indonesia', 'ID', '+62'],
-            ['France', 'FR', '+33'],
-            ['=XLOOKUP("Indonesia", A1:A5, C1:C5)'],
-        ], { useColumnIndex: false })
+  it('should find value in simple column range (official example 1)', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['China', 'CN'],
+      ['India', 'IN'],
+      ['United States', 'US'],
+      ['Indonesia', 'ID'],
+      ['France', 'FR'],
+      ['=XLOOKUP("Indonesia", A1:A5, B1:B5)'],
+    ], { useColumnIndex: false })
 
-        expect(engine.getCellValue(adr('A6'))).toEqual('+62')
-    })
+    expect(engine.getCellValue(adr('A6'))).toEqual('ID')
+  })
+
+  it('should find range in table (official example 2)', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['8389', 'Dianne Pugh', 'Finance'],
+      ['4390', 'Ned Lanning', 'Marketing'],
+      ['8604', 'Margo Hendrix', 'Sales'],
+      ['8389', 'Dianne Pugh', 'Finance'],
+      ['4937', 'Earlene McCarty', 'Accounting'],
+      ['=XLOOKUP(A1, A2:A5, B2:C5)'],
+    ], { useColumnIndex: false })
+
+    expect(engine.getRangeValues(AbsoluteCellRange.spanFrom(adr('A6'), 2, 1))).toEqual([['Dianne Pugh', 'Finance']])
+  })
 })
