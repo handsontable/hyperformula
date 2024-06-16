eecce3a43a4c7fac98358134d08ec5479648accc
Selim Youssry <selimyoussry@gmail.com>
2024-06-16 15:39:02 +0000

    ISS-1 sapiologie/hyperformula progress on Xlookup logic

--- test/interpreter/function-xlookup.spec.ts
+++ test/interpreter/function-xlookup.spec.ts
@@ -1,6 +1,23 @@
+/**
+ * Examples from
+ * https://support.microsoft.com/en-us/office/xlookup-function-b7fd680e-6d10-43e6-84f9-88eae8bf5929
+ */
+
+import { HyperFormula } from './../../src'
+import {adr} from '../testUtils'
+
+
 describe('Function XLOOKUP', () => {
-    // TODO @selim - implement me
-    it('wrong number of arguments', () => {
-        expect(true).toEqual(true)
+    it('should find value in range (official example 1)', () => {
+        const engine = HyperFormula.buildFromArray([
+            ['China', 'CN', '+86'],
+            ['India', 'IN', '+91'],
+            ['United States', 'US', '+1'],
+            ['Indonesia', 'ID', '+62'],
+            ['France', 'FR', '+33'],
+            ['=XLOOKUP("Indonesia", A1:A5, C1:C5)'],
+        ], { useColumnIndex: false })
+
+        expect(engine.getCellValue(adr('A6'))).toEqual('+62')
     })
 })
