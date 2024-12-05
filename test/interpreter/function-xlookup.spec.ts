9c448c259180c96264a3e28630d93ceb4e7f4dca
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2024-12-05 11:39:44 +0100

    Add test suite for the XLOOKUP function

---
+++ test/interpreter/function-xlookup.spec.ts
@@ -0,0 +1,8 @@
+import {HyperFormula} from '../../src'
+import {ErrorType} from '../../src'
+import {ErrorMessage} from '../../src/error-message'
+import {adr, detailedError} from '../testUtils'
+
+describe('Function XLOOKUP', () => {
+  
+})
