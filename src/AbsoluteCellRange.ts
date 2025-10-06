b4c7df6331ef0d8ce9a7c2c72b24545ca8083775
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2025-10-06 11:45:44 +0200

    Fix type error

--- src/AbsoluteCellRange.ts
+++ src/AbsoluteCellRange.ts
@@ -28,11 +28,12 @@ export interface SimpleCellRange {
 
 /**
  * Type guard that checks if an object is a valid SimpleCellRange.
- * @param {unknown} obj - The object to check
+ * @param {unknown} val - Value to check
  * @returns {boolean} True if and only if the object is a valid SimpleCellRange
  */
-export function isSimpleCellRange(obj: unknown): obj is SimpleCellRange {
-  if (obj && (typeof obj === 'object' || typeof obj === 'function')) {
+export function isSimpleCellRange(val: unknown): val is SimpleCellRange {
+  if (val && (typeof val === 'object' || typeof val === 'function')) {
+    const obj = val as Record<string, unknown>
     return 'start' in obj && isSimpleCellAddress(obj.start) && 'end' in obj && isSimpleCellAddress(obj.end)
   } else {
     return false
