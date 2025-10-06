94d563b120cd021f2aac390956a2c0df3cb6f50a
Kuba Sekowski <jakub.sekowski@handsontable.com>
2025-10-06 10:42:02 +0200

    Testing compatibility of HF with Excel and Sheets (#1537)
    * Add test/compatibility subdirectory
    
    * Install exceljs lib
    
    * Compare evaluation results
    
    * Use epsilon to compare numbers
    
    * Handle cell errors
    
    * Add script that runs all the test files in a loop
    
    * Handle null-like values in xlsx files
    
    * Update file-import guide in the docs to handle empty cells correctly
    
    * Add test:compatibility to npm run test script
    
    * Add test/compatibility/README.md file

---
+++ DEV_DOCS.md
@@ -0,0 +1,9 @@
+# Dev Docs
+
+Random notes and things to know useful for maintainers and contributors.
+
+## Sources of the function translations
+
+HF supports internationalization and provides the localized function names for all built-in languages. When looking for the valid translations for the new functions, try these sources:
+- https://support.microsoft.com/en-us/office/excel-functions-translator-f262d0c0-991c-485b-89b6-32cc8d326889
+- http://dolf.trieschnigg.nl/excel/index.php
---
+++ test/unit/interpreter/function-csch.spec.ts
@@ -0,0 +1,48 @@
+import {ErrorType, HyperFormula} from '../../../src'
+import {ErrorMessage} from '../../../src/error-message'
+import {adr, detailedError} from '../testUtils'
+
+describe('Function CSCH', () => {
+  it('happy path', () => {
+    const engine = HyperFormula.buildFromArray([['=CSCH(1)']])
+
+    expect(engine.getCellValue(adr('A1'))).toBeCloseTo(0.850918128239322, 11)
+  })
+
+  it('when value not numeric', () => {
+    const engine = HyperFormula.buildFromArray([['=CSCH("foo")']])
+
+    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.VALUE, ErrorMessage.NumberCoercion))
+  })
+
+  it('wrong number of arguments', () => {
+    const engine = HyperFormula.buildFromArray([['=CSCH()', '=CSCH(1,-1)']])
+
+    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.WrongArgNumber))
+    expect(engine.getCellValue(adr('B1'))).toEqualError(detailedError(ErrorType.NA, ErrorMessage.WrongArgNumber))
+  })
+
+  it('use number coercion', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['="-1"', '=CSCH(A1)'],
+    ])
+
+    expect(engine.getCellValue(adr('B1'))).toBeCloseTo(-0.850918128239322)
+  })
+
+  it('div/zero', () => {
+    const engine = HyperFormula.buildFromArray([
+      [0, '=CSCH(A1)'],
+    ])
+
+    expect(engine.getCellValue(adr('B1'))).toEqualError(detailedError(ErrorType.DIV_BY_ZERO))
+  })
+
+  it('errors propagation', () => {
+    const engine = HyperFormula.buildFromArray([
+      ['=CSCH(4/0)'],
+    ])
+
+    expect(engine.getCellValue(adr('A1'))).toEqualError(detailedError(ErrorType.DIV_BY_ZERO))
+  })
+})
