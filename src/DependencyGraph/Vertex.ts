df95821fb7dd6bd393c020aa7607db220f870aa6
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2026-01-09 14:21:45 +0100

    Add explenatory comment

--- src/DependencyGraph/Vertex.ts
+++ src/DependencyGraph/Vertex.ts
@@ -9,6 +9,10 @@ import { GraphNode } from './Graph'
  * Base class for all types of vertices in the dependency graph
  */
 export abstract class Vertex implements GraphNode {
+  /**
+   * Used internally by Graph class. Should not be set by other classes.
+   * idInGraph is defined if and only if the vertex is stored in dependency graph.
+   */
   public idInGraph?: number
 
   constructor() {
